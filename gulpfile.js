fe172677b3a800844f79abaa14845afc276cfaa4
Ian Page Hands <iphands@gmail.com>
2018-05-03 13:49:35 -0400

    add a watcher for pug/sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,16 @@ const gulp = require('gulp');
 const pug  = require('gulp-pug');
 const sass = require('gulp-sass');
 
+gulp.task('default', ['sass:watch', 'pug:watch']);
+
+gulp.task('sass:watch', ['sass'], () => {
+    gulp.watch('sass/**/*.scss', ['sass']);
+});
+
+gulp.task('pug:watch', ['pug'], () => {
+    gulp.watch('pug/**/*.pug', ['pug']);
+});
+
 gulp.task('sass', () => {
     return gulp.src('sass/chrome.scss')
         .pipe(sass().on('error', sass.logError))
