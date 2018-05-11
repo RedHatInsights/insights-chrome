2a9c4b4329165e395516a72d3f665db07414e5c8
Karel Hala <khala@redhat.com>
2018-05-11 15:40:07 +0200

    Currently observed items stays active

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,42 +1,45 @@
-const menu = {
-    'dashboard': {
-        url: '/insights/dashboard/map'
-    },
-    'advisor': {
-        url: '/insights/advisor'
-    },
-    'security': {
-        url: '/insights/security'
-    },
-    'compliance': {
-        url: '/insights/compliance'
-    },
-    'cmaas': {
-        url: '/insights/cmaas'
-    },
-    'vmaas': {
-        url: '/insights/vmaas'
-    },
-    'inventory': {
-        url: '/insights/deployments'
-    },
-    'reports': {
-        url: '/insights/reports'
-    },
-    'settings': {
-        url: '/insights/settings'
+document.addEventListener("DOMContentLoaded", function (event) {
+    const menu = {
+        'dashboard': {
+            url: '/insights/dashboard/map'
+        },
+        'advisor': {
+            url: '/insights/advisor'
+        },
+        'security': {
+            url: '/insights/security'
+        },
+        'compliance': {
+            url: '/insights/compliance'
+        },
+        'cmaas': {
+            url: '/insights/cmaas'
+        },
+        'vmaas': {
+            url: '/insights/vmaas'
+        },
+        'inventory': {
+            url: '/insights/deployments'
+        },
+        'reports': {
+            url: '/insights/reports'
+        },
+        'settings': {
+            url: '/insights/settings'
+        }
+    };
+
+    let selectedMenu = Object.keys(menu).filter(oneKey => menu[oneKey].url === location.pathname)[0];
+
+    function makeActive(menuItem) {
+        selectedMenu = menuItem;
+        document.getElementById(selectedMenu).classList.add('active');
     }
-};
 
-let selectedMenu = 'dashboard';
+    makeActive(selectedMenu);
 
-const selectOption = (menuItem) => {
-    const prevMenu = selectedMenu;
-    selectedMenu = menuItem;
-
-    // remove active class from prevMenu and set to selectedMenu
-    document.getElementById(prevMenu).classList.remove('active');
-    document.getElementById(selectedMenu).classList.add('active');
-
-    window.location.href = menu[menuItem].url;
-};
+    window.selectOption = (menuItem) => {
+        makeActive(selectedMenu);
+        window.location.href = menu[menuItem].url;
+    };
+});
