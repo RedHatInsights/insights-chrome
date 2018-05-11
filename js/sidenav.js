be16f3083536169a192501806e8f5dd1f4eaa269
Josh Kinlaw <jkinlaw@redhat.com>
2018-05-11 08:50:46 -0400

    adds menu page switching

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,3 +1,33 @@
+const menu = {
+    'dashboard': {
+        url: '/insights/dashboard/map'
+    },
+    'advisor': {
+        url: '/insights/advisor'
+    },
+    'security': {
+        url: '/insights/security'
+    },
+    'compliance': {
+        url: '/insights/compliance'
+    },
+    'cmaas': {
+        url: '/insights/cmaas'
+    },
+    'vmaas': {
+        url: '/insights/vmaas'
+    },
+    'inventory': {
+        url: '/insights/deployments'
+    },
+    'reports': {
+        url: '/insights/reports'
+    },
+    'settings': {
+        url: '/insights/settings'
+    }
+};
+
 let selectedMenu = 'dashboard';
 
 const selectOption = (menuItem) => {
@@ -7,4 +37,6 @@ const selectOption = (menuItem) => {
     // remove active class from prevMenu and set to selectedMenu
     document.getElementById(prevMenu).classList.remove('active');
     document.getElementById(selectedMenu).classList.add('active');
+
+    window.location.href = menu[menuItem].url;
 };
