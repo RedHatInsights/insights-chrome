a9d8d7bbc0f18c08255958087f4b2fd3ffe0a50f
Josh Kinlaw <jkinlaw@redhat.com>
2018-05-11 14:16:20 -0400

    moving code under src folder

--- gulpfile.js
+++ gulpfile.js
@@ -8,32 +8,32 @@ const sass = require('gulp-sass');
 gulp.task('default', ['sass:watch', 'pug:watch', 'scripts:watch']);
 
 gulp.task('sass:watch', ['sass'], () => {
-    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('src/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('pug:watch', ['pug'], () => {
-    gulp.watch('pug/**/*.pug', ['pug']);
+    gulp.watch('src/pug/**/*.pug', ['pug']);
 });
 
 gulp.task('scripts:watch', ['scripts'], () => {
-    gulp.watch('js/**/*.js', ['scripts']);
+    gulp.watch('src/js/**/*.js', ['scripts']);
 });
 
 gulp.task('sass', () => {
-    return gulp.src('sass/chrome.scss')
+    return gulp.src('src/sass/chrome.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('build'));
 });
 
 gulp.task('pug', () => {
-  return gulp.src('pug/*.pug')
+  return gulp.src('src/pug/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('build/snippets'))
     ;
 });
 
 gulp.task('scripts', function () {
-    return gulp.src('js/**/*.js')
+    return gulp.src('src/js/**/*.js')
         .pipe(concat('js/chrome.js'))
         .pipe(gulp.dest('build'))
 });
