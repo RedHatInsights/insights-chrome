c3acecfcdb857aad037b48580ca3cae21e3d1be5
Karel Hala <khala@redhat.com>
2018-05-24 18:19:09 +0200

    Add redux and improve it with custom API

--- src/js/chrome.js
+++ src/js/chrome.js
@@ -1,1 +1,22 @@
-import './sidenav';
+import {
+    applyReducerHash,
+    middlewares,
+    dispatchActionsToStore,
+    rootReducer,
+    addReducer,
+    spinUpStore,
+    combineReducersWithState,
+    clearReducers
+} from './redux-config'
+
+window.insights = window.insights || {};
+window.insights.redux = {
+    middlewares,
+    applyReducerHash,
+    dispatchActionsToStore,
+    rootReducer,
+    addReducer,
+    spinUpStore,
+    combineReducersWithState,
+    clearReducers
+};
---
+++ src/js/redux/actions.js
@@ -0,0 +1,3 @@
+import { CLICK_ACTION } from './action-types';
+
+export const clickAction = (data) => ({ type: CLICK_ACTION, payload: data });
