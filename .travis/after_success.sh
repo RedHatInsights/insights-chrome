6e8728c1d5ee28ddf28eb54b3bbaa56bf28c5a76
Josh Kinlaw <jkinlaw@redhat.com>
2018-07-19 12:48:36 -0400

    Implements auto deployment process with Travis and Jenkins (#13)
    * adding travis/jenkins auto deployments
    
    * updating deploy_key
    
    * updating REPO in travis.xml
    
    * updating Jenkinsfile with correct Net Storage paths
    
    * testing PR
    
    * replacing deploy_key
    
    * replacing key
    
    * try again
    
    * removing test code
    
    * changing step names

---
+++ .travis/after_success.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Check if it is a pull request
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    echo -e "Pull Request, not pushing a build"
+    exit 0;
+fi
+
+# If current dev branch is master, push to build repo master
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    .travis/release.sh
+fi
+
+# If current dev branch is stable/foo, push to build repo stable
+if [[ ${TRAVIS_BRANCH} =~ stable\/* ]]; then
+    .travis/release_stable.sh
+fi
