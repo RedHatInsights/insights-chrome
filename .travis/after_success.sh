a4077f5f2691d48e559d4fee2998928d821fdb1d
Ryan Long <rlong@redhat.com>
2018-07-20 11:41:46 -0400

    moving deploy setup to after_success.sh

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 # Check if it is a pull request
+# If it is not a pull request, generate the deploy key
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     echo -e "Pull Request, not pushing a build"
     exit 0;
+else
+    openssl aes-256-cbc -K $encrypted_3e47efa205c7_key -iv $encrypted_3e47efa205c7_iv -in deploy_key.enc -out deploy_key -d
+    chmod 600 deploy_key
+    eval `ssh-agent -s`
+    ssh-add deploy_key
 fi
 
 # If current dev branch is master, push to build repo master
