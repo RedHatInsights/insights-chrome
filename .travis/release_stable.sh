05a7700780ce14014e31e50e657c19accc9631f3
Josh Kinlaw <jkinlaw@redhat.com>
2018-08-17 10:16:43 -0400

    moves build process from internal Jenkins to OpenShift Jenkins

--- .travis/release_stable.sh
+++ .travis/release_stable.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
-cp Jenkinsfile build/Jenkinsfile
-cd build
+git clone ${REPO}.git
+cd dist
+cp ../${REPO_DIR}/Jenkinsfile ./Jenkinsfile
 git init
 git config --global user.name $COMMIT_AUTHOR_USERNAME
 git config --global user.email $COMMIT_AUTHOR_EMAIL
