c693cdeca9d1b46b9ba5104ce664432233ec1d74
Josh Kinlaw <jkinlaw@redhat.com>
2018-08-17 10:25:53 -0400

    Enhancements/build on open shift (#22)
    * changing Jenkinsfile to work with OpenShift Jenkins instance
    
    * copy/pasta error
    
    * moves build process from internal Jenkins to OpenShift Jenkins
    
    * removing Jenkinsfile

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
-cp Jenkinsfile build/Jenkinsfile
-cd build
+git clone ${REPO}.git
+cd dist
+cp ../${REPO_DIR}/Jenkinsfile ./Jenkinsfile
 git init
 git config --global user.name $COMMIT_AUTHOR_USERNAME
 git config --global user.email $COMMIT_AUTHOR_EMAIL
 git remote add travis-build ${REPO}.git
 git add .
 git commit -m 'Build by Travis'
-git config --list
 git push --force --set-upstream travis-build master
--- .travis/release_stable.sh
+++ .travis/release_stable.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
-cp Jenkinsfile build/Jenkinsfile
-cd build
+git clone ${REPO}.git
+cd dist
+cp ../${REPO_DIR}/Jenkinsfile ./Jenkinsfile
 git init
 git config --global user.name $COMMIT_AUTHOR_USERNAME
 git config --global user.email $COMMIT_AUTHOR_EMAIL
 git remote add travis-build ${REPO}.git
-git fetch origin
-git branch -v -a
 git checkout -b stable
 git add .
 git commit -m 'Build Stable by Travis'
-git config --list
 git push --force --set-upstream travis-build stable:stable
