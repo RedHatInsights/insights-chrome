04f3cb59db9689735e72e5ea6c93e1126495e6fe
Ryan Long <rlong@redhat.com>
2018-10-09 15:00:52 -0400

    dirty pendo

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,4 +1,22 @@
+import { WSAEINVALIDPROVIDER } from "constants";
+
 'use strict';
 
+let priv = {};
+let pub = {};
+
 // eslint-disable-next-line
-(function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/f210c485-387f-43ad-4eee-f55bab22507f/pendo.js';z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+priv.pendoBlob = function (apiKey) { (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/f210c485-387f-43ad-4eee-f55bab22507f/pendo.js';z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo') };
+
+function AnalyticsService() {
+    pub.initPendo = function () {
+        priv.pendoBlob();
+        const pendoConf = {
+            apiKey: 'f210c485-387f-43ad-4eee-f55bab22507f'
+        };
+        window.pendo.initialize(pendoConf);
+        }
+    return pub;
+}
+
+AnalyticsService.initPendo();
