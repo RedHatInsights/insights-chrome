554e63f0e5ca0ff3913133bcc7d38c719c4fb12c
Ryan Long <rlong@redhat.com>
2018-10-10 12:16:25 -0400

    correctly adding pendo

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,22 +1,11 @@
-import { WSAEINVALIDPROVIDER } from "constants";
-
 'use strict';
 
-let priv = {};
-let pub = {};
-
 // eslint-disable-next-line
-priv.pendoBlob = function () { (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/f210c485-387f-43ad-4eee-f55bab22507f/pendo.js';z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo') };
+(function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/f210c485-387f-43ad-4eee-f55bab22507f/pendo.js';z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
 
-function AnalyticsService() {
-    pub.initPendo = function () {
-        priv.pendoBlob();
-        const pendoConf = {
-            apiKey: 'f210c485-387f-43ad-4eee-f55bab22507f'
-        };
-        window.pendo.initialize(pendoConf);
-        }
-    return pub;
-}
+const pendoConf = {
+    apiKey: 'f210c485-387f-43ad-4eee-f55bab22507f'
+    // TODO add account data
+};
 
-AnalyticsService.initPendo();
+window.pendo.initialize(pendoConf);
