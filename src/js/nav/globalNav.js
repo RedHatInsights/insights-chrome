68b03bf8ffcb8202904e48037d5ceb627e2d0885
Chris Hambridge <chambrid@redhat.com>
2018-10-17 14:50:01 -0400

    Add subscriptions to the global navigation.

--- src/js/nav/globalNav.js
+++ src/js/nav/globalNav.js
@@ -31,6 +31,9 @@ export const options = Object.freeze([{
     id: 'service-portal',
     title: 'Service Portal'
 }, {
+    id: 'subscriptions',
+    title: 'Subscriptions'
+}, {
     id: 'settings',
     title: 'Settings'
 }]);
