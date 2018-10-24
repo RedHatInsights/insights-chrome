da56684680dffceda68dbf59b97c5541adc67e79
Karel Hala <ch4rlien@gmail.com>
2018-10-24 18:56:53 +0200

    Update packages and move cokies initialization to header file (#56)

--- src/js/chrome.js
+++ src/js/chrome.js
@@ -4,25 +4,8 @@ import loadInventory from './inventory';
 import auth from './auth';
 import analytics from './analytics';
 import jwt from 'jwt-redhat';
-import cookies from 'js-cookie';
 
 const onAuth = auth();
-
-// DO THIS FRIST, BEFORE ANYTHING ELSE :D
-// Why: this is to ensure (in a sync manner) we have the rh_jwt coookie prepped
-// Keycloak sends this back as a query param
-// Apps need it to be avalible right away
-// the jwt-redhat lib will take care of this
-// but it seems to happen only after a network call and we need to to be sync
-(function() {
-    const search = window.location.search;
-    const split  = search.split('KEYCLOAK_IDENTITY=');
-    const fifteenMinutes = new Date(new Date().getTime() + 15 * 60 * 1000);
-    if (split && split.length === 2) {
-        if (console.log) { console.log('chrome_debug: forcing cookie from param'); }
-        cookies.set('rh_jwt', split[1], { domain: '.redhat.com', expires: fifteenMinutes });
-    }
-}());
 
 onAuth.then(() => {
     const userInfo = jwt.getUserInfo();
