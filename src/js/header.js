abee266ceda2b803257b64d9a62ff25e30e6afaa
Ian Page Hands <iphands@gmail.com>
2018-10-24 18:48:45 -0400

    two in one :( set public-url for maps, add url-parse to KC head logic

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,5 @@
 import cookies from 'js-cookie';
+import parse   from 'url-parse';
 
 // DO THIS FRIST, BEFORE ANYTHING ELSE :D
 // Why: this is to ensure (in a sync manner) we have the rh_jwt coookie prepped
@@ -7,16 +8,17 @@ import cookies from 'js-cookie';
 // the jwt-redhat lib will take care of this
 // but it seems to happen only after a network call and we need to to be sync
 (function () {
-    const search = window.location.search;
-    const split = search.split('KEYCLOAK_IDENTITY=');
-    const fifteenMinutes = new Date(new Date().getTime() + 15 * 60 * 1000);
-    if (split && split.length === 2) {
+    const url = parse(window.location.href, true);
+
+    if (url.query.hasOwnProperty('code') && url.query.code.length > 4) {
+        const fifteenMinutes = new Date(new Date().getTime() + 15 * 60 * 1000);
+
         // eslint-disable-next-line no-console
         if (console.log) {
             // eslint-disable-next-line no-console
             console.log('chrome_debug: forcing cookie from param');
         }
 
-        cookies.set('rh_jwt', split[1], { domain: '.redhat.com', expires: fifteenMinutes });
+        cookies.set('rh_jwt', url.query.code, { domain: '.redhat.com', expires: fifteenMinutes });
     }
 }());
