ac6a5bae661d506fd927f0da8900ecae8e59b548
Ryan Long <rlong@redhat.com>
2018-11-05 10:06:35 -0500

    adding ability to refresh token when expires

--- src/js/jwt/jwt.js
+++ src/js/jwt/jwt.js
@@ -14,7 +14,6 @@ const priv = {};
 
 pub.login = () => {
     log('Logging in');
-
     // Redirect to login
     priv.keycloak.login({ redirectUri: location.href });
 };
@@ -26,11 +25,16 @@ pub.init = (options) => {
     options.url = insightsUrl();
 
     priv.keycloak = Keycloak(options);
+
+    priv.keycloak.onTokenExpired = pub.updateToken;
+
     return priv.keycloak.init(options);
 };
 
 pub.logout = () => {
     log('Logging out');
+
+    priv.keycloak.clearToken();
 
     // Redirect to logout
     priv.keycloak.logout(priv.keycloak);
@@ -44,6 +48,19 @@ pub.getUser = () => {
 pub.userReady = () => {
     log(`User ready: ${priv.keycloak.authenticated}`);
     return priv.keycloak.authenticated;
+};
+
+pub.updateToken = () => {
+    log('Trying to update token');
+    priv.keycloak.updateToken().success(function(refreshed) {
+        if (refreshed) {
+            log('Token was successfully refreshed');
+        } else {
+            log('Token is still valid');
+        }
+    }).error(function() {
+        log('Failed to refresh the token, or the session has expired');
+    });
 };
 
 module.exports = pub;
--- src/js/jwt/logger.js
+++ src/js/jwt/logger.js
@@ -3,7 +3,7 @@
 module.exports = (fileName) => {
     return (msg) => {
         if (window.console) {
-            window.console.log(`[jwt][${fileName}] ${msg}`);
+            window.console.log(`[JWT][${fileName}] ${msg}`);
         }
     };
 };
