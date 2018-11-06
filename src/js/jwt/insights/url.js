da7968f0424a2c07404e3f051b7d0ae26ff03659
Ryan Long <rlong@redhat.com>
2018-11-06 12:57:24 -0500

    updating dropdown, updating docs

--- src/js/jwt/insights/url.js
+++ src/js/jwt/insights/url.js
@@ -5,11 +5,13 @@ module.exports = (env) => {
     // Get the environments
     const entries = Object.entries(env);
     for (const [keys, values] of entries) {
-        // Returns: envirnment and object containing urls and sso path
-        // Iterate through single object with urls + sso
-        for (const urls of values.url) {
+        /* Returns:
+         * Keys: Environments - prod, ci, etc
+         * Values: Object with two arrays - {[[url], sso]}
+         */
+        for (const url of values.url) {
             // If it matches the hostname, return the sso url
-            if (urls === location.hostname) {
+            if (url === location.hostname) {
                 log(`SSO: ${values.sso}`);
                 log(`ENV: ${keys}`);
                 return (values.sso);
