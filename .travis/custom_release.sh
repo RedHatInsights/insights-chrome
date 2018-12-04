f6065d3a66af43ff3344724fb0f1d96dc65b730e
Ian Page Hands <iphands@gmail.com>
2018-12-04 15:34:36 -0500

    more send everywhere work

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+
 # for now in chrome... push everywhere when master updates
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     for env in ci qa prod
     do
         for release in stable beta
         do
-            git remote remove travis-build || true
+            echo
+            echo
+            echo "PUSHING ${env-release}"
+            rm -rf ./build/.git
             .travis/release.sh "${env-release}"
         done
     done
