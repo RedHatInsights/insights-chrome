6215e670c970ebd452f9c86509001a2fc7e938e5
Asa Price <aprice@redhat.com>
2019-01-21 12:18:46 -0500

    Remove unintentional file addition

--- local.chrome.only.js
+++
@@ -1,9 +0,0 @@
-/*global module*/
-
-// SPANDX_CONFIG=./local.api.only.js bash ~/prog/insights/proxy/scripts/run.sh
-
-module.exports = {
-    routes: {
-        '/insights/platform' : {host:  'https://access.redhat.com/'}
-    }
-};
