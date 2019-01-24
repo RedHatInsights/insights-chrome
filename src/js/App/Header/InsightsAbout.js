92d5da09226525c2af95ee7fc07889fad118f9d6
Ian Page Hands <iphands@gmail.com>
2019-01-24 02:37:50 -0600

    Features/enable app about (#111)
    * Add about application modal with username and active app
    
    * refactored TextListItems, and lint
    
    * added blurb and app path
    
    * About: Show versions for Chrome, Inventory, Remediations, Current App
    
    * Remove unintentional file addition
    
    * About page PR changes
    
    Added more app versions to About; moved function from render(); consolidated app name/path/version & refactored methods; fixed linting errors

---
+++ src/js/App/Header/InsightsAbout.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { AboutModal, TextContent, TextList, TextListItem } from '@patternfly/react-core';
+import logo from './logo.svg';
+import { connect } from 'react-redux';
+
+class InsightsAbout extends Component {
+    constructor(props) {
+        super(props);
+
+        let app;
+        if (this.props.globalNav) {
+            app = this.props.globalNav.find(item => item.active);
+            if (app.subItems) {
+                const subApp = app.subItems.find(subItem => subItem.id === this.props.activeApp);
+                app = subApp && subApp.reload ? subApp : app;
+            }
+        }
+
+        this.state = {
+            appDetails: { apps: [
+                { name: 'Chrome', path: 'static/chrome/app.info.json', version: 'N/A' },
+                { name: 'Inventory', path: `platform/inventory/app.info.json`, version: 'N/A' },
+                { name: 'Remediations', path: `platform/remediations/app.info.json`, version: 'N/A' },
+                { name: 'Vulnerabilities', path: 'platform/vulnerability/app.info.json', version: 'N/A' },
+                { name: 'Compliance', path: 'platform/compliance/app.info.json', version: 'N/A' },
+                { name: 'Cost Management', path: 'platform/cost-management/app.info.json', version: 'N/A' },
+                { name: 'Advisor', path: 'platform/advisor/app.info.json', version: 'N/A' }
+            ] },
+            currentApp: app && app.title
+        };
+    }
+
+    getItem(term, details) {
+        return <React.Fragment>
+            <TextListItem component="dt">{term}:</TextListItem>
+            <TextListItem component="dd">
+                {
+                    (typeof details === 'function') ? details() : details
+                }
+            </TextListItem>
+        </React.Fragment>;
+    }
+
+    componentDidMount() {
+        function updateAppVersion(oldAppDetails, app, version) {
+            let appDetails = oldAppDetails;
+            for (let i = 0; i < appDetails.apps.length; i++) {
+                if (appDetails.apps[i].name === app.name) {
+                    appDetails.apps[i].version = version;
+                    break;
+                }
+            }
+
+            this.setState(appDetails);
+        }
+
+        let appDetails = this.state.appDetails;
+        this.state.appDetails.apps.forEach(function(app) {
+            fetch(app.path)
+            .then(response => response.json())
+            .then(data => updateAppVersion(appDetails, app, data.travis.build_number));
+        });
+    }
+
+    render() {
+        const { isModalOpen, onClose, user } = this.props;
+
+        return (
+            <AboutModal
+                isOpen={isModalOpen}
+                onClose={onClose}
+                brandImageSrc={logo}
+                brandImageAlt="Red Hat Insights Logo"
+                heroImageSrc={`${document.baseURI}/static/chrome/assets/images/pfbg_2000.jpg`}
+            >
+                <p>
+                    Please include these details when opening a support case against Insights
+                </p>
+                <TextContent>
+                    <TextList component="dl">
+                        {this.getItem('User Name', user && user.username)}
+                        {this.getItem('Current Application', this.state.currentApp)}
+                        {this.getItem('Application Path', window.location.pathname)}
+                        {this.state.appDetails.apps.map((app) => {
+                            return this.getItem(app.name + ' Version', app.version);
+                        })}
+                    </TextList>
+                </TextContent>
+            </AboutModal>
+        );
+    }
+}
+
+function mapStateToProps({ chrome: { user, appId, globalNav, activeApp } }) {
+    return { appId, globalNav, user, activeApp };
+}
+
+export default connect(mapStateToProps)(InsightsAbout);
