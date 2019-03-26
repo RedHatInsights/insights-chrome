0a6d9e547a39f19bd5bc13af0f0006c9ab0b5646
Ian Page Hands <iphands@gmail.com>
2019-03-26 16:35:58 -0400

    fix tests: added jwt object to libjwt mock

--- src/js/chrome.test.js
+++ src/js/chrome.test.js
@@ -3,26 +3,35 @@ import { chromeInit } from './entry';
 import * as actions from './redux/actions.js';
 
 const mockedPromis = new Promise(() => {});
+const mockedJwt = {
+    getUserInfo: () => {
+        return { then: () => { return { user: 'mock' }; } };
+    }
+};
+
+const getMockLibJwt = () => {
+    return { initPromise: mockedPromis, jwt: mockedJwt };
+};
 
 describe('Chrome API', () => {
     test('can be initialized', () => {
-        chromeInit({ initPromise: mockedPromis });
+        chromeInit(getMockLibJwt());
     });
 
     test('allows for an event listener to be registered', () => {
-        const chrome = chromeInit({ initPromise: mockedPromis });
+        const chrome = chromeInit(getMockLibJwt());
         chrome.on('APP_NAVIGATION', () => true);
     });
 
     test('throws an error if an unknown event listener registration is attempted', () => {
-        const chrome = chromeInit({ initPromise: mockedPromis });
+        const chrome = chromeInit(getMockLibJwt());
         expect(() => chrome.on('NON_EXISTENT_EVENT', () => true))
         .toThrowError('Unknown event type: NON_EXISTENT_EVENT');
     });
 
     test('allows for an event listener to be registered', () => {
         let result;
-        const chrome = chromeInit({ initPromise: mockedPromis });
+        const chrome = chromeInit(getMockLibJwt());
         chrome.on('APP_NAVIGATION', event => result = event);
 
         chrome.$internal.store.dispatch(
@@ -36,7 +45,7 @@ describe('Chrome API', () => {
 
     test('allows for an event listener to be unregistered', () => {
         let result;
-        const chrome = chromeInit({ initPromise: mockedPromis });
+        const chrome = chromeInit(getMockLibJwt());
         const unregister = chrome.on('APP_NAVIGATION', event => result = event);
 
         chrome.$internal.store.dispatch(
