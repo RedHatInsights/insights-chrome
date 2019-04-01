a83a42a92b55e864e2afbcc430aff7d92a5d9a4c
Ryan Long <rlong@redhat.com>
2019-04-01 09:50:00 -0400

    updating analytics to include adobe prod/pre-prod

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,11 +1,30 @@
 'use strict';
 
+function initAdobe() {
+    let adobe = document.createElement('script');
+
+    // production: dtm.js
+    // pre-prod: dtm-staging.js
+    const isStaging = (window.location.hostname === 'cloud.redhat.com' ? '' : '-staging.js');
+
+    adobe.type = ('text/javascript');
+    adobe.src = `https://www.redhat.com/dtm${isStaging}.js`;
+    adobe.async = true; //asynchronous
+    console.log(adobe);
+    document.head.appendChild(adobe);
+}
+
+
 function initPendo(pendoConf) {
     window.pendo.initialize(pendoConf);
 }
 
 export default (user) => {
 
+    // Initialize Adobe
+    initAdobe();
+
+    // Initialize Pendo
     // eslint-disable-next-line
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/f210c485-387f-43ad-4eee-f55bab22507f/pendo.js';z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
 
