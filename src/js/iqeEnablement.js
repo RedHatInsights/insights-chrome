b86939517ec197e60c1718bd693e0b6b650fbce9
Karel Hala <khala@redhat.com>
2019-04-03 12:13:27 +0200

    Init iqe only if correct localStorage key not set

--- src/js/iqeEnablement.js
+++ src/js/iqeEnablement.js
@@ -1,20 +1,20 @@
 let results = [];
-let initted = false;
 
 function init () {
+    window.localStorage.setItem('iqe:chrome:init', 'true');
     console.log('[iqe] initialized'); // eslint-disable-line no-console
 
     const open = window.XMLHttpRequest.prototype.open;
     const send = window.XMLHttpRequest.prototype.send;
 
     // must use function here because arrows dont "this" like functions
-    window.XMLHttpRequest.prototype.open = function openReplacement(method, url) { // eslint-disable-line func-names
+    window.XMLHttpRequest.prototype.open = function(_method, url) {
         this._url = url;
         return open.apply(this, arguments);
     };
 
     // must use function here because arrows dont "this" like functions
-    window.XMLHttpRequest.prototype.send = function sendReplacement() { // eslint-disable-line func-names
+    window.XMLHttpRequest.prototype.send = function() {
         results.push(this);
         return send.apply(this, arguments);
     };
@@ -22,20 +22,18 @@ function init () {
 
 export default {
     init: () => {
-        if (!initted) {
-            initted = true;
-            if (window.localStorage &&
-                window.localStorage.getItem('iqe:chrome:init') === 'true') {
-                init();
-            }
+        if (window.localStorage.getItem('iqe:chrome:init') !== 'true') {
+            init();
         }
     },
     hasPendingAjax: () => {
-        const removed = results.filter(result => result.readyState === 4);
-        results = results.filter(result => result.readyState !== 4);
-        for (const e of removed) { console.log(`[iqe] complete:   ${e._url}`); } // eslint-disable-line no-console
+        results
+        .filter(result => result.readyState === 4)
+        .forEach(e => console.log(`[iqe] complete:   ${e._url}`)); // eslint-disable-line no-console
 
-        for (const e of results) { console.log(`[iqe] incomplete: ${e._url}`); } // eslint-disable-line no-console
+        results
+        .filter(result => result.readyState !== 4)
+        .forEach(e => console.log(`[iqe] incomplete:   ${e._url}`)); // eslint-disable-line no-console
 
         return results.length > 0;
     }
