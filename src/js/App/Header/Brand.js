9dbfb57291e025ce9fb2f0ecd52e17b19e54580f
Ryan Long <rlong@redhat.com>
2019-04-18 10:51:44 -0400

    fixing styles in header and sidebar

--- src/js/App/Header/Brand.js
+++ src/js/App/Header/Brand.js
@@ -4,22 +4,25 @@ import { onToggle } from '../../redux/actions';
 import { Button } from '@patternfly/react-core/dist/esm/components/Button';
 import BarsIcon from '@patternfly/react-icons/dist/esm/icons/bars-icon';
 import Logo from './Logo';
+import { PageHeader } from '@patternfly/react-core';
 
-const Brand = ({ toggleNav, navHidden }) => (<div className="pf-l-page__header-brand pf-c-page__header-brand">
-    <div hidden={navHidden}>
-        <Button
-            variant="plain"
-            aria-label="Toggle primary navigation"
-            widget-type="InsightsNavToggle"
-            onClick={() => toggleNav && toggleNav()}
-        >
-            <BarsIcon size="md"/>
-        </Button>
+const Brand = ({ toggleNav, navHidden }) => (
+    <div className="pf-c-page__header-brand">
+        <div hidden={navHidden} className='pf-c-page__header-brand-toggle'>
+            <Button
+                variant="plain"
+                aria-label="Toggle primary navigation"
+                widget-type="InsightsNavToggle"
+                onClick={() => toggleNav && toggleNav()}
+            >
+                <BarsIcon size="md"/>
+            </Button>
+        </div>
+        <a className="pf-c-page__header-brand-link" href="./">
+            <Logo />
+        </a>
     </div>
-    <a className="pf-l-page__header-brand-link pf-c-page__header-brand-link" href="./">
-        <Logo />
-    </a>
-</div>);
+);
 
 function mapDispatchToProps(dispatch) {
     return {
