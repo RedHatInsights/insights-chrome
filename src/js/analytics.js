afce759192a2f1391a34d2f2a1aafaacbab5197c
Ryan Long <rlong@redhat.com>
2019-04-23 16:32:52 -0400

    Update Pendo object (#261)
    * updating pendo object
    
    * fixing lang
    
    * updating account_id and visitor
    
    * lint
    
    * adding new dropdown items, remove accountNumber if -1 (#260)
    
    * adding new dropdown items, remove accountNumber if -1
    
    * moving separator
    
    * removing fragment, linting
    
    * Patch to let UHC make offline token requests to KeyCloak not gobble up the data (#256)
    
    * Patch to let UHC make offline token requests to KeyCloack not gobble up the data
    
    * Elads patch
    
    * added tests
    
    * Use insights path directly
    
    * adding a test to check on the pendo config generation
    
    * only disable eslint camelcase on one function

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -6,23 +6,24 @@ function initPendo(pendoConf) {
     window.pendo.initialize(pendoConf);
 }
 
-export default (user) => {
-
-    // eslint-disable-next-line
-    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${API_KEY}/pendo.js`;z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-
-    const pendoConf = {
+function getPendoConf(data) {
+    return {
         visitor: {
-            id: user.id,
-            internal: user.is_internal,
-            lang: user.locale
+            id: data.internal.account_id,
+            internal: data.user.is_internal,
+            lang: data.user.locale
         },
         account: {
             // TODO add in customer name as name:
             // here if/when we get that in the JWT
-            id: user.account_number
+            id: data.account_number
         }
     };
+}
 
-    initPendo(pendoConf);
+export default (data) => {
+    // eslint-disable-next-line
+    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${API_KEY}/pendo.js`;z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+
+    initPendo(getPendoConf(data.identity));
 };
