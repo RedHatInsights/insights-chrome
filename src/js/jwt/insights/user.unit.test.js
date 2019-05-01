52892766b61b3f4df60a7dc0a9a6bf65d645e7e0
Ian Page Hands <iphands@gmail.com>
2019-05-01 13:17:04 -0400

    tests to prove that the tryBounceIfUnentitled works

--- src/js/jwt/insights/user.unit.test.js
+++ src/js/jwt/insights/user.unit.test.js
@@ -1,27 +1,56 @@
-/*global expect, require, test, describe, jest*/
+/*global expect, require, test, describe, jest, beforeEach*/
 jest.mock('./entitlements');
 
 const mockedEntitlements = require('./entitlements');
-const token      = require('../../../../testdata/token.json');
-const userOutput = require('../../../../testdata/user.json');
-const user       = require('./user');
+const token       = require('../../../../testdata/token.json');
+const userOutput  = require('../../../../testdata/user.json');
+const user        = require('./user');
+const replaceMock = jest.fn();
+
+user.__set__('getWindow', () => {
+    return {
+        location: {
+            pathname: '/insights/foo',
+            replace: replaceMock
+        }
+    };
+});
 
 describe('User', () => {
     const buildUser = user.__get__('buildUser');
-    user.__set__('getWindow', () => {
-        return {
-            location: {
-                pathname: '/insights/',
-                replace: () => {}
-            }
-        };
-    });
 
     describe('buildUser', () => {
         test('transforms a token into a User object', () => {
             expect(buildUser(token)).toMatchObject(userOutput);
         });
     });
+
+    /* eslint-disable camelcase */
+    describe('tryBounceIfUnentitled', () => {
+        const tryBounceIfUnentitled = user.__get__('tryBounceIfUnentitled');
+        const ents = {
+            insights: {
+                is_entitled: false
+            }
+        };
+
+        beforeEach(() => {
+            replaceMock.mockReset();
+        });
+
+        test('shouild bounce if unentitled', () => {
+            ents.insights.is_entitled = false;
+            tryBounceIfUnentitled(ents, 'insights');
+            expect(replaceMock).toBeCalledWith('http://localhost/?not_entitled=insights');
+        });
+
+        test('should *not* bounce if entitled', () => {
+            ents.insights.is_entitled = true;
+            tryBounceIfUnentitled(ents, 'insights');
+            expect(replaceMock).not.toBeCalled();
+        });
+    });
+    /* eslint-enable camelcase */
 
     describe('default', () => {
         test('appends the entitlements data onto the user object', () => {
