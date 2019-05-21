c9d1204a50a1ca325c234c0f40a6b2a353112cee
Karel Hala <khala@redhat.com>
2019-05-21 15:58:52 +0200

    Update to newer version of inventory table

--- src/js/externalDependencies.js
+++ src/js/externalDependencies.js
@@ -5,5 +5,5 @@ export let PFReactTable;
 export default function setDependencies({ reactRouterDom, pfReact, pfReactTable }) {
     ReactRouterDOM = reactRouterDom || ReactRouterDOM;
     PFReact = pfReact || PFReact;
-    PFReactTable = pfReactTable || PFReactTable;
+    PFReactTable = pfReactTable || PFReactTable || {};
 }
