ed585f084f5dda4c4051d335ae5051dd39d70197
Asa Price <aprice@redhat.com>
2019-07-16 10:09:53 -0400

    Enhancements/global nav source of truth (#312)
    * Close to working
    
    * refactor/reorg
    
    * Include default property; remove some console logs
    
    * Write out globalnav JSON
    
    * id, not title; remove console log
    
    * very few links break but we're almost there
    
    * Cleanup
    
    * nav object data cleanup
    
    * Reduce unnecessary processing
    
    * suppress id instead of override
    
    * Fixes and cleanup
    
    * Refactoring
    
    * Progress on fetch + redux
    
    * Oops I changed json back to js
    
    * Redux cleanup
    
    * more progress
    
    * Remove reducers; fix some imports; merge from master
    
    * SideNav working; needs caching
    
    * Cache source of truth; fix insights and settings reload issues
    
    * Current app state
    
    * Update with requested changes
    
    * Fix to get dynamic nav working
    
    * Update test, remove obsolete files
    
    * linting

--- src/js/nav/globalNav.js
+++ src/js/nav/globalNav.js
@@ -1,278 +1,49 @@
-export const grouppedNav = {
-    insights: {
-        title: 'Red Hat Insights',
-        routes: [
-            {
-                group: 'insights',
-                id: 'actions',
-                title: 'Overview',
-                default: true
-            },
-            {
-                group: 'insights',
-                id: 'rules',
-                title: 'Rules'
-            },
-            {
-                id: 'inventory',
-                title: 'Inventory'
-            },
-            {
-                id: 'remediations',
-                title: 'Remediations'
-            },
-            {
-                title: 'Settings',
-                disabled: window.location.pathname.indexOf('/beta') === -1,
-                subItems: [
-                    {
-                        id: 'hooks',
-                        title: 'Hooks',
-                        reload: 'settings/hooks'
-                    },
-                    {
-                        id: 'sources',
-                        title: 'Sources',
-                        reload: 'settings/sources'
-                    }
-                ]
-            }
-        ]
-    },
-    rhel: {
-        title: 'Cloud Management Services',
-        routes: [
-            {
-                id: 'dashboard',
-                title: 'Dashboard',
-                default: true
-            },
-            {
-                id: 'vulnerability',
-                title: 'Vulnerability'
-            },
-            {
-                id: 'compliance',
-                title: 'Compliance'
-            },
-            {
-                id: 'drift',
-                title: 'System Comparison'
-            },
-            {
-                id: 'inventory',
-                title: 'Inventory'
-            },
-            {
-                id: 'remediations',
-                title: 'Remediations'
-            }
-
-        ]
-    },
-    openshift: {
-        title: 'Red Hat OpenShift Cluster Manager',
-        routes: [
-            {
-                id: 'clusters',
-                title: 'Clusters',
-                default: true
-            }
-        ]
-    },
-    hybrid: {
-        title: 'Hybrid Cloud Management services',
-        routes: [
-            {
-                id: 'catalog',
-                title: 'Catalog',
-                subItems: [
-                    {
-                        id: 'portfolios',
-                        title: 'Portfolios',
-                        default: true
-                    },
-                    {
-                        id: 'platforms',
-                        title: 'Platforms'
-                    },
-                    {
-                        id: 'orders',
-                        title: 'Orders'
-                    },
-                    {
-                        id: 'approval',
-                        title: 'Approval',
-                        reload: 'catalog/approval'
-                    }
-                ]
-            },
-            {
-                id: 'cost-management',
-                title: 'Cost Management',
-                subItems: [
-                    {
-                        id: '',
-                        title: 'Overview',
-                        default: true
-                    },
-                    {
-                        id: 'ocp-on-aws',
-                        title: 'OpenShift on cloud details'
-                    },
-                    {
-                        id: 'ocp',
-                        title: 'OpenShift details'
-                    },
-                    {
-                        id: 'aws',
-                        title: 'Cloud details'
-                    },
-                    {
-                        id: 'cost-models',
-                        title: 'Cost model details'
-                    }
-                ]
-            },
-            {
-                title: 'Settings',
-                subItems: [
-                    {
-                        id: 'sources',
-                        title: 'Catalog Sources',
-                        reload: 'settings/sources'
-                    },
-                    {
-                        id: 'cost-management-sources',
-                        title: 'Cost Management Sources',
-                        reload: 'cost-management/sources'
-                    },
-                    {
-                        id: 'rbac',
-                        title: 'User Access Management',
-                        reload: 'settings/rbac'
-                    }
-                ]
-            }
-        ]
-    },
-    staging: {
-        title: 'Staging Bundle',
-        routes: [
-            {
-                id: 'migration-analytics',
-                title: 'Migration Analytics',
-                disabled: window.location.pathname.indexOf('/beta') === -1 || window.location.hostname === 'cloud.redhat.com',
-                default: true
-            },
-            {
-                id: 'ruledev',
-                title: 'Home',
-                default: true
-            }
-        ]
-    }
+export let getNavFromConfig = (masterConfig) => {
+    let globalNav = {};
+    // Get the top-level apps from the master config
+    Object.keys(masterConfig).filter(appid => masterConfig[appid].top_level).forEach((appid) => {
+        globalNav[appid] = getAppData(appid, 'routes', masterConfig);
+    });
+    return globalNav;
 };
 
-export default Object.freeze([
-    {
-        id: 'dashboard',
-        title: 'Dashboard'
-    },
-    {
-        id: 'catalog',
-        title: 'Catalog',
-        // nav is built before window.insights.chrome
-        // detect isProd manually here
-        disabled: window.location.hostname === 'cloud.redhat.com',
-        subItems: [
-            {
-                id: 'portfolios',
-                title: 'Portfolios'
-            },
-            {
-                id: 'platforms',
-                title: 'Platforms'
-            },
-            {
-                id: 'orders',
-                title: 'Orders'
+// Returns a list of routes/subItems owned by an app
+function getRoutesForApp(app, masterConfig) {
+    if (app.hasOwnProperty('frontend') && app.frontend.hasOwnProperty('sub_apps')) {
+        let routes = [];
+        app.frontend.sub_apps.forEach((subItem => {
+            let subAppData = getAppData(subItem.id || subItem, 'subItems', masterConfig);
+            if (!subAppData) {
+                subAppData = {
+                    id: subItem.id || '',
+                    title: subItem.title || ''
+                };
             }
-        ]
-    },
-    {
-        id: 'advisor',
-        title: 'Insights',
-        subItems: [
-            {
-                id: 'actions',
-                title: 'Actions',
-                default: true
-            },
-            {
-                id: 'rules',
-                title: 'Rules'
-            }
-        ]
-    },
-    {
-        id: 'vulnerability',
-        title: 'Vulnerability'
-    },
-    {
-        id: 'compliance',
-        title: 'Compliance'
-    },
-    {
-        id: 'cost-management',
-        title: 'Cost Management',
-        subItems: [
-            {
-                id: '',
-                title: 'Overview',
-                default: true
-            },
-            {
-                id: 'ocp',
-                title: 'OpenShift Details'
-            },
-            {
-                id: 'aws',
-                title: 'AWS Details'
-            }
-        ]
-    },
-    {
-        id: 'inventory',
-        title: 'Inventory'
-    },
-    {
-        id: 'topological-inventory',
-        title: 'Sources',
-        disabled: window.location.hostname === 'cloud.redhat.com'
-    },
-    {
-        id: 'remediations',
-        title: 'Remediations'
-    },
-    {
-        id: 'uhc',
-        title: 'UHC',
-        disabled: window.location.hostname === 'cloud.redhat.com'
-    },
-    {
-        id: 'drift',
-        title: 'System Comparison'
-    },
-    {
-        id: 'tower-analytics',
-        title: 'Tower Analytics',
-        disabled: window.location.hostname === 'cloud.redhat.com'
-    },
-    {
-        id: 'migration-analytics',
-        title: 'Migration Analytics',
-        disabled: window.location.pathname.indexOf('/beta') === -1 || window.location.hostname === 'cloud.redhat.com',
-        default: true
+
+            if (subItem.default) {subAppData.default = subItem.default;}
+
+            if (subItem.group) {subAppData.group = subItem.group;}
+
+            routes.push(subAppData);
+        }));
+        return routes;
     }
-]);
+}
+
+// Gets the app's data from the master config, if it exists
+function getAppData(appId, propName, masterConfig) {
+    const app = masterConfig[appId];
+    if (app && app.hasOwnProperty('frontend')) {
+        const routes = getRoutesForApp(app, masterConfig);
+        let appData = {
+            title: app.frontend.title || app.title
+        };
+        if (!app.frontend.suppress_id) {appData.id = appId;}
+
+        if (app.frontend.reload) {appData.reload = app.frontend.reload;}
+
+        if (routes) {appData[propName] = routes;}
+
+        return appData;
+    }
+}
