f187b68f95ce5284dea7ad837185efcb27b126fb
Asa Price <aprice@redhat.com>
2019-07-16 10:09:53 -0400

    Enhancements/global nav source of truth (#312)
    * Close to working
    
    * refactor/reorg
    
    * Include default property; remove some console logs
    
    * Write out globalnav JSON
    
    * id, not title; remove console log
    
    * very few links break but we're almost there
    
    * Cleanup
    
    * nav object data cleanup
    
    * Reduce unnecessary processing
    
    * suppress id instead of override
    
    * Fixes and cleanup
    
    * Refactoring
    
    * Progress on fetch + redux
    
    * Oops I changed json back to js
    
    * Redux cleanup
    
    * more progress
    
    * Remove reducers; fix some imports; merge from master
    
    * SideNav working; needs caching
    
    * Cache source of truth; fix insights and settings reload issues
    
    * Current app state
    
    * Update with requested changes
    
    * Fix to get dynamic nav working
    
    * Update test, remove obsolete files
    
    * linting

--- src/js/redux-config.js
+++ src/js/redux-config.js
@@ -5,19 +5,15 @@ import ReducerRegistry, {
 } from '@redhat-cloud-services/frontend-components-utilities/files/ReducerRegistry';
 import MiddlewareListener from '@redhat-cloud-services/frontend-components-utilities/files/MiddlewareListener';
 
-import options, { grouppedNav } from './nav/globalNav.js';
-
 const basicMiddlewares = [];
 if (process.env.NODE_ENV === 'development') {
     import('redux-logger').then(logger => basicMiddlewares.push(logger.default));
 }
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 export function spinUpStore(middlewares = []) {
     const middlewareListener = new MiddlewareListener();
     const reduxRegistry = new ReducerRegistry(
-        initialState(),
+        { chrome: {} },
         [
             middlewareListener.getMiddleware(),
             ...basicMiddlewares,
@@ -30,21 +26,4 @@ export function spinUpStore(middlewares = []) {
 
     const actions = dispatchActionsToStore(actionTemplates, store);
     return { store, middlewareListener, actions };
-}
-
-function initialState () {
-    const splitted = location.pathname.split('/') ;
-    const active = splitted[1] === 'beta' ? splitted[2] : splitted[1];
-    return {
-        chrome: {
-            ...grouppedNav[active] ? {
-                globalNav: grouppedNav[active].routes,
-                activeTechnology: grouppedNav[active].title,
-                activeLocation: active
-            } : {
-                globalNav: options,
-                activeTechnology: 'Applications'
-            }
-        }
-    };
 }
