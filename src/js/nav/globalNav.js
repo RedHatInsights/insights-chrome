eb9a9bf0fd4c1db6c25c8afbb5cad21131681409
Ryan Felton <rfelton@redhat.com>
2019-07-17 10:51:55 -0400

    Revert "fixed navigation to catalog/approval from outside of catalog app"
    This reverts commit 8931d644c75975f8a9351cfa765db632dc95158e.

--- src/js/nav/globalNav.js
+++ src/js/nav/globalNav.js
@@ -103,7 +103,7 @@ export const grouppedNav = {
                     {
                         id: 'approval',
                         title: 'Approval',
-                        reload: 'approval'
+                        reload: 'catalog/approval'
                     }
                 ]
             },
