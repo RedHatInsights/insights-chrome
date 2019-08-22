3cc658c9a0d0c34023e4e37daf53ed82f82b6749
Ryan Felton <rfelton@redhat.com>
2019-08-22 14:41:08 -0400

    fixed app name in InsightsAbout.js

--- src/js/App/Header/InsightsAbout.js
+++ src/js/App/Header/InsightsAbout.js
@@ -74,7 +74,7 @@ class InsightsAbout extends Component {
                 { name: 'Cost Management', path: 'apps/cost-management/app.info.json', version: 'N/A' },
                 { name: 'Insights', path: 'apps/insights/app.info.json', version: 'N/A' },
                 { name: 'System Comparison', path: 'apps/drift/app.info.json', version: 'N/A' },
-                { name: 'Migrations', path: 'apps/migration-analytics/app.info.json', version: 'N/A' }
+                { name: 'Migration Analytics', path: 'apps/migration-analytics/app.info.json', version: 'N/A' }
             ] },
             showCopyAlert: false,
             showCopyAlertError: false,
