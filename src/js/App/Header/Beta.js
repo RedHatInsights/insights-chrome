fce5284e6039c79e92a2c3e56adb18f056c868ca
Ryan Felton <rfelton@redhat.com>
2019-09-17 16:25:50 -0400

    small fixes/improvements

--- src/js/App/Header/Beta.js
+++ src/js/App/Header/Beta.js
@@ -3,22 +3,24 @@ import PropTypes from 'prop-types';
 import { Tooltip, Button } from '@patternfly/react-core';
 import cookie from 'js-cookie';
 
-
 const Beta = () => (
-    <div className='ins-c-page__beta' style={{ display: window.location.href.includes('/beta') ? 'flex' : 'none' }}>
+    <div className={'ins-c-page__beta ' + (window.location.href.includes('/beta') ? '' : 'ins-c-button_hidden')}>
         <i className='fas fa-flask'></i>
         <Tooltip
             content={
                 <div>This is a beta environment and contains Technology Preview
                  features that are not supported by Red Hat production service-level
                  agreements (SLAs) and might not be functionally complete. Red Hat
-                 recommends not sing them for production. See fore information here.</div>
+                 recommends not using them for production. See fore information here.</div>
             }
         >
             <p>Insights & Cloud Management Services Beta.</p>
         </Tooltip>
         <p><a onClick={ goToStable }> Take me to Stable</a>.</p>
-        <Button variant="primary" id="beta_button" className={{ display: cookie.get('betaDefault') ? 'none' : 'flex' }} onClick={ setBetaDefault }>
+        <Button
+            variant="primary" id="beta_button"
+            className={(cookie.get('betaDefault') ? 'ins-c-button_hidden' : '')}
+            onClick={ setBetaDefault }>
             Set as Default
         </Button>
     </div>
@@ -35,7 +37,7 @@ function goToStable() {
 
 function setBetaDefault() {
     cookie.set('betaDefault', true, { expires: 9999 });
-    document.getElementById("beta_button").style.display = 'none';
+    document.getElementById('beta_button').className += ' ins-c-button_hidden';
 }
 
 //export default Beta;
