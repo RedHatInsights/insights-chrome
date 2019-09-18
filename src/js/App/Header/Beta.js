b8e9d8c8f84b0d0e5f4e879a307ac51d92d504d8
Ryan Felton <rfelton@redhat.com>
2019-09-18 09:26:35 -0400

    moved beta style to beta.scss, switched to conditional rendering

--- src/js/App/Header/Beta.js
+++ src/js/App/Header/Beta.js
@@ -2,29 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Tooltip, Button } from '@patternfly/react-core';
 import cookie from 'js-cookie';
+import "../../../sass/beta.scss"
 
-const Beta = () => (
-    <div className={'ins-c-page__beta ' + (window.location.href.includes('/beta') ? '' : 'ins-c-page_beta-hidden')}>
-        <i className='fas fa-flask'></i>
-        <Tooltip
-            content={
-                <div>This is a beta environment and contains Technology Preview
-                 features that are not supported by Red Hat production service-level
-                 agreements (SLAs) and might not be functionally complete. Red Hat
-                 recommends not using them for production. See fore information here.</div>
-            }
-        >
-            <p>Insights & Cloud Management Services Beta.</p>
-        </Tooltip>
-        <p><a onClick={ goToStable }> Take me to Stable</a>.</p>
-        <Button
-            variant="primary" id="beta_button"
-            className={(cookie.get('betaDefault') ? 'ins-c-button_hidden' : '')}
-            onClick={ setBetaDefault }>
-            Set as Default
-        </Button>
-    </div>
-);
+const Beta = () => {
+    if (window.location.href.includes('/beta')) {
+        return (
+            <div className={'ins-c-page__beta '}>
+                <i className='fas fa-flask'></i>
+                <Tooltip
+                    content={
+                        <div>This is a beta environment and contains Technology Preview
+                        features that are not supported by Red Hat production service-level
+                        agreements (SLAs) and might not be functionally complete. Red Hat
+                        recommends not using them for production.</div>
+                    }
+                >
+                    <p>Insights & Cloud Management Services Beta.</p>
+                </Tooltip>
+                <p><a onClick={ goToStable }> Take me to Stable</a>.</p>
+                {cookie.get('betaDefault') ? null :
+                    <Button
+                        variant="primary" 
+                        id="beta_button"
+                        onClick={ setBetaDefault }>
+                        Set as Default
+                    </Button> 
+                }
+            </div>
+        )
+    } else {
+        return null
+    }
+};
 
 Beta.propTypes = {
     betaHidden: PropTypes.bool
