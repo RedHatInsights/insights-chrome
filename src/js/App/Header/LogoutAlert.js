986d061fb3bc58cce597f2488b2a8a43c909eda0
Yash Nalla <yashnalla98@gmail.com>
2019-09-20 15:30:32 -0400

    WIP: adding inline logout notification

---
+++ src/js/App/Header/LogoutAlert.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Alert, AlertActionCloseButton } from '@patternfly/react-core';
+
+class LogoutAlert extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { alertOneVisible: true };
+        this.hideAlertOne = () => this.setState({ alertOneVisible: false });
+    }
+    render() {
+        const { alertOneVisible } = this.state;
+        return (
+            <React.Fragment>
+                { alertOneVisible && (
+                    <Alert
+                        variant="success"
+                        isInline={ true }
+                        title="Congratualtions you have successfully logged out!"
+                        action={ <AlertActionCloseButton onClose={ this.hideAlertOne } /> }
+                    >
+                    </Alert>
+                ) }
+            </React.Fragment>
+        );
+    }
+}
+
+export default LogoutAlert;
