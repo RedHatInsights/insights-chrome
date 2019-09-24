07cbbc27ae2949a2b879a8456f31916f1eecdc8b
Karel Hala <khala@redhat.com>
2019-09-24 09:53:53 +0200

    Add log messages if url not found in environment

--- src/js/jwt/insights/url.js
+++ src/js/jwt/insights/url.js
@@ -3,11 +3,14 @@ const log = require('../logger')('insights/url.js');
 // Parse through keycloak options routes
 module.exports = (env) => Object.entries(env)
 .reduce((acc, [keys, values]) => {
-    return (values.url.some(url => {
-        if (url === location.hostname) {
-            log(`SSO: ${values.sso}`);
-            log(`ENV: ${keys}`);
-            return true;
-        }
-    }) && values.sso) || acc;
+    const urlFound = values.url.some(url => url === location.hostname);
+    if (urlFound) {
+        log(`SSO: ${values.sso}`);
+        log(`ENV: ${keys}`);
+        return values.sso;
+    } else {
+        log('SSO: url not found, defaulting to QA');
+        log('ENV: qa');
+        return acc;
+    }
 }, 'https://sso.qa.redhat.com/auth');
