def5d7a37c403f30a9c02855262c171e8b667f2b
Yash Nalla <yashnalla98@gmail.com>
2019-10-11 13:47:18 -0400

    adding to index

---
+++ src/js/App/Header/LogoutAlert.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Alert, AlertActionCloseButton } from '@patternfly/react-core';
+import cookie from 'js-cookie';
+
+class LogoutAlert extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            alertOneVisible: cookie.get('justLoggedOut') === 'true',
+            delay: 5000
+        };
+    }
+
+    hideAlertOne = () => {
+        this.setState({ alertOneVisible: false });
+        cookie.set('justLoggedOut', 'false');
+    }
+    componentDidMount() {
+        this.setTimer();
+    }
+
+    setTimer = () => {
+        if (this._timer !== null) {
+            clearTimeout(this._timer);
+        }
+
+        // hide after `delay` milliseconds
+        this._timer = setTimeout(function() {
+            this.setState({ alertOneVisible: false });
+            this._timer = null;
+        }.bind(this), this.state.delay);
+    }
+
+    render() {
+        const { alertOneVisible } = this.state;
+        return (
+            <React.Fragment>
+                { alertOneVisible && (
+                    <Alert
+                        variant="success"
+                        title="Congratualtions you have successfully logged out!"
+                        className="ins-c-alert__logout"
+                        action={ <AlertActionCloseButton onClose={ this.hideAlertOne } /> }
+                    >
+                    </Alert>
+                ) }
+            </React.Fragment>
+        );
+    }
+}
+
+export default LogoutAlert;
