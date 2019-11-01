8195ffa2ac7f28ca17c0d2c7560bd4db2c428421
Yash Nalla <yashnalla98@gmail.com>
2019-11-01 13:02:44 -0400

    mocking store in brand.test.js

--- src/js/App/Header/Brand.test.js
+++ src/js/App/Header/Brand.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Brand from './Brand';
 import { render } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import RootApp from './RootApp';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import { shallow } from 'enzyme';
@@ -11,4 +10,25 @@ import { shallow } from 'enzyme';
 //     shallow(<Brand />);
 // });
 
+describe('Brand', () => {
+    let initialState;
+    let mockStore;
+    beforeEach(() => {
+        mockStore = configureStore();
+        initialState = {
+            chrome: {
+                navHidden: true
+            }
+        };
+    });
 
+    it('should render correctly with initial state', () => {
+        const store = mockStore(initialState);
+        const wrapper = render(
+            <Provider store={store}>
+                <Brand/>
+            </Provider>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+});
