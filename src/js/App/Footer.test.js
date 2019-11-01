1bc98a97cb930f942183c158827c60693ed5addf
Yash Nalla <yashnalla98@gmail.com>
2019-11-01 13:02:44 -0400

    mocking store in brand.test.js

--- src/js/App/Footer.test.js
+++ src/js/App/Footer.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Footer from './Footer';
 
 import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 it('renders without crashing!', () => {
     const wrapper = shallow(<Footer />);
-    const declaration = <div>I am Footer</div>;
-    expect(wrapper.contains(declaration)).toEqual(true);
+    expect(toJson(wrapper)).toMatchSnapshot();
 });
