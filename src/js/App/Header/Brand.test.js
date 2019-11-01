18a650e004ece29dba74e9787655080ca8cd0bee
Yash Nalla <yashnalla98@gmail.com>
2019-11-01 11:11:48 -0400

    remove setupTests

---
+++ src/js/App/Header/Brand.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Brand from './Brand';
+import { render } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import RootApp from './RootApp';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { shallow } from 'enzyme';
+
+// it('renders without crashing!', () => {
+//     shallow(<Brand />);
+// });
+
+
