a4f45c41dcf598db638cb01ba1b74817516bf3ea
Karel Hala <khala@redhat.com>
2019-11-02 20:02:01 +0100

    Enable redux logging by changing localStorage value

--- src/js/debugFunctions.js
+++ src/js/debugFunctions.js
@@ -8,5 +8,6 @@ export default ({
     remediationsDebug: () => functionBuilder('remediations:debug', true),
     invTags: () => functionBuilder('rhcs-tags', true),
     shortSession: () => functionBuilder('chrome:jwt:shortSession', true),
-    jwtDebug: () => functionBuilder('chrome:jwt:debug', true)
+    jwtDebug: () => functionBuilder('chrome:jwt:debug', true),
+    reduxDebug: () => functionBuilder('chrome:redux:debug', true)
 });
