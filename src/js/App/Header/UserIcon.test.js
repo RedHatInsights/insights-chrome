d71eb97351f3991cb43aff77eb39f3432d50d72b
Ryan Long <rlong@redhat.com>
2019-11-12 15:45:34 -0500

    updating test stuff

---
+++ src/js/App/Header/UserIcon.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ConnectedUserIcon, { UserIcon }  from './UserIcon';
+import toJson from 'enzyme-to-json';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { shallow } from 'enzyme';
+
+describe('Connected User Icon', () => {
+    let initialState;
+    let mockStore;
+    beforeEach(() => {
+        mockStore = configureStore();
+        initialState = {
+            chrome: {
+                user: {
+                    identity: {
+                        user: {
+                            username: 'test-user'
+                        }
+                    }
+                }
+            }
+        };
+    });
+
+    it('should render correctly with initial state', () => {
+        const store = mockStore(initialState);
+        const wrapper = shallow(
+            <Provider store={store}>
+                <ConnectedUserIcon/>
+            </Provider>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
+
+describe('User Icon', () => {
+
+    it('should render correctly with initial state', () => {
+        const account = {
+            username: 'test'
+        }
+        const wrapper = shallow(
+            <UserIcon account={account}/>
+        );
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
