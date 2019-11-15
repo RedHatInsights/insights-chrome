b83db4ef79a0f0b27cb0c9059a447262b586d898
Yash Nalla <yashnalla98@gmail.com>
2019-11-15 16:13:19 -0500

    navigation tests

--- src/js/App/Sidenav/Navigation.test.js
+++ src/js/App/Sidenav/Navigation.test.js
@@ -5,22 +5,46 @@ import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 
-// describe('Navigation', () => {
-//     it('should render corectly', () =>{
-//         const wrapper = shallow(<Navigation/>);
-//         expect(toJson(wrapper)).toMatchSnapshot();
-//     });
-// });
+describe('Navigation', () => {
+    const initialProps = {
+        settings: ['someItem', 'someKey'],
+        activeApp: 'someApp',
+        activeLocation: 'someLocation',
+        documentation: 'someDocs'
+    };
+    it('should render corectly with nav not Hidden', () =>{
+        let props = {
+            ...initialProps,
+            navHidden: false
+        };
+        const wrapper = shallow(<Navigation { ...props }/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+    it('should render corectly with nav Hidden', () =>{
+        let props = {
+            ...initialProps,
+            navHidden: true
+        };
+        const wrapper = shallow(<Navigation { ...props }/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
 
 describe('ConnectedNavigation', () => {
     let initialState;
     let mockStore;
+    let globalNavData = require('../../../../testdata/globalNav.json')
 
     beforeEach(() =>{
         mockStore = configureStore();
         initialState = ({
             chrome: {
-                globalNav: 
+                globalNav: globalNavData,
+                activeApp: 'someApp',
+                navHidden: false,
+                activeLocation: 'someLocation',
+                activeGroup: 'someGroup',
+                appId: 'someId'
             }
         });
     });
