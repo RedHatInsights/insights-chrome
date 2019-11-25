084231eed65941154dfc3dcf9f0aad096eddc114
Yash Nalla <yashnalla98@gmail.com>
2019-11-25 16:33:15 -0500

    linting changes

--- config/setupTests.js
+++ config/setupTests.js
@@ -1,7 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'babel-polyfill';
-import { JSDOM } from 'jsdom';
 
 configure({ adapter: new Adapter() });
 global.SVGPathElement = function () {};
@@ -14,10 +13,6 @@ global.MutationObserver = class {
 
 global.fetch = require('jest-fetch-mock');
 global.window = Object.create(window);
-
-// const dom = new JSDOM(`<!DOCTYPE html><aside>Hello world</aside>`);
-// global.document = dom.window.document;
-// global.window = dom.window;
 
 global.window.insights = {
     ...window.insights || {},
--- src/js/nav/globalNav.js
+++ src/js/nav/globalNav.js
@@ -4,9 +4,7 @@ export let getNavFromConfig = (masterConfig) => {
     Object.keys(masterConfig).filter(appid => masterConfig[appid].top_level).forEach((appid) => {
         globalNav[appid] = getAppData(appid, 'routes', masterConfig);
     });
-
     return globalNav;
-
 };
 
 // Returns a list of routes/subItems owned by an app
