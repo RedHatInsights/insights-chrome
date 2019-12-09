52538af30317535957a4937df02311b7c9370b53
Ryan Long <rlong@redhat.com>
2019-12-09 14:52:25 -0500

    lint

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -35,13 +35,13 @@ function shouldInitPendo(data) {
     const environment = window.location.host.split('.')[0];
 
     // Keycloak returns data.account_number as a string
-    const account_number = Number(data.account_number);
-    
+    const accountNumber = Number(data.account_number);
+
     if (environment !== 'cloud' ||
-        account_number < 100 ||
-        account_number === NaN ||
+        accountNumber < 100 ||
+        isNaN(accountNumber) ||
         data.user.is_internal === true ||
-        internalAccounts.includes(account_number)) {
+        internalAccounts.includes(accountNumber)) {
         log('User is internal or this is pre-production, Pendo will not be initialized');
         return false;
     } else {
@@ -72,7 +72,7 @@ function getPendoConf(data) {
 export default (data) => {
 
     // Check to see if user is internal/has an internal account & isProd
-    if(shouldInitPendo(data.identity)) {
+    if (shouldInitPendo(data.identity)) {
         // eslint-disable-next-line
         (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${API_KEY}/pendo.js`;z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
 
