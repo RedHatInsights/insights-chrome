4b07ad8c3b2fd56d183a29b2e1ac889fe988fd5d
Ryan Long <rlong@redhat.com>
2020-01-08 14:32:36 -0500

    add check for nan and low accounts

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,6 +1,22 @@
 'use strict';
 
+const log = require('./jwt/logger')('analytics.js');
+
 const API_KEY = 'bde62396-720d-45b5-546a-e02df377a965';
+
+// Checks account number. Do not init if null or less than 100
+function shouldInitPendo(data) {
+    // Keycloak returns data.account_number as a string
+    const accountNumber = Number(data.account_number);
+
+    if (accountNumber < 100 || isNaN(accountNumber)) {
+        log('Account number is less than 100 or not a number, not initializing Pendo');
+        return false;
+    } else {
+        log('Initializing Pendo');
+        return true;
+    }
+}
 
 // Checks to see if email contains "redhat" string to blacklist in Pendo
 function generateEmail(email, isInternal) {
@@ -37,8 +53,10 @@ function getPendoConf(data) {
 
 export default (data) => {
 
-    // eslint-disable-next-line
-    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${API_KEY}/pendo.js`;z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+    if(shouldInitPendo(data.identity)) {
+        // eslint-disable-next-line
+        (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${API_KEY}/pendo.js`;z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
 
-    initPendo(getPendoConf(data.identity));
+        initPendo(getPendoConf(data.identity));
+    }
 };
