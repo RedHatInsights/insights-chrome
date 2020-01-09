beaac73391d874dd11799ec61afa3dcc13e4e6d6
Ryan Long <rlong@redhat.com>
2020-01-09 14:33:46 -0500

    update tests

--- src/js/analytics.test.js
+++ src/js/analytics.test.js
@@ -1,20 +1,35 @@
 const analytics = require('./analytics');
 const user      = require('./jwt/insights/user');
 const token     = require('../../testdata/token.json');
+const externalToken = require('../../testdata/externalToken.json')
 
 describe('User + Analytics', () => {
     const getPendoConf = analytics.__get__('getPendoConf');
     const buildUser    = user.__get__('buildUser');
-    describe('buildUser + getPendoConf', () => {
-        test('should build a valid Pendo config', () => {
+    describe('buildUser + getPendoConf internal', () => {
+        test('should build a valid internal Pendo config', () => {
             const conf = getPendoConf(buildUser(token).identity);
             expect(conf).toMatchObject({
                 account: {
                     id: '540155'
                 },
                 visitor: {
-                    id: 5299389,
+                    id: '5299389_redhat',
                     internal: true,
+                    lang: 'en_US'
+                }
+            });
+        });
+
+        test('should build a valid external Pendo config', () => {
+            const conf = getPendoConf(buildUser(externalToken).identity);
+            expect(conf).toMatchObject({
+                account: {
+                    id: '540155'
+                },
+                visitor: {
+                    id: '5299389',
+                    internal: false,
                     lang: 'en_US'
                 }
             });
