bff83a3d978b3ad8e91cd1f22ed8d437eb935855
Robin Knaur <rknaur@redhat.com>
2020-01-31 14:05:11 +0100

    Fix chrome cache (#512)
    Co-authored-by: Karel Hala <ch4rlien@gmail.com>

--- src/js/jwt/insights/entitlements.js
+++ src/js/jwt/insights/entitlements.js
@@ -1,13 +1,24 @@
 const axios = require('axios');
 const { ServicesApi } = require('@redhat-cloud-services/entitlements-client');
-const { bootstrapCache } = require('../../utils');
+const { deleteLocalStorageItems, bootstrapCache, lastActive } = require('../../utils');
+
 const BASE_PATH = '/api/entitlements/v1';
 
 module.exports = (cachePrefix) => {
     const cache = bootstrapCache(BASE_PATH, `${cachePrefix}-entitlements`);
 
     const instance = axios.create({ adapter: cache.adapter });
-    instance.interceptors.response.use((response) => response.data || response);
+    instance.interceptors.response.use((response) => {
 
+        if (response && response.request && response.request.fromCache !== true) {
+            const last = lastActive('/api/entitlements/v1/services', 'fallback');
+            const keys = Object.keys(localStorage).filter(key => key.endsWith('/api/entitlements/v1/services') && key !== last);
+
+            deleteLocalStorageItems(keys);
+
+        }
+
+        return response.data || response;
+    });
     return new ServicesApi(undefined, BASE_PATH, instance);
 };
