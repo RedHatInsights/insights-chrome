7c11308c0d071399638f926fff129485e9e08178
Chris Hambridge <chambrid@redhat.com>
2020-02-03 12:19:34 -0500

    Drop OCP 4.3 version to try and default to latest OCP version.

--- src/js/App/Sidenav/index.js
+++ src/js/App/Sidenav/index.js
@@ -10,7 +10,7 @@ const documentationLink = {
     insights: 'https://access.redhat.com/documentation/en-us/red_hat_insights/',
     openshift: 'https://docs.openshift.com/container-platform/latest/',
     subscriptions: 'https://access.redhat.com/documentation/Subscription_Central/',
-    'cost-management': 'https://access.redhat.com/documentation/en-us/openshift_container_platform/4.3/#category-cost-management'
+    'cost-management': 'https://access.redhat.com/documentation/en-us/openshift_container_platform/#category-cost-management'
 };
 
 export class SideNav extends Component {
