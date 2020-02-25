f1043517d8b2320c0c3713d8cf933e5a0b492386
Ryan Long <rlong@redhat.com>
2020-02-25 13:18:41 -0500

    add ability to use pen test cookie

--- src/js/jwt/insights/url.js
+++ src/js/jwt/insights/url.js
@@ -1,10 +1,15 @@
 const log = require('../logger')('insights/url.js');
+import cookie from 'js-cookie';
 
 // Parse through keycloak options routes
 module.exports = (env) => Object.entries(env)
 .reduce((acc, [keys, values]) => {
     const urlFound = values.url.some(url => url === location.hostname);
     if (urlFound) {
+        log(`Found pen test cookie, using QA auth`);
+        if(cookie.get('x-rh-insights-pentest')) {
+            return 'https://sso.qa.redhat.com/auth';
+        };
         log(`SSO: ${values.sso}`);
         log(`ENV: ${keys}`);
         return values.sso;
