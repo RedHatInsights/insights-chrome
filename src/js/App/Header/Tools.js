68697efee99e35c22ca4321a3669f0dc4c7dad48
Ryan Long <rlong@redhat.com>
2020-06-01 12:16:16 -0400

    update masthead again, fix lint

--- src/js/App/Header/Tools.js
+++ src/js/App/Header/Tools.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import { Button } from '@patternfly/react-core4/dist/js/components/Button/Button';
 import { DropdownItem } from '@patternfly/react-core4/dist/js/components/Dropdown/DropdownItem';
+import { PageHeaderTools } from '@patternfly/react-core4/dist/js/components/Page/PageHeaderTools';
+import { PageHeaderToolsGroup } from '@patternfly/react-core4/dist/js/components/Page/PageHeaderToolsGroup';
+import { PageHeaderToolsItem } from '@patternfly/react-core4/dist/js/components/Page/PageHeaderToolsItem';
+
 import QuestionCircleIcon from '@patternfly/react-icons/dist/js/icons/question-circle-icon';
 import CogIcon from '@patternfly/react-icons/dist/js/icons/cog-icon';
 
@@ -9,18 +14,27 @@ import UserIcon from './UserIcon';
 import ToolbarToggle from './ToolbarToggle';
 import InsightsAbout from './InsightsAbout';
 
-import PropTypes from 'prop-types';
-import { PageHeaderTools } from '@patternfly/react-core4/dist/js/components/Page/PageHeaderTools';
-import { PageHeaderToolsGroup } from '@patternfly/react-core4/dist/js/components/Page/PageHeaderToolsGroup';
-import { PageHeaderToolsItem } from '@patternfly/react-core4/dist/js/components/Page/PageHeaderToolsItem';
+const Tools = () => {
 
-const aboutButton = {
-    title: 'FAQ',
-    icon: QuestionCircleIcon,
-    id: 'HelpMenu',
-    className: 'ins-c-toolbar__menu-help',
-    widget: 'HelpMenu',
-    items: [
+    const [isSettingsDisabled, setIsSettingsDisabled] = useState(true);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useEffect(() => {
+        window.insights.chrome.auth.getUser().then((user)=> user.identity.account_number && setIsSettingsDisabled(false));
+    }, []);
+
+    const SettingsButton = () => (
+        <Button variant="plain"
+            aria-label="Go to settings"
+            widget-type='SettingsButton'
+            className='ins-c-toolbar__button-settings'
+            isDisabled={ isSettingsDisabled }
+            onClick={() => window.location.href = `${document.baseURI}settings/rbac/`}>
+            <CogIcon/>
+        </Button>
+    );
+
+    const aboutMenuDropdownItems = [
         {
             title: 'Customer support',
             url: 'https://access.redhat.com/support'
@@ -31,127 +45,63 @@ const aboutButton = {
             title: 'API documentation',
             onClick: () => window.location.href = `${document.baseURI}docs/api`
         }, {
-            title: 'About'
+            title: 'About',
+            onClick: () => setIsModalOpen(true)
         }
-    ]
-};
+    ];
 
-const actions = [
-    aboutButton
-];
-
-const userToggleExtras = [
-    {
-        title: 'Customer support',
-        url: 'https://access.redhat.com/support'
-    }, {
-        title: 'Training',
-        url: 'https://www.redhat.com/en/services/training-and-certification'
-    }, {
-        title: 'API documentation',
-        onClick: () => window.location.href = `${document.baseURI}docs/api`
-    }, {
-        title: 'About',
-        onClick: () => this.onModalToggle
-    }, {
+    const mobileDropdownItems = [...aboutMenuDropdownItems, {
         title: 'Settings',
         onClick: () => window.location.href = `${document.baseURI}settings/rbac/`
-    }
-];
+    }];
 
-const SettingsButton = ({ isDisabled }) => (
-    <Button variant="plain"
-        aria-label="Go to settings"
-        widget-type='SettingsButton'
-        className='ins-c-toolbar__button-settings'
-        isDisabled={ isDisabled }
-        onClick={() => window.location.href = `${document.baseURI}settings/rbac/`}>
-        <CogIcon/>
-    </Button>
-);
+    const AboutButton = () => (
+        <ToolbarToggle
+            key='Help menu'
+            icon={ QuestionCircleIcon }
+            id='HelpMenu'
+            className='ins-c-toolbar__menu-help'
+            widget-type='HelpMenu'
+            dropdownItems={ aboutMenuDropdownItems }
+        />
+    );
 
-SettingsButton.propTypes = {
-    isDisabled: PropTypes.bool
+    return (
+        <PageHeaderTools widget-type="InsightsToolbar">
+
+            {/* Show tools on medium and above screens */}
+            <PageHeaderToolsGroup breakpointMods={[{ modifier: 'hidden' }, { modifier: 'visible', breakpoint: 'sm' }]}>
+                <PageHeaderToolsItem>
+                    { <SettingsButton/> }
+                </PageHeaderToolsItem>
+                <PageHeaderToolsItem>
+                    { <AboutButton/> }
+                </PageHeaderToolsItem>
+            </PageHeaderToolsGroup>
+
+            {/* Show full user dropdown on medium and above screens */}
+            <PageHeaderToolsGroup breakpointMods={[{ modifier: 'hidden' }, { modifier: 'visible', breakpoint: 'sm' }]}>
+                <PageHeaderToolsItem>
+                    <UserToggle className='ins-c-dropdown__user'/>
+                </PageHeaderToolsItem>
+            </PageHeaderToolsGroup>
+
+            {/* Collapse tools and user dropdown to kebab on small screens  */}
+            <PageHeaderToolsGroup breakpointMods={[{ modifier: 'hidden', breakpoint: 'sm' }]}>
+                <PageHeaderToolsItem>
+                    <UserToggle isSmall extraItems={mobileDropdownItems.map((action, key) => (
+                        <DropdownItem key={key} component="button" onClick={action.onClick}>{action.title}</DropdownItem>
+                    ))} />
+                </PageHeaderToolsItem>
+            </PageHeaderToolsGroup>
+
+            {/* User icon always visible */}
+            <UserIcon/>
+
+            {/* Render About Modal */}
+            { isModalOpen && <InsightsAbout isModalOpen={isModalOpen} onClose={() => setIsModalOpen(!isModalOpen)} /> }
+        </PageHeaderTools>
+    );
 };
-
-class Tools extends Component {
-    constructor(props) {
-        super(props);
-        this.onModalToggle = this.onModalToggle.bind(this);
-        aboutButton.items[3].onClick = this.onModalToggle.bind(this);
-        this.state = {
-            isModalOpen: false,
-            isSettingsDisabled: true
-        };
-    }
-
-    componentDidMount() {
-        window.insights.chrome.auth.getUser().then((user)=> user.identity.account_number && this.handleSettingsCog());
-    }
-
-    handleSettingsCog() {
-        this.setState({
-            isSettingsDisabled: false
-        });
-    }
-
-    onModalToggle() {
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        });
-    }
-
-    render() {
-        const { isModalOpen, isSettingsDisabled } = this.state;
-        return (
-            <PageHeaderTools widget-type="InsightsToolbar">
-                {/* This group is for larger than mobile screens */}
-                <PageHeaderToolsGroup breakpointMods={[{ modifier: 'hidden' }, { modifier: 'visible', breakpoint: 'sm' }]}>
-                    <PageHeaderToolsItem>
-                        { <SettingsButton isDisabled={ isSettingsDisabled }/> }
-                    </PageHeaderToolsItem>
-                    <PageHeaderToolsItem>
-                        { actions.map((oneItem, key) => (
-                            oneItem.items
-                                ? <ToolbarToggle
-                                    key={key}
-                                    icon={oneItem.icon}
-                                    id={oneItem.id}
-                                    className={oneItem.className}
-                                    widget-type={oneItem.widget}
-                                    dropdownItems={oneItem.items} />
-                                : <Button
-                                    data-key={key}
-                                    key={key}
-                                    id={oneItem.id}
-                                    className={oneItem.className}
-                                    variant="plain"
-                                    aria-label={`Overflow ${oneItem.title}`}
-                                    widget-type={oneItem.widget}
-                                    onClick={event => oneItem.onClick && oneItem.onClick(event)}>
-                                        <oneItem.icon />
-                                    </Button>
-                        ))}
-                    </PageHeaderToolsItem>
-                </PageHeaderToolsGroup>
-                <PageHeaderToolsGroup>
-                    {/* Show Kebab for user menu on mobile + extras */}
-                    <PageHeaderToolsItem breakpointMods={[{ modifier: 'hidden', breakpoint: 'md' }]}>
-                        <UserToggle isSmall extraItems={userToggleExtras.map((action, key) => (
-                            <DropdownItem key={key} component="button">{action.title}</DropdownItem>
-                        ))} />
-                    </PageHeaderToolsItem>
-
-                    {/* Show regular user dropdown above mobile */}
-                    <PageHeaderToolsItem breakpointMods={[{ modifier: 'hidden' }, { modifier: 'visible', breakpoint: 'md' }]}>
-                        <UserToggle className='ins-c-dropdown__user'/>
-                    </PageHeaderToolsItem>
-                </PageHeaderToolsGroup>
-                <UserIcon/>
-                { isModalOpen && <InsightsAbout isModalOpen={isModalOpen} onClose={this.onModalToggle} /> }
-            </PageHeaderTools>
-        );
-    }
-}
 
 export default Tools;
