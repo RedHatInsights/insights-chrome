a696e76ffc768248179f25270dd94e05111accb2
Ryan Long <rlong@redhat.com>
2020-06-19 09:38:14 -0400

    add entitlements and trials to pendo

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -23,17 +23,24 @@ function getPendoConf(data) {
 
     const accountID = `${data.identity.internal.account_id}${isInternalFlag(data.identity.user.email, data.identity.user.is_internal)}`;
 
+    const entitlements = {};
+    const trials = {};
+
+    Object.entries(data.entitlements).forEach(([key,value])=>{
+        entitlements[`entitlements-${key}`] = value.is_entitled;
+        trials[`entitlements-${key}-trial`] = value.is_trial;
+    })
+
     return {
         visitor: {
             id: accountID,
             internal: data.identity.user.is_internal,
             lang: data.identity.user.locale,
             isOrgAdmin: data.identity.user.is_org_admin,
-            url: {
-                path: window.location.pathname,
-                href: window.location.href
-            },
-            entitlements: data.entitlements
+            url: window.location.href,
+            urlPathname: window.location.pathname,
+            ...entitlements,
+            ...trials
         },
         account: {
             // TODO add in customer name as name:
@@ -47,6 +54,5 @@ export default (data) => {
 
     // eslint-disable-next-line
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${API_KEY}/pendo.js`;z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-    console.debug(getPendoConf(data));
     initPendo(getPendoConf(data));
 };
