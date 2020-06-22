639d190ba57056e46f45b987d6790ac51a00933d
Ryan Long <rlong@redhat.com>
2020-06-22 11:30:22 -0400

    add new data for pendo in prod beta

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -21,19 +21,28 @@ function isInternalFlag(email, isInternal) {
 
 function getPendoConf(data) {
 
-    const accountID = `${data.internal.account_id}${isInternalFlag(data.user.email, data.user.is_internal)}`;
+    const accountID = `${data.identity.internal.account_id}${isInternalFlag(data.identity.user.email, data.identity.user.is_internal)}`;
+
+    const entitlements = {};
+
+    data.entitlements && Object.entries(data.entitlements).forEach(([key, value])=> {
+        entitlements[`entitlements-${key}`] = value.is_trial ? 'trial' : value.is_entitled;
+    });
 
     return {
         visitor: {
             id: accountID,
-            internal: data.user.is_internal,
-            lang: data.user.locale,
-            isOrgAdmin: data.user.is_org_admin
+            internal: data.identity.user.is_internal,
+            lang: data.identity.user.locale,
+            isOrgAdmin: data.identity.user.is_org_admin,
+            url: window.location.href,
+            urlPathname: window.location.pathname,
+            ...entitlements
         },
         account: {
             // TODO add in customer name as name:
             // here if/when we get that in the JWT
-            id: data.account_number
+            id: data.identity.account_number
         }
     };
 }
@@ -43,5 +52,5 @@ export default (data) => {
     // eslint-disable-next-line
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${API_KEY}/pendo.js`;z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
 
-    initPendo(getPendoConf(data.identity));
+    initPendo(getPendoConf(data));
 };
---
+++ src/js/analytics.readme.md
@@ -0,0 +1,37 @@
+# Analytics
+
+## Pendo
+
+### Visitor
+
+`id` : `number (540155)`
+
+`internal` : `bool (true/false)`
+
+`lang` : `string ('en-us')`
+
+`isOrgAdmin` : `bool (true/false)`
+
+`url` : `string ('cloud.redhat.com/insights/dashboard)`
+
+`urlPathname` : `string ('/insights/dashboard)`
+
+`entitlement-ansible` : `oneOf([string, bool]) (true/'trial'/false)` -- depending on SKU
+
+`entitlement-cost_management` : `oneOf[[string, bool]] (true/'trial'/false)` -- depending on SKU
+
+`entitlement-insights` : `oneOf[[string, bool]] (true/'trial'/false)` -- depending on SKU
+
+`entitlement-migrations` : `oneOf[[string, bool]] (true/'trial'/false)` -- depending on SKU
+
+`entitlement-subscriptions` : `oneOf[[string, bool]] (true/'trial'/false)` -- depending on SKU
+
+`entitlement-settings` : `oneOf[[string, bool]] (true/'trial'/false)` -- depending on SKU
+
+`entitlement-openshift` : `oneOf[[string, bool]] (true/'trial'/false)` -- depending on SKU
+
+`entitlement-smart_management` : `oneOf[[string, bool]] (true/'trial'/false)` -- depending on SKU
+
+### Account
+
+`id` : `number (12345678)`
