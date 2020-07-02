b17a445e3db1eb864c359f7d1b3cb231ed981509
Ryan Long <rlong@redhat.com>
2020-07-02 10:43:00 -0400

    pendo config

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const log = require('./jwt/logger')('Analytics.js');
+
 const API_KEY = 'bde62396-720d-45b5-546a-e02df377a965';
 
 function initPendo(pendoConf) {
@@ -19,16 +21,36 @@ function isInternalFlag(email, isInternal) {
     return '';
 }
 
+function getUrl(type) {
+
+    if (window.location.pathname === ('/beta' || '/')) {
+        return 'landing';
+    }
+
+    const sections = window.location.pathname.split('/');
+    if (sections[1] === 'beta') {
+        return type === 'bundle' ? sections[2] : sections[3];
+    }
+
+    return type === 'bundle' ? sections[1] : sections[2];
+}
+
 function getPendoConf(data) {
 
     const accountID = `${data.internal.account_id}${isInternalFlag(data.user.email, data.user.is_internal)}`;
 
+    const currentBundle = getUrl('bundle');
+    const currentApp = getUrl('app');
+
     return {
         visitor: {
             id: accountID,
-            internal: data.user.is_internal,
-            lang: data.user.locale,
-            isOrgAdmin: data.user.is_org_admin
+            internal: data.identity.user.is_internal,
+            lang: data.identity.user.locale,
+            isOrgAdmin: data.identity.user.is_org_admin,
+            currentBundle: currentBundle,
+            currentApp: currentApp,
+            ...entitlements
         },
         account: {
             // TODO add in customer name as name:
@@ -43,5 +65,10 @@ export default (data) => {
     // eslint-disable-next-line
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${API_KEY}/pendo.js`;z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
 
-    initPendo(getPendoConf(data.identity));
+    try {
+        initPendo(getPendoConf(data));
+        log('Pendo initialized');
+    } catch {
+        log('Pendo init failed');
+    }
 };
