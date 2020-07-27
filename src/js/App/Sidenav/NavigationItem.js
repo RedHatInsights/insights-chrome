aae6038d5dc9d30d6d11d5bc790c2c005904adcd
Ryan Long <rlong@redhat.com>
2020-07-27 15:02:06 -0400

    add ability to change casing in nav item

--- src/js/App/Sidenav/NavigationItem.js
+++ src/js/App/Sidenav/NavigationItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavItem } from '@patternfly/react-core/dist/js/components/Nav/NavItem';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 const basepath = document.baseURI;
 
-const NavigationItem = ({ itemID, title, parent, navigate, ...props }) => (
+const NavigationItem = ({ itemID, title, parent, navigate, ignoreCase, ...props }) => (
     <NavItem
         {...props}
         itemId={itemID}
+        className={classNames({ 'ins-m-ignore-case': ignoreCase })}
         preventDefault
         to={navigate || `${basepath}${parent}/${itemID}`}
     >
@@ -19,7 +21,8 @@ NavigationItem.propTypes = {
     itemID: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     title: PropTypes.node,
     navigate: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    parent: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+    parent: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    ignoreCase: PropTypes.bool
 };
 
 NavigationItem.defaultProps = {
