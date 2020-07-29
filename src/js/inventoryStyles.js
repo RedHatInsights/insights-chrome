3dddf5a2d831d96ae2c46da77ca919bfd02d0e81
Karel Hala <khala@redhat.com>
2020-07-29 13:19:44 +0200

    Pass list of applications and render them if tab clicked (#856)
    * Pass list of applications and render them if tab clicked
    
    * Update snapshots
    
    * Final styling of each tab and added proper loading component
    
    * Add account number list and enable all details for them
    
    * Add all details setter to debug functions
    
    * Remove console logs

--- src/js/inventoryStyles.js
+++ src/js/inventoryStyles.js
@@ -1,2 +1,14 @@
+import '@patternfly/patternfly/components/Toolbar/toolbar.css';
+import '@patternfly/patternfly/components/Pagination/pagination.css';
+import '@patternfly/patternfly/components/Table/table.css';
+import '@patternfly/patternfly/components/Chip/chip.css';
+import '@patternfly/patternfly/components/Tabs/tabs.css';
+import '@patternfly/patternfly/components/ChipGroup/chip-group.css';
+import '@redhat-cloud-services/frontend-components/index.css';
 import '@redhat-cloud-services/frontend-components-inventory/index.css';
+import '@redhat-cloud-services/frontend-components-inventory-general-info/index.css';
+import '@redhat-cloud-services/frontend-components-inventory-insights/index.css';
+import '@redhat-cloud-services/frontend-components-inventory-vulnerabilities/dist/index.css';
+import '@redhat-cloud-services/frontend-components/components/conditional-filter.scss';
+import '@redhat-cloud-services/frontend-components/components/TableToolbar.scss';
 import '../sass/inventory.scss';
