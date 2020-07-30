4ed56f61a2293ea1a22271e3e5e4a0803ee923c6
Filip Hlavac <50696716+fhlavac@users.noreply.github.com>
2020-07-30 16:26:27 +0200

    Replace NoAccess with frontend component (#902)
    * use frontend component for noAccess
    * add loading state
    
    Co-authored-by: Karel Hala <khala@redhat.com>

--- src/js/App/NoAccess.js
+++ src/js/App/NoAccess.js
@@ -1,51 +1,20 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { Title } from '@patternfly/react-core/dist/js/components/Title/Title';
-import { Button } from '@patternfly/react-core/dist/js/components/Button/Button';
-import { EmptyState, EmptyStateVariant } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyState';
-import { EmptyStateIcon } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateIcon';
-import { EmptyStateBody } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateBody';
+import { NotAuthorized } from '@redhat-cloud-services/frontend-components/components/NotAuthorized';
+import { Spinner } from '@redhat-cloud-services/frontend-components/components/Spinner';
+import { Bullseye } from '@patternfly/react-core/dist/js/layouts/Bullseye';
 
-import LockIcon from '@patternfly/react-icons/dist/js/icons/lock-icon';
+const NoAccess = ({ activeAppTitle }) => (
+    activeAppTitle ?
+        <NotAuthorized serviceName={activeAppTitle}/>
+        :
+        <Bullseye>
+            <Spinner centered />
+        </Bullseye>
 
-const NoAccess = ({ activeAppTitle }) => {
-    return (
-        <Fragment>
-            <section
-                className="pf-m-light pf-c-page-header pf-c-page__main-section pf-m-light"
-                widget-type="InsightsPageHeader">
-                <div className="pf-c-content">
-                    <h1
-                        className="pf-c-title pf-m-2xl ins-l-page__header"
-                        widget-type='InsightsPageHeaderTitle'
-                    >
-                        <div className="apptitle">
-                            { `${activeAppTitle}` }
-                        </div>
-                    </h1>
-                </div>
-            </section>
-            <section className="pf-c-page__no-access">
-                <EmptyState variant={ EmptyStateVariant.full }>
-                    <EmptyStateIcon icon={ LockIcon } />
-                    <Title headingLevel="h5" size="lg">
-                        { `You do not have access to ${ activeAppTitle }` }
-                    </Title>
-                    <EmptyStateBody>
-                        Contact your organization administrator(s) for more information.
-                    </EmptyStateBody>
-                    {
-                        document.referrer ?
-                            <Button variant="primary" onClick={ () => history.back() }>Return to previous page</Button> :
-                            <Button variant="primary" component="a" href=".">Go to landing page</Button>
-                    }
-                </EmptyState>
-            </section>
-        </Fragment>
-    );
-};
+);
 
 NoAccess.propTypes = {
     activeAppTitle: PropTypes.string
