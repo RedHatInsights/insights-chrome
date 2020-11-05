97b5fde9ed9586deb6a4f3ad53a9d89421ca5572
Ryan Long <rlong@redhat.com>
2020-11-05 14:08:40 -0500

    lint

--- src/js/jwt/insights/url.js
+++ src/js/jwt/insights/url.js
@@ -5,13 +5,13 @@ const log = logger('insights/url.js');
 export default (env) => {
   const ssoEnv = Object.entries(env).find(([, { url }]) => url.includes(location.hostname));
 
-  if(ssoEnv) {
+  if (ssoEnv) {
     log(`SSO Url: ${ssoEnv?.[1].sso}`);
     log(`Current env: ${ssoEnv?.[0]}`);
-    return ssoEnv?.[1].sso
+    return ssoEnv?.[1].sso;
   } else {
     log('SSO url: not found, defaulting to qa');
     log('Current env: not found, defaultint to qa');
-    return 'https://sso.qa.redhat.com/auth'
+    return 'https://sso.qa.redhat.com/auth';
   }
 };
