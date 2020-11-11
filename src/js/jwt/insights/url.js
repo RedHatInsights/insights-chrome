82014b525d304ff0e44ef609911d8f44181f65d5
Brandon Squizzato <bsquizza@redhat.com>
2020-11-11 12:56:55 -0500

    Support an env var for setting SSO URL

--- src/js/jwt/insights/url.js
+++ src/js/jwt/insights/url.js
@@ -5,7 +5,10 @@ const log = logger('insights/url.js');
 export default (env) => {
   const ssoEnv = Object.entries(env).find(([, { url }]) => url.includes(location.hostname));
 
-  if (ssoEnv) {
+  if (process.env.SSO_URL) {
+    log(`SSO url set via env: ${process.env.SSO_URL}`);
+    return process.env.SSO_URL;
+  } else if (ssoEnv) {
     log(`SSO Url: ${ssoEnv?.[1].sso}`);
     log(`Current env: ${ssoEnv?.[0]}`);
     return ssoEnv?.[1].sso;
