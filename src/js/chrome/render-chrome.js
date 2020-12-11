13325d0c3270dad7a7411bfe3e05aeacc5b688c7
Karel Hala <khala@redhat.com>
2020-12-11 12:50:02 +0100

    Add docs on how to migrate (#1092)
    * Add docs on how to migrate
    
    * Add section about moduleName

--- src/js/chrome/render-chrome.js
+++ src/js/chrome/render-chrome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { spinUpStore } from '../redux-config';
 import RootApp from '../App/RootApp';
 
@@ -14,20 +14,31 @@ window.insights.loadInventory = loadInventory;
 window.insights.experimental.loadRemediations = loadRemediations;
 
 const App = () => {
-  const config = {
-    advisor: {
-      name: 'advisor',
-      manifestLocation: `${window.location.origin}/apps/advisor/fed-mods.json`,
+  const globalNav = useSelector(({ chrome }) => chrome?.globalNav);
+
+  const config = globalNav?.reduce(
+    (acc, curr) => {
+      if (curr?.module) {
+        const appName = curr.module?.appName || curr.id;
+        return {
+          ...acc,
+          [appName]: {
+            name: appName,
+            manifestLocation: `${window.location.origin}${curr.module?.manifest || `/apps/${appName}/fed-mods.json`}`,
+          },
+        };
+      }
+
+      return acc;
     },
-    inventory: {
-      name: 'inventory',
-      manifestLocation: `${window.location.origin}/apps/inventory/fed-mods.json`,
-    },
-    chrome: {
-      name: 'chrome',
-      manifestLocation: `${window.location.origin}/apps/chrome/js/fed-mods.json`,
-    },
-  };
+    {
+      chrome: {
+        name: 'chrome',
+        manifestLocation: `${window.location.origin}/apps/chrome/js/fed-mods.json`,
+      },
+    }
+  );
+
   return <RootApp config={config} />;
 };
 
