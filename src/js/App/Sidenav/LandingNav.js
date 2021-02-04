9aa6b4e1f02354cd6803b3b0666ead888c563b08
Martin Maroši <marvusm.mmi@gmail.com>
2021-02-04 08:57:01 +0100

    Remove navigation from login page. (#1154)

--- src/js/App/Sidenav/LandingNav.js
+++ src/js/App/Sidenav/LandingNav.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Nav, NavExpandable, NavItem, NavList } from '@patternfly/react-core';
 import useGlobalNav from '../../utils/useGlobalNav';
 import { isBeta } from '../../utils';
 import NavLoader from './Loader';
 import './LandingNav.scss';
+import { useSelector } from 'react-redux';
 
 const LandingNav = () => {
-  const { apps } = useGlobalNav();
+  const [elementReady, setElementReady] = useState(false);
+  const showNav = useSelector(({ chrome: { user } }) => !!user);
+  useEffect(() => {
+    if (showNav) {
+      const elem = document.querySelector('aside#ins-c-landing-nav');
+      /**
+       * Nav classes have to be added at runtime only when the nav should be rendered
+       * to prevent navigation background to be displayed in non ci-beta envs.
+       */
+      elem.classList.add('pf-m-dark', 'pf-c-page__sidebar');
+      setElementReady(true);
+    }
+  }, [showNav]);
+  const { apps, isLoaded } = useGlobalNav();
   const isBetaEnv = isBeta();
 
+  /**
+   * render navigation only if the user is logged in
+   */
+  if (!showNav || !elementReady) {
+    return null;
+  }
   return (
-    <Nav className="pf-m-dark pf-c-page__sidebar ins-c-landing-nav">
-      {apps.length === 0 ? (
+    <Nav className="ins-c-landing-nav">
+      {!isLoaded ? (
         <NavLoader />
       ) : (
         <NavList>
