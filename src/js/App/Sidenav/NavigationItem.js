b4712ce596a4e3db52e6e693e6cb6d5d43ba3368
Ryan Long <rlong@redhat.com>
2021-02-05 09:25:10 -0500

    lint

--- src/js/App/Sidenav/NavigationItem.js
+++ src/js/App/Sidenav/NavigationItem.js
@@ -6,13 +6,7 @@ import { titleCase } from 'title-case';
 const basepath = document.baseURI;
 
 const NavigationItem = ({ itemID, title, parent, navigate, ignoreCase, className, ...props }) => (
-  <NavItem
-    {...props}
-    itemId={itemID}
-    className={className}
-    preventDefault
-    to={navigate || `${basepath}${parent}/${itemID}`}
-  >
+  <NavItem {...props} itemId={itemID} className={className} preventDefault to={navigate || `${basepath}${parent}/${itemID}`}>
     {typeof title === 'string' && !ignoreCase ? titleCase(title) : title}
   </NavItem>
 );
