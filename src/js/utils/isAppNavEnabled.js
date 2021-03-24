a6bad5f92ead9c0b7d90ea7b92eac3df973d8673
john-dupuy <44065123+john-dupuy@users.noreply.github.com>
2021-03-24 03:30:22 -0400

    Enable the app filter in stage beta (#1283)

--- src/js/utils/isAppNavEnabled.js
+++ src/js/utils/isAppNavEnabled.js
@@ -1,5 +1,6 @@
 import { getEnv, isBeta } from '../utils';
 
-export const isFilterEnabled = localStorage.getItem('chrome:experimental:app-filter') === 'true' || (['ci', 'qa'].includes(getEnv()) && isBeta());
+export const isFilterEnabled =
+  localStorage.getItem('chrome:experimental:app-filter') === 'true' || (['ci', 'qa', 'stage'].includes(getEnv()) && isBeta());
 
 export const isContextSwitcherEnabled = localStorage.getItem('chrome:experimental:context-switcher') === 'true' || (getEnv() === 'ci' && isBeta());
