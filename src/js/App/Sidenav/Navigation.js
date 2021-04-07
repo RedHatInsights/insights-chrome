6b3c53415d1c6e99166a0da6f45f53a8c7761477
Pete Muir <pmuir@bleepbleep.org.uk>
2021-04-07 18:52:10 +0100

    fix: remove top level Documentation link for app services (#1313)
    * fix: remove top level Documentation link for app services
    
    * Update src/js/App/Sidenav/Navigation.js
    
    Co-authored-by: Karel Hala <ch4rlien@gmail.com>

--- src/js/App/Sidenav/Navigation.js
+++ src/js/App/Sidenav/Navigation.js
@@ -78,14 +78,6 @@ const extraLinks = {
       external: true,
     },
   ],
-  'application-services': [
-    {
-      id: 'extra-application-services-docs',
-      url: 'https://access.redhat.com/documentation/en-us/red_hat_openshift_streams_for_apache_kafka',
-      title: 'Documentation',
-      external: true,
-    },
-  ],
 };
 
 const NavItemLink = ({ id, title, external, url, link }) => (
