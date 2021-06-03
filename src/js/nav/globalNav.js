1f60295aafaa67d1d734a9d1b3466c1950db02fd
Karel Hala <khala@redhat.com>
2021-06-03 12:26:19 +0200

    Ignore subitems for empty app of ansible (#1452)

--- src/js/nav/globalNav.js
+++ src/js/nav/globalNav.js
@@ -155,7 +155,7 @@ export async function loadNav(yamlConfig, cache) {
           activeLocation: active,
           activeSection:
             globalNav?.find?.(({ id }) => id === section) ||
-            globalNav?.find?.(({ subItems } = {}) => active === 'ansible' && subItems?.find(({ id } = {}) => id === section)) ||
+            globalNav?.find?.(({ subItems } = {}) => active === 'ansible' && section && subItems?.find(({ id } = {}) => id === section)) ||
             globalNav?.find?.(({ default: isDefault }) => isDefault),
         }
       : {
