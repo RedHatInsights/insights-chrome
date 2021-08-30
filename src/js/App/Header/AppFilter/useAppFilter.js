79b9d8666f19e5f93ce805d03dface7c9b3ddf11
Martin Maroši <marvusm.mmi@gmail.com>
2021-08-30 10:17:22 +0200

    Fix app filter items order with async check.

--- src/js/App/Header/AppFilter/useAppFilter.js
+++ src/js/App/Header/AppFilter/useAppFilter.js
@@ -86,6 +86,7 @@ const useAppFilter = () => {
       subs: [],
     };
     const promises = links.map(async ({ costLinks, subscriptionsLinks, ...rest }) => {
+      const nextIndex = bundleLinks.length;
       if (costLinks.length > 0) {
         extraLinks.cost = await costLinks.filter(evaluateVisibility);
       }
@@ -96,9 +97,14 @@ const useAppFilter = () => {
       if (rest.filterable !== true && (subscriptionsLinks.length > 0 || costLinks.length > 0)) {
         return;
       }
+
+      /**
+       * We have to create a placeholder for the link item, in order to preserver the links order
+       */
+      bundleLinks.push({ ...rest, isHidden: true });
       const link = await evaluateVisibility(rest);
 
-      bundleLinks.push(link);
+      bundleLinks[nextIndex] = link;
     });
     await Promise.all(promises);
 
