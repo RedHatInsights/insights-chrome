5165e852947c1268de3c55dfe32ffafc8d57347d
Ryan Long <rlong@redhat.com>
2021-09-30 10:51:27 -0400

    lint

--- src/js/jwt/insights/user.js
+++ src/js/jwt/insights/user.js
@@ -25,7 +25,7 @@ function buildUser(token) {
         identity: {
           account_number: token.account_number,
           type: token.type,
-          ...(token.idp && {idp: token.idp}),
+          ...(token.idp && { idp: token.idp }),
           user: {
             username: token.username,
             email: token.email,
