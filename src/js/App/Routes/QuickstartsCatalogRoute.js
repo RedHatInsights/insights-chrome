e6768bfe633fb5cbf3d4f020e23d8a3abf8935ff
Martin Maroši <marvusm.mmi@gmail.com>
2021-10-07 11:47:56 +0200

    FIx linter errors.

--- src/js/App/Routes/QuickstartsCatalogRoute.js
+++ src/js/App/Routes/QuickstartsCatalogRoute.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { getUrl } from '../../utils';
 import QuickStartCatalog from '../QuickStart/QuickStartCatalog';
 
