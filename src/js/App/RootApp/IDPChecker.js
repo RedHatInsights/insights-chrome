344570879966015bb4f1497431fa3b204a267333
Martin Maroši <marvusm.mmi@gmail.com>
2021-10-19 10:39:01 +0200

    Check fed ramp env API response for unauthenticated call.

---
+++ src/js/App/RootApp/IDPChecker.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+import { isFedRamp } from '../../utils';
+import IDPError from '../IDPError/IDPError';
+
+const isFedrampEnv = isFedRamp();
+
+const IDPStatuses = {
+  OK: 'OK',
+  UNKNOWN: 'UNKNOWN',
+  PENDING: 'PENDING',
+  ERROR: 'ERROR',
+};
+
+const IDPChecker = ({ children }) => {
+  const missingIDP = useSelector(({ chrome }) => chrome?.missingIDP);
+  const [status, setStatus] = useState(() => {
+    if (isFedrampEnv) {
+      return missingIDP === true ? IDPStatuses.ERROR : IDPStatuses.UNKNOWN;
+    }
+    return IDPStatuses.OK;
+  });
+  const hasUser = useSelector(({ chrome: { user } }) => Object.keys(user || {}).length > 0);
+  const allowStateChange = useRef(isFedrampEnv);
+
+  useEffect(() => {
+    if (isFedrampEnv && status !== IDPStatuses.PENDING && hasUser) {
+      allowStateChange.current && setStatus(IDPStatuses.PENDING);
+      axios
+        .get('/api/entitlements/v1/services')
+        .then(() => {
+          allowStateChange.current && setStatus(IDPStatuses.OK);
+        })
+        .catch((err) => {
+          const authError = err.response.status === 401 || err.response.status === 403 || err.response.status === 404;
+          allowStateChange.current && setStatus(authError ? IDPStatuses.ERROR : IDPStatuses.OK);
+        });
+    }
+  }, [hasUser, missingIDP]);
+
+  useEffect(() => {
+    if (missingIDP === true) {
+      allowStateChange.current && setStatus(IDPStatuses.ERROR);
+      allowStateChange.current = false;
+    }
+  }, [missingIDP]);
+
+  if (status === IDPStatuses.OK) {
+    return children;
+  }
+  if (status === IDPStatuses.ERROR) {
+    return <IDPError />;
+  }
+  return null;
+};
+
+IDPChecker.propTypes = {
+  children: PropTypes.node,
+};
+
+export default IDPChecker;
--- src/js/App/RootApp/RootApp.js
+++ src/js/App/RootApp/RootApp.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { isBeta } from '../../utils';
 import chromeHistory from '../../utils/chromeHistory';
 import CrossRequestNotifier from '../CrossRequestNotifier';
+import IDPChecker from './IDPChecker';
 import ScalprumRoot from './ScalprumRoot';
-
-const IDPChecker = ({ children }) => {
-  const missingIDP = useSelector(({ chrome }) => chrome?.missingIDP);
-  if (missingIDP === true) {
-    return 'No IDP';
-  }
-  return children;
-};
 
 const RootApp = (props) => (
   <Router history={chromeHistory} basename={isBeta() ? '/beta' : '/'}>
