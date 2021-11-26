cb5f09491a432140faf2d8b01513d73030931b74
Karel Hala <khala@redhat.com>
2021-11-26 14:33:50 +0100

    Change how the dynamic nav partial is calculated to use hooks

--- src/js/App/Sidenav/Navigation/DynamicNav.js
+++ src/js/App/Sidenav/Navigation/DynamicNav.js
@@ -1,63 +1,79 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment, useState } from 'react';
 import { useLoadModule } from '@scalprum/react-core';
 import PropTypes from 'prop-types';
 import { Skeleton, SkeletonSize } from '@redhat-cloud-services/frontend-components/Skeleton';
 import { NavItem } from '@patternfly/react-core';
 import { useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
+import isEqual from 'lodash/isEqual';
+import ChromeNavItem from './ChromeNavItem';
 import { loadLeftNavSegment } from '../../../redux/actions';
 
-const DynamicNav = ({ dynamicNav }) => {
-  const {
-    location: { pathname },
-  } = useHistory();
-  const dispatch = useDispatch();
-  const [appName] = dynamicNav.split('/');
+const toArray = (value) => (Array.isArray(value) ? value : [value]);
+const mergeArrays = (orig, index, value) => [...orig.slice(0, index), ...toArray(value), ...orig.slice(index)];
+
+const HookedNavigation = ({ useNavigation, dynamicNav, pathname, ...props }) => {
   const currentNamespace = pathname.split('/')[1];
+  const [isLoaded, setIsLoaded] = useState(false);
+  const dispatch = useDispatch();
   const schema = useSelector(({ chrome: { navigation } }) => navigation[currentNamespace]);
-  const [{ default: navigation }] = useLoadModule({ appName, scope: appName, module: './Navigation' }, {});
+  const currNav = useSelector(({ chrome: { navigation } }) =>
+    navigation[currentNamespace]?.navItems?.filter((item) => item.dynamicNav === dynamicNav)
+  );
+  const newNav = useNavigation({ schema, dynamicNav, currentNamespace, currNav });
   useEffect(() => {
-    if (navigation) {
-      if (typeof navigation === 'function') {
-        const indexOfDynamicNav = schema.navItems.findIndex((nav) => nav.dynamicNav === dynamicNav);
-        if (indexOfDynamicNav !== -1) {
-          const { dynamicNav: _dynamicNav, ...originalNav } = schema.navItems[indexOfDynamicNav];
-          Promise.resolve(navigation({ schema, dynamicNav, currentNamespace })).then((data) => {
-            const newValue = Array.isArray(data)
-              ? data.map((item) => ({
-                  appId: dynamicNav.split('/')[0],
-                  ...originalNav,
-                  ...item,
-                }))
-              : [
-                  {
-                    ...originalNav,
-                    ...data,
-                  },
-                ];
-            dispatch(
-              loadLeftNavSegment(
-                {
-                  ...schema,
-                  navItems: schema.navItems.flatMap((item, key) => (key === indexOfDynamicNav ? newValue : item)),
-                },
-                currentNamespace,
-                pathname,
-                true
-              )
-            );
-          });
-        }
+    if (newNav?.length > 0) {
+      const newValue = toArray(newNav).map((item, key) => ({
+        appId: dynamicNav.split('/')[0],
+        ...(currNav?.[key] || currNav?.[0]),
+        ...item,
+      }));
+      if (!isEqual(newValue, currNav)) {
+        const currNavIndex = schema.navItems.findIndex((item) => item.dynamicNav === dynamicNav);
+        dispatch(
+          loadLeftNavSegment(
+            {
+              ...schema,
+              navItems: mergeArrays(
+                schema.navItems.filter((item) => !(item.dynamicNav && item.dynamicNav === dynamicNav)),
+                currNavIndex,
+                newValue
+              ),
+            },
+            currentNamespace,
+            pathname,
+            true
+          )
+        );
       }
+      setIsLoaded(true);
     }
-  }, [navigation]);
-  return (
+  }, [JSON.stringify(newNav)]);
+
+  return isLoaded ? (
+    <ChromeNavItem {...props} />
+  ) : (
     <NavItem preventDefault>
       <a href="#">
         <Skeleton size={SkeletonSize.lg} isDark />
       </a>
     </NavItem>
   );
+};
+
+HookedNavigation.propTypes = {
+  useNavigation: PropTypes.func.isRequired,
+  dynamicNav: PropTypes.string.isRequired,
+  pathname: PropTypes.string.isRequired,
+};
+
+const DynamicNav = ({ dynamicNav, ...props }) => {
+  const {
+    location: { pathname },
+  } = useHistory();
+  const [appName] = dynamicNav.split('/');
+  const [{ useNavigation }] = useLoadModule({ appName, scope: appName, module: './Navigation' }, {});
+  return useNavigation ? <HookedNavigation {...props} dynamicNav={dynamicNav} useNavigation={useNavigation} pathname={pathname} /> : <Fragment />;
 };
 
 DynamicNav.propTypes = {
