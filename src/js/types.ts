e159dc1230726d2e142ec805f69cd1b166e463dc
Martin Maroši <marvusm.mmi@gmail.com>
2022-03-03 14:53:01 +0100

    Migrate JS utils to TS.

--- src/js/types.ts
+++ src/js/types.ts
@@ -5,3 +5,13 @@
 export type AnyObject = {
   [key: string]: any;
 };
+
+/**
+ * TODO: Move to the component once it is migrated to TS
+ */
+export type NavItem = {
+  href?: string;
+  routes?: NavItem[];
+  navItems?: NavItem[];
+  active?: boolean;
+};
