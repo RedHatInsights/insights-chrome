e91bf96e9ddeef7f71db117f1f2fc5dd7118b6a3
Martin Maroši <marvusm.mmi@gmail.com>
2022-06-07 12:42:22 +0200

    Preserve sso-url.js output chunk name.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,7 +29,12 @@ const commonConfig = ({ dev }) => {
       path: path.resolve(__dirname, '../build/js'),
       filename: 'chrome-root.[fullhash].js',
       publicPath,
-      chunkFilename: '[name].[fullhash].js',
+      chunkFilename: ({ chunk }) => {
+        /**
+         * The sso-url.js chunk is required by ephemeral env config map
+         */
+        return chunk.name === 'sso-url' ? '[name].js' : `[name].[fullhash].js`;
+      },
     },
     devtool: false,
     resolve: {
