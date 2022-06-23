a8a0455587725b1500228a57db9dd47b05a60d4c
Ryan Long <rlong@redhat.com>
2022-06-23 10:00:56 -0400

    adding more urls

--- src/js/App/Header/Header.js
+++ src/js/App/Header/Header.js
@@ -13,6 +13,7 @@ import { useSelector } from 'react-redux';
 import Logo from './Logo';
 import ChromeLink from '../Sidenav/Navigation/ChromeLink';
 import { Route } from 'react-router-dom';
+import { activationRequestURLs } from '../../consts';
 
 import './Header.scss';
 
@@ -30,9 +31,8 @@ const FeedbackRoute = ({ user }) => {
 
 export const Header = () => {
   const user = useSelector(({ chrome }) => chrome?.user);
-  const search = new URLSearchParams(window.location.search);
-  const isActivationPath =
-    search.has('azure-openshift-activation') || search.has('aws-openshift-activation') || search.has('azure-ansible-activation');
+  const search = new URLSearchParams(window.location.search).keys().next().value;
+  const isActivationPath = activationRequestURLs.includes(search.toString());
 
   return (
     <Fragment>
@@ -43,7 +43,7 @@ export const Header = () => {
       </MastheadMain>
       <MastheadContent>
         {user && ReactDOM.createPortal(<FeedbackRoute user={user} />, document.body)}
-        {user && isActivationPath && <Activation user={user} request={search.keys().next().value} />}
+        {user && isActivationPath && <Activation user={user} request={search} />}
         <Toolbar isFullHeight>
           <ToolbarContent>
             <ToolbarGroup variant="filter-group">
