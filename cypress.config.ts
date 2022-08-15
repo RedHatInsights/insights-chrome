a7a50241c9bd20f3e4c943d4d6edfe1d25d62958
Martin Maroši <marvusm.mmi@gmail.com>
2022-08-15 16:21:30 +0200

    Add cypress browser dimensions in headless mode.

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,6 +9,20 @@ export default defineConfig({
     excludeSpecPattern: ["/snapshots/*", "/image_snapshots/*", "/src/*"],
     setupNodeEvents(on, config) {
       addMatchImageSnapshotPlugin(on, config);
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+          launchOptions.args.push('--window-size=1280,720')
+      
+          // force screen to be non-retina
+          launchOptions.args.push('--force-device-scale-factor=1')
+        }
+      
+        if (browser.name === 'electron' && browser.isHeadless) {
+          // fullPage screenshot size is 1280x720
+          launchOptions.preferences.width = 1280
+          launchOptions.preferences.height = 720
+        }
+      })
     },
     video: false,
     devServer: {
