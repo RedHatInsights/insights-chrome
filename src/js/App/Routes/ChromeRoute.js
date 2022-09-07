7064f27b1c1574aad3e194b8b711a63bba7f8136
Martin Maroši <marvusm.mmi@gmail.com>
2022-09-07 08:35:50 +0200

    Remove unecessary re-renders for default layout.

--- src/js/App/Routes/ChromeRoute.js
+++ src/js/App/Routes/ChromeRoute.js
@@ -1,5 +1,5 @@
 import { ScalprumComponent } from '@scalprum/react-core';
-import React, { useContext, useEffect } from 'react';
+import React, { memo, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import LoadingFallback from '../../utils/loading-fallback';
@@ -10,61 +10,70 @@ import { getPendoConf } from '../../analytics';
 import classNames from 'classnames';
 import { HelpTopicContext } from '@patternfly/quickstarts';
 
-const ChromeRoute = ({ scope, module, insightsContentRef, scopeClass, ...props }) => {
-  const dispatch = useDispatch();
-  const { setActiveHelpTopicByName } = useContext(HelpTopicContext);
-  const user = useSelector(({ chrome: { user } }) => user);
-  /**
-   * If default title was not set, use module scope (appId)
-   */
-  const defaultTitle = useSelector(({ chrome: { modules } }) => modules?.[scope]?.defaultDocumentTitle || scope);
-  useEffect(() => {
-    batch(() => {
-      dispatch(changeActiveModule(scope));
-      /**
-       * Default document title update. If application won't update its title chrome sets a title using module config
-       */
-      dispatch(updateDocumentTitle(defaultTitle));
-    });
+// eslint-disable-next-line react/display-name
+const ChromeRoute = memo(
+  ({ scope, module, insightsContentRef, scopeClass, ...props }) => {
+    const dispatch = useDispatch();
+    const { setActiveHelpTopicByName } = useContext(HelpTopicContext);
+    const user = useSelector(({ chrome: { user } }) => user);
     /**
-     * update pendo metadata on application change
+     * If default title was not set, use module scope (appId)
      */
-    try {
-      window?.pendo?.updateOptions(getPendoConf(user));
-    } catch (error) {
-      console.error('Unable to update pendo options');
-      console.error(error);
-    }
-
-    /**
-     * TODO: Discuss default close feature of topics
-     * Topics drawer has no close button, therefore there might be an issue with opened topics after user changes route and does not clear the active topic trough the now non existing elements.
-     */
-    setActiveHelpTopicByName && setActiveHelpTopicByName('');
-
-    return () => {
+    const defaultTitle = useSelector(({ chrome: { modules } }) => modules?.[scope]?.defaultDocumentTitle || scope);
+    useEffect(() => {
+      batch(() => {
+        dispatch(changeActiveModule(scope));
+        /**
+         * Default document title update. If application won't update its title chrome sets a title using module config
+         */
+        dispatch(updateDocumentTitle(defaultTitle));
+      });
       /**
-       * Reset global filter when switching application
+       * update pendo metadata on application change
        */
-      dispatch(toggleGlobalFilter(false));
-    };
-  }, [scope]);
+      try {
+        window?.pendo?.updateOptions(getPendoConf(user));
+      } catch (error) {
+        console.error('Unable to update pendo options');
+        console.error(error);
+      }
 
-  return (
-    <Route key={props.path} {...props}>
-      <div className={classNames(scopeClass, scope)}>
-        <ScalprumComponent
-          ErrorComponent={<ErrorComponent />}
-          appName={scope}
-          fallback={LoadingFallback}
-          LoadingFallback={LoadingFallback}
-          scope={scope}
-          module={module}
-        />
-      </div>
-    </Route>
-  );
-};
+      /**
+       * TODO: Discuss default close feature of topics
+       * Topics drawer has no close button, therefore there might be an issue with opened topics after user changes route and does not clear the active topic trough the now non existing elements.
+       */
+      setActiveHelpTopicByName && setActiveHelpTopicByName('');
+
+      return () => {
+        /**
+         * Reset global filter when switching application
+         */
+        dispatch(toggleGlobalFilter(false));
+      };
+    }, [scope]);
+
+    return (
+      <Route key={props.path} {...props}>
+        <div className={classNames(scopeClass, scope)}>
+          <ScalprumComponent
+            ErrorComponent={<ErrorComponent />}
+            appName={scope}
+            fallback={LoadingFallback}
+            LoadingFallback={LoadingFallback}
+            scope={scope}
+            module={module}
+          />
+        </div>
+      </Route>
+    );
+  },
+  // prevent unecessary re-render that can trigger initialization phase of a module
+  (prevProps, nextProps) =>
+    prevProps.scope === nextProps.scope &&
+    prevProps.module === nextProps.module &&
+    prevProps.scopeClass === nextProps.scopeClass &&
+    prevProps.path === nextProps.path
+);
 
 ChromeRoute.propTypes = {
   scope: PropTypes.string.isRequired,
