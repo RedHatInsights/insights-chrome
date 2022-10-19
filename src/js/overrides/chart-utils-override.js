40c7ee5d0ba12c7a319d7287ea504b9df74d9db9
Martin Maroši <marvusm.mmi@gmail.com>
2022-10-19 14:13:16 +0200

    Add react-chars override for missing export. (#2091)

---
+++ src/js/overrides/chart-utils-override.js
@@ -0,0 +1,4 @@
+export * from '@patternfly/react-charts/dist/js/components/ChartUtils/chart-theme';
+import { getThemeColors as getThemeColorsInternal } from '@patternfly/react-charts/dist/js/components/ChartUtils/chart-theme';
+export const getThemeColors = getThemeColorsInternal;
+export const getLightThemeColors = getThemeColorsInternal;
