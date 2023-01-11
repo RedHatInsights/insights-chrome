1b170817dac4d9dbbfc43c12c03a0dc00cf89d73
Martin Maroši <marvusm.mmi@gmail.com>
2023-01-11 10:06:22 +0100

    Fix eslint for cypress ts files.

--- cypress/component/helptopics/HelpTopicManager.cy.tsx
+++ cypress/component/helptopics/HelpTopicManager.cy.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { Store } from 'redux';
 import ReducerRegistry from '@redhat-cloud-services/frontend-components-utilities/ReducerRegistry';
@@ -14,7 +14,7 @@ import LibtJWTContext from '../../../src/components/LibJWTContext';
 import { ChromeUser } from '@redhat-cloud-services/types';
 import { LibJWT } from '../../../src/auth';
 
-const Wrapper = ({ store }: {store: Store}) => (
+const Wrapper = ({ store }: { store: Store }) => (
   <IntlProvider locale="en">
     <LibtJWTContext.Provider
       value={{
@@ -26,17 +26,19 @@ const Wrapper = ({ store }: {store: Store}) => (
       }}
     >
       <Provider store={store}>
-        <RootApp config={{
-          TestApp: {
-            name: 'TestApp',
-            appId: 'TestApp',
-            manifestLocation: '/foo/bar.json',          
-          }
-        }} />
+        <RootApp
+          config={{
+            TestApp: {
+              name: 'TestApp',
+              appId: 'TestApp',
+              manifestLocation: '/foo/bar.json',
+            },
+          }}
+        />
       </Provider>
     </LibtJWTContext.Provider>
   </IntlProvider>
-)
+);
 
 const TestComponent = () => {
   const chrome = useChrome();
@@ -52,8 +54,8 @@ const TestComponent = () => {
         Open a topic create-environment
       </Button>
     </div>
-  )
-}
+  );
+};
 
 describe('HelpTopicManager', () => {
   let store;
@@ -65,15 +67,14 @@ describe('HelpTopicManager', () => {
         ...chromeInitialState.chrome,
         moduleRoutes: [
           {
-            path: "/__cypress/iframes//home/martin/insights/insights-chrome/cypress/component/helptopics/HelpTopicManager.cy.tsx",
+            path: '/__cypress/iframes//home/martin/insights/insights-chrome/cypress/component/helptopics/HelpTopicManager.cy.tsx',
             module: './TestApp',
             scope: 'TestApp',
             appId: 'TestApp',
             manifestLocation: '/foo/bar.json',
-
-          }
+          },
         ],
-        user: testUser  ,
+        user: testUser,
       },
     });
     reduxRegistry.register(chromeReducer());
@@ -87,39 +88,36 @@ describe('HelpTopicManager', () => {
     cy.intercept('POST', '/api/featureflags/v0/client/metrics', {});
     cy.intercept('POST', 'https://api.segment.io/v1/*', {});
     cy.intercept('GET', ' /api/rbac/v1/access/?application=inventory&limit=1000', {
-      data: []
+      data: [],
     });
     cy.intercept('GET', '/api/quickstarts/v1/progress?account=*', {
-      data: []
+      data: [],
     });
-    cy.intercept('GET', '/api/quickstarts/v1/*', (req) => {      
+    cy.intercept('GET', '/api/quickstarts/v1/*', (req) => {
       if (req.url.includes('/helptopics')) {
-        req.reply({status: 200, fixture: 'helpTopicsResponse.json'})
-        return
+        req.reply({ status: 200, fixture: 'helpTopicsResponse.json' });
+        return;
       }
 
-      req.reply({status: 200, body: {data: []}})
-    })
-
-  })
+      req.reply({ status: 200, body: { data: [] } });
+    });
+  });
 
   it('should switch help topics drawer content', () => {
     // change screen size
     cy.viewport(1280, 720);
     // mount element
-    cy.mount(<Wrapper store={store}></Wrapper>)
-    cy.wait('@manifest')
+    cy.mount(<Wrapper store={store}></Wrapper>);
+    cy.wait('@manifest');
 
     // mock the dynamic module
-    cy.window().then(win => {
+    cy.window().then((win) => {
       win.TestApp = {
-        init: () => {
-
-        },
+        init: () => {},
         get: () => () => ({
-          default: TestComponent
-        })
-      }
+          default: TestComponent,
+        }),
+      };
       win.__scalprum__ = {
         ...window.__scalprum__,
         scalprumOptions: {
@@ -131,37 +129,36 @@ describe('HelpTopicManager', () => {
             modules: {
               './TestApp': {
                 __esModule: true,
-                default: TestComponent
-              }
-            }
-          }
-        }
-      }
-    })
+                default: TestComponent,
+              },
+            },
+          },
+        },
+      };
+    });
 
     // open drawer
-    cy.get("#open-one").click()
-    cy.get(`h1.pf-c-title`).should('be.visible').contains('Configure components')
+    cy.get('#open-one').click();
+    cy.get(`h1.pf-c-title`).should('be.visible').contains('Configure components');
     // switch from external button
-    cy.get("#open-two").click()
-    cy.get(`h1.pf-c-title`).should('be.visible').contains('Create a new environment')
+    cy.get('#open-two').click();
+    cy.get(`h1.pf-c-title`).should('be.visible').contains('Create a new environment');
 
     // open help topics context menu
-    cy.get("button#helptopics-toggle").click()
-    cy.get("button.pf-c-options-menu__menu-item").contains('Automatic Deployment').click()
-    cy.get(`h1.pf-c-title`).should('be.visible').contains('Automatic Deployment')
+    cy.get('button#helptopics-toggle').click();
+    cy.get('button.pf-c-options-menu__menu-item').contains('Automatic Deployment').click();
+    cy.get(`h1.pf-c-title`).should('be.visible').contains('Automatic Deployment');
 
     // switch from external button back to first topic
-    cy.get("#open-one").click()
-    cy.get(`h1.pf-c-title`).should('be.visible').contains('Configure components')
+    cy.get('#open-one').click();
+    cy.get(`h1.pf-c-title`).should('be.visible').contains('Configure components');
 
     // close drawer
-    cy.get('div.pfext-quick-start-panel-content__close-button').click()
-    cy.get(`h1.pf-c-title`).contains('Configure components').should('not.exist')
+    cy.get('div.pfext-quick-start-panel-content__close-button').click();
+    cy.get(`h1.pf-c-title`).contains('Configure components').should('not.exist');
 
     // open second help topic
-    cy.get("#open-two").click()
-    cy.get(`h1.pf-c-title`).should('be.visible').contains('Create a new environment')
-
+    cy.get('#open-two').click();
+    cy.get(`h1.pf-c-title`).should('be.visible').contains('Create a new environment');
   });
 });
