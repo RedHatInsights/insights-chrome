2605bd6468cf2d36cafaf847dff60b5ac758d991
Martin Maroši <marvusm.mmi@gmail.com>
2023-01-26 13:15:31 +0100

    Collect previously visited bundle title.

---
+++ src/hooks/useBundle.ts
@@ -0,0 +1,23 @@
+import { getUrl } from '../utils/common';
+
+const bundleMapping: {
+  [bundleId: string]: string;
+} = {
+  'application-services': 'Application and Data Services',
+  openshift: 'OpenShift',
+  ansible: 'Ansible Automation Platform',
+  insights: 'Red Hat Insights',
+  edge: 'Edge management',
+  settings: 'Settings',
+  landing: 'Home',
+  allservices: 'Home',
+  iam: 'Identity & Access Management',
+  internal: 'Internal',
+};
+
+const useBundle = () => {
+  const bundleId = getUrl('bundle');
+  return { bundleId, bundleTitle: bundleMapping[bundleId] || bundleId };
+};
+
+export default useBundle;
