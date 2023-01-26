f0740dced16947fdb48ec45e761824691f07cde6
Martin Maroši <marvusm.mmi@gmail.com>
2023-01-26 17:32:33 +0100

    Use chrome link title to update document title.

--- src/components/ChromeLink/ChromeLink.tsx
+++ src/components/ChromeLink/ChromeLink.tsx
@@ -25,90 +25,93 @@ interface RefreshLinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
 export interface LinkWrapperProps extends RefreshLinkProps {
   className?: string;
   tabIndex?: number;
+  documentTitleUpdate?: string;
 }
 
-const LinkWrapper: React.FC<LinkWrapperProps> = memo(({ href, isBeta, onLinkClick, className, currAppId, appId, children, tabIndex }) => {
-  const linkRef = useRef<HTMLAnchorElement | null>(null);
-  const moduleRoutes = useSelector<ReduxState, RouteDefinition[]>(({ chrome: { moduleRoutes } }) => moduleRoutes);
-  const moduleEntry = useMemo(() => moduleRoutes.find((route) => href.includes(route.path)), [href, appId]);
-  const defaultTitle = useSelector(({ chrome: { modules } }: ReduxState) => appId && modules?.[appId]?.defaultDocumentTitle);
-  const preloadTimeout = useRef<NodeJS.Timeout>();
-  let actionId = href.split('/').slice(2).join('/');
-  if (actionId.includes('/')) {
-    actionId = actionId.split('/').pop() as string;
-  }
-  if (currAppId !== appId && href.split('/').length === 3) {
-    actionId = '/';
-  }
+const LinkWrapper: React.FC<LinkWrapperProps> = memo(
+  ({ documentTitleUpdate, href, isBeta, onLinkClick, className, currAppId, appId, children, tabIndex }) => {
+    const linkRef = useRef<HTMLAnchorElement | null>(null);
+    const moduleRoutes = useSelector<ReduxState, RouteDefinition[]>(({ chrome: { moduleRoutes } }) => moduleRoutes);
+    const moduleEntry = useMemo(() => moduleRoutes.find((route) => href.includes(route.path)), [href, appId]);
+    const defaultTitle = useSelector(({ chrome: { modules } }: ReduxState) => appId && modules?.[appId]?.defaultDocumentTitle);
+    const preloadTimeout = useRef<NodeJS.Timeout>();
+    let actionId = href.split('/').slice(2).join('/');
+    if (actionId.includes('/')) {
+      actionId = actionId.split('/').pop() as string;
+    }
+    if (currAppId !== appId && href.split('/').length === 3) {
+      actionId = '/';
+    }
 
-  /**
-   * If the sub nav item points to application root
-   * eg. /openshift/cost-management we don't want to send "/cost-management" but "/"
-   * We are not in app sub route but in app root
-   */
-  const domEvent: NavDOMEvent = {
-    href,
-    id: actionId,
-    navId: actionId,
     /**
-     * @deprecated
-     * Remove once nav overhaul is in all environments
+     * If the sub nav item points to application root
+     * eg. /openshift/cost-management we don't want to send "/cost-management" but "/"
+     * We are not in app sub route but in app root
      */
-    type: 'click',
-  };
-  const dispatch = useDispatch();
-  const onClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    if (defaultTitle || appId) {
-      updateDocumentTitle(defaultTitle || appId);
-    }
+    const domEvent: NavDOMEvent = {
+      href,
+      id: actionId,
+      navId: actionId,
+      /**
+       * @deprecated
+       * Remove once nav overhaul is in all environments
+       */
+      type: 'click',
+    };
+    const dispatch = useDispatch();
+    const onClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      if (defaultTitle || documentTitleUpdate || appId) {
+        updateDocumentTitle(defaultTitle || documentTitleUpdate || appId);
+      }
 
-    if (event.ctrlKey || event.shiftKey) {
-      return false;
-    }
-    if (onLinkClick && isBeta) {
-      if (!onLinkClick(event, href)) {
+      if (event.ctrlKey || event.shiftKey) {
         return false;
       }
-    }
-
-    /**
-     * Add reference to the DOM link element
-     */
-    domEvent.target = linkRef.current;
-    dispatch(appNavClick({ id: actionId }, domEvent));
-  };
-
-  // turns /settings/rbac/roles -> settings_rbac_roles
-  const quickStartHighlightId = href
-    .split('/')
-    .slice(href.startsWith('/') ? 1 : 0)
-    .join('_');
-  return (
-    <NavLink
-      onMouseEnter={() => {
-        if (moduleEntry) {
-          preloadTimeout.current = setTimeout(() => {
-            preloadModule(moduleEntry?.scope, moduleEntry?.module);
-          }, 250);
+      if (onLinkClick && isBeta) {
+        if (!onLinkClick(event, href)) {
+          return false;
         }
-      }}
-      onMouseLeave={() => {
-        if (preloadTimeout.current) {
-          clearTimeout(preloadTimeout.current);
-        }
-      }}
-      tabIndex={tabIndex}
-      ref={linkRef}
-      data-testid="router-link"
-      onClick={onClick}
-      to={href}
-      className={className}
-      data-quickstart-id={quickStartHighlightId}
-    >
-      {children}
-    </NavLink>
-  );
-});
+      }
+
+      /**
+       * Add reference to the DOM link element
+       */
+      domEvent.target = linkRef.current;
+      dispatch(appNavClick({ id: actionId }, domEvent));
+    };
+
+    // turns /settings/rbac/roles -> settings_rbac_roles
+    const quickStartHighlightId = href
+      .split('/')
+      .slice(href.startsWith('/') ? 1 : 0)
+      .join('_');
+    return (
+      <NavLink
+        onMouseEnter={() => {
+          if (moduleEntry) {
+            preloadTimeout.current = setTimeout(() => {
+              preloadModule(moduleEntry?.scope, moduleEntry?.module);
+            }, 250);
+          }
+        }}
+        onMouseLeave={() => {
+          if (preloadTimeout.current) {
+            clearTimeout(preloadTimeout.current);
+          }
+        }}
+        tabIndex={tabIndex}
+        ref={linkRef}
+        data-testid="router-link"
+        onClick={onClick}
+        to={href}
+        className={className}
+        data-quickstart-id={quickStartHighlightId}
+      >
+        {children}
+      </NavLink>
+    );
+  }
+);
 
 LinkWrapper.displayName = 'MemoizedLinkWrapper';
 
