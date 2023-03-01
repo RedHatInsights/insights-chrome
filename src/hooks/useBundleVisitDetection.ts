7a90c64e6b9b75067c32551c4f3b3cbf81e55667
Martin Maroši <marvusm.mmi@gmail.com>
2023-03-01 16:22:29 +0100

    Detect users first bundle visits. (#2305)

---
+++ src/hooks/useBundleVisitDetection.ts
@@ -0,0 +1,34 @@
+import { useEffect, useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+import { VisitedBundles, useVisitedBundles } from '@redhat-cloud-services/chrome';
+import { getUrl } from '../utils/common';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { ReduxState } from '../redux/store';
+
+// TMP Insights specific trigger
+const shouldSendVisit = (bundle: string, visits: VisitedBundles) => bundle === 'insights' && !visits[bundle];
+
+const sendVisitedBundle = async (orgId: string) => {
+  axios.post('/api/insights/v1/weeklyreportautosubscribe/', {
+    org_id: orgId,
+    is_auto_subscribed: true,
+  });
+};
+
+const useBundleVisitDetection = () => {
+  const { pathname } = useLocation();
+  const orgId = useSelector(({ chrome: { user } }: ReduxState) => user?.identity?.org_id);
+  const { markVisited, visitedBundles } = useVisitedBundles();
+  const bundle = useMemo(() => getUrl('bundle'), [pathname]);
+  useEffect(() => {
+    if (shouldSendVisit(bundle, visitedBundles) && orgId) {
+      sendVisitedBundle(orgId);
+    }
+    if (bundle?.length > 0 && orgId) {
+      markVisited(bundle);
+    }
+  }, [bundle, orgId]);
+};
+
+export default useBundleVisitDetection;
