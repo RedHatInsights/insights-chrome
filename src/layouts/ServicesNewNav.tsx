e2f8ac0eaf0380015434b88118a2ec5f4e41997b
ewinchel <ewinchel@redhat.com>
2023-03-13 16:18:35 -0400

    dropdown styling updates

--- src/layouts/ServicesNewNav.tsx
+++ src/layouts/ServicesNewNav.tsx
@@ -1,6 +1,23 @@
 import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import { Dropdown, DropdownToggle, Gallery, Icon, Masthead, Page, PageSection, Panel, PanelMain, Sidebar, Split, SplitItem, Text, TextContent, TextVariants, Title } from '@patternfly/react-core';
+import {
+  Dropdown,
+  DropdownToggle,
+  Gallery,
+  Icon,
+  Masthead,
+  Page,
+  PageSection,
+  Panel,
+  PanelMain,
+  Sidebar,
+  Split,
+  SplitItem,
+  Text,
+  TextContent,
+  TextVariants,
+  Title,
+} from '@patternfly/react-core';
 import {
   Button,
   Card,
@@ -57,7 +74,6 @@ const AppLinks = ({ id, title, links = [], setIsOpen }: AppLinksProps) =>
       </Split>
     </div>
   ) : null;
-
 
 export type ServicesNewNavDropdownProps = {
   isLoaded: boolean;
@@ -131,110 +147,98 @@ const ServicesNewNavDropdown = ({ isLoaded, setIsOpen, isOpen, filterValue, setF
       ouiaId="App Filter"
     >
       {ReactDOM.createPortal(
-        <div id="chrome-app-render-root">
-        <Page
-          className="chr-c-all-services"
-          onPageResize={null} // required to disable PF resize observer that causes re-rendring issue
-          header={
-            <Masthead className="chr-c-masthead">
-              <Header />
-            </Masthead>
-          }
-        >
-          <RedirectBanner />
-
-          <PageSection padding={{ default: 'noPadding'}}>
-            <Panel variant="raised" className="chr-c-panel-services-nav">
-              <PanelMain>
-                <Sidebar className="pf-u-pt-md pf-u-pt-0-on-md">
-                  <SidebarPanel>
-                    {' '}
-                    <Tabs
-                      inset={{
-                        default: 'insetNone',
-                      }}
-                      activeKey={activeTabKey}
-                      onSelect={handleTabClick}
-                      isVertical
-                      expandable={{
-                        default: 'expandable',
-                        md: 'nonExpandable',
-                      }}
-                      isExpanded={isExpanded}
-                      onToggle={onToggle}
-                      toggleText="Containers"
-                      aria-label="Tabs in the vertical expandable example"
-                      role="region"
-                      className="pf-u-pl-md"
-                    >
-                      {linkSections.map((section, index) => (
-                        <Tab
-                          eventKey={index}
-                          title={<TabTitleText>{section.title}</TabTitleText>}
-                          tabContentId="refTab1Section"
-                          tabContentRef={contentRef1}
-                          onClick={() => onTabClick(section, index)}
-                        />
-                      ))}
-                    </Tabs>
-                    <Divider inset={{default: 'insetNone'}} className="pf-u-py-md" />
-                    <TextContent className='pf-u-pb-md pf-u-pl-lg'>
-                      <Text component={TextVariants.p}>
-                        <ChromeLink href="/allservices">
-                          <Icon className='pf-u-mr-sm' isInline>
-                            <BookOpenIcon />
-                          </Icon>
-                          Browse all cloud services
-                        </ChromeLink>
-                      </Text>
-                    </TextContent>
-                  </SidebarPanel>
-                  <SidebarContent>
-                    <Card isPlain>
-                      <CardHeader>
-                        <Title headingLevel="h2">
-                          {convertTitleIcon(selectedService.icon)} &nbsp;{selectedService.title}
-                        </Title>
-                        <CardActions>
-                          <Button variant="plain" aria-label="Close menu">
-                            <TimesIcon />
-                          </Button>
-                        </CardActions>
-                      </CardHeader>
-                      <CardBody>
-                        <TabContent eventKey={activeTabKey} id="refTab1Section" ref={contentRef1} aria-label={selectedService.description}>
-                          <Gallery hasGutter>
-                            {selectedService.links.map((link) => (
-                              <Card isFlat isSelectableRaised>
-                                <CardBody className="pf-u-p-md">
-                                  <Split>
-                                    <SplitItem className="pf-m-fill">{link.title}</SplitItem>
-                                    <SplitItem>
-                                      <Icon className='chr-c-icon-service-card'>
-                                        <StarIcon />
-                                      </Icon>
-                                    </SplitItem>
-                                  </Split>
-                                  <TextContent>
-                                    <Text component="small">bundle name</Text>
-                                    <Text component="small" className="pf-u-color-100">{linkDescription(link)}</Text>
-                                  </TextContent>
-                                </CardBody>
-                              </Card>
-                            ))}
-                          </Gallery>
-                        </TabContent>
-                      </CardBody>
-                    </Card>
-                  </SidebarContent>
-                </Sidebar>
-              </PanelMain>
-            </Panel>
-          </PageSection>
-        </Page>
-      </div>, 
-      document.body
-    )}
+        <div ref={dropdownRef} className="pf-c-dropdown chr-c-page__services-nav-dropdown-menu" data-testid="chr-c__find-app-service">
+          <Panel variant="raised" className="pf-c-dropdown__menu pf-u-p-0 pf-u-w-100 chr-c-panel-services-nav ">
+            <PanelMain>
+              <Sidebar className="pf-u-pt-md pf-u-pt-0-on-md">
+                <SidebarPanel>
+                  {' '}
+                  <Tabs
+                    inset={{
+                      default: 'insetNone',
+                    }}
+                    activeKey={activeTabKey}
+                    onSelect={handleTabClick}
+                    isVertical
+                    expandable={{
+                      default: 'expandable',
+                      md: 'nonExpandable',
+                    }}
+                    isExpanded={isExpanded}
+                    onToggle={onToggle}
+                    toggleText="Containers"
+                    aria-label="Tabs in the vertical expandable example"
+                    role="region"
+                    className="pf-u-pl-md"
+                  >
+                    {linkSections.map((section, index) => (
+                      <Tab
+                        eventKey={index}
+                        title={<TabTitleText>{section.title}</TabTitleText>}
+                        tabContentId="refTab1Section"
+                        tabContentRef={contentRef1}
+                        onClick={() => onTabClick(section, index)}
+                      />
+                    ))}
+                  </Tabs>
+                  <Divider inset={{ default: 'insetNone' }} className="pf-u-py-md" />
+                  <TextContent className="pf-u-pb-md pf-u-pl-lg">
+                    <Text component={TextVariants.p}>
+                      <ChromeLink href="/allservices">
+                        <Icon className="pf-u-mr-sm" isInline>
+                          <BookOpenIcon />
+                        </Icon>
+                        Browse all cloud services
+                      </ChromeLink>
+                    </Text>
+                  </TextContent>
+                </SidebarPanel>
+                <SidebarContent>
+                  <Card isPlain>
+                    <CardHeader>
+                      <Title headingLevel="h2">
+                        {convertTitleIcon(selectedService.icon)} &nbsp;{selectedService.title}
+                      </Title>
+                      <CardActions>
+                        <Button variant="plain" aria-label="Close menu">
+                          <TimesIcon />
+                        </Button>
+                      </CardActions>
+                    </CardHeader>
+                    <CardBody>
+                      <TabContent eventKey={activeTabKey} id="refTab1Section" ref={contentRef1} aria-label={selectedService.description}>
+                        <Gallery hasGutter>
+                          {selectedService.links.map((link) => (
+                            <Card isFlat isSelectableRaised>
+                              <CardBody className="pf-u-p-md">
+                                <Split>
+                                  <SplitItem className="pf-m-fill">{link.title}</SplitItem>
+                                  <SplitItem>
+                                    <Icon className="chr-c-icon-service-card">
+                                      <StarIcon />
+                                    </Icon>
+                                  </SplitItem>
+                                </Split>
+                                <TextContent>
+                                  <Text component="small">bundle name</Text>
+                                  <Text component="small" className="pf-u-color-100">
+                                    {linkDescription(link)}
+                                  </Text>
+                                </TextContent>
+                              </CardBody>
+                            </Card>
+                          ))}
+                        </Gallery>
+                      </TabContent>
+                    </CardBody>
+                  </Card>
+                </SidebarContent>
+              </Sidebar>
+            </PanelMain>
+          </Panel>
+        </div>,
+        document.body
+      )}
     </Dropdown>
   );
 };
@@ -247,15 +251,9 @@ const ServicesNewNav = () => {
   const { filteredApps, isLoaded, isOpen, setIsOpen, filterValue, setFilterValue } = useAppFilter();
   return (
     <React.Fragment>
-      <ServicesNewNavDropdown
-        isLoaded={isLoaded}
-        setIsOpen={setIsOpen}
-        isOpen={isOpen}
-        filterValue={filterValue}
-        setFilterValue={setFilterValue}
-      />
+      <ServicesNewNavDropdown isLoaded={isLoaded} setIsOpen={setIsOpen} isOpen={isOpen} filterValue={filterValue} setFilterValue={setFilterValue} />
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default ServicesNewNav;
