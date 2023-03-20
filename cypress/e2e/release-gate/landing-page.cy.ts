d2ecbf5403ca8178c28e60177f3d82da94632d31
Martin Maroši <marvusm.mmi@gmail.com>
2023-03-20 10:54:32 +0100

    Add global login CY command.

--- cypress/e2e/release-gate/landing-page.cy.ts
+++ cypress/e2e/release-gate/landing-page.cy.ts
@@ -2,16 +2,7 @@ describe('Landing page', () => {
   it('visit landing page', () => {
     cy.visit('/');
 
-    cy.wait(1000);
-
-    // login into the session
-    cy.get('#username-verification').type(Cypress.env('E2E_USER'));
-    cy.get('#login-show-step2').click();
-    cy.get('#password').type(Cypress.env('E2E_PASSWORD'));
-    cy.get('#rh-password-verification-submit-button').click();
-
-    // close cookies bar
-    cy.get('#truste-consent-buttons').click();
+    cy.login();
 
     // check if a favorites link exists on the page
     cy.contains('View my favorite services').should('exist');
