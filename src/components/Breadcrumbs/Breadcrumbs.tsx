82d3eff7ba1de4f07669d6830a48bd5d319a835f
Martin Maroši <marvusm.mmi@gmail.com>
2023-04-11 09:09:38 +0200

    Add favoriting to breadcrumbs.

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,5 +1,5 @@
 import { Breadcrumb, BreadcrumbItem, FlexItem, MastheadToggle, PageBreadcrumb, PageToggleButton } from '@patternfly/react-core';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import BarsIcon from '@patternfly/react-icons/dist/js/icons/bars-icon';
 import { onToggle } from '../../redux/actions';
@@ -8,6 +8,8 @@ import useBreadcrumbsLinks from '../../hooks/useBreadcrumbsLinks';
 import ChromeLink from '../ChromeLink/ChromeLink';
 import './Breadcrumbs.scss';
 import classNames from 'classnames';
+import BreadcrumbsFavorites from './BreadcrumbsFavorites';
+import { useFavoritePages } from '@redhat-cloud-services/chrome';
 
 export type Breadcrumbsprops = {
   isNavOpen?: boolean;
@@ -18,6 +20,13 @@ export type Breadcrumbsprops = {
 const Breadcrumbs = ({ hideNav, isNavOpen, setIsNavOpen }: Breadcrumbsprops) => {
   const dispatch = useDispatch();
   const segments = useBreadcrumbsLinks();
+  const { favoritePages, favoritePage, unfavoritePage } = useFavoritePages();
+
+  const activeSegment = useMemo(() => segments.find(({ active }) => active), [segments]);
+  const isFavorited = useMemo(
+    () => favoritePages.find(({ pathname, favorite }) => favorite && pathname === activeSegment?.href),
+    [favoritePages, activeSegment]
+  );
   return (
     <PageBreadcrumb className="chr-c-breadcrumbs pf-u-pt-0">
       <div className="chr-c-breadcrumbs__alignment">
@@ -54,6 +63,15 @@ const Breadcrumbs = ({ hideNav, isNavOpen, setIsNavOpen }: Breadcrumbsprops) => 
             ))}
           </Breadcrumb>
         </FlexItem>
+        {activeSegment && (
+          <FlexItem className="pf-u-ml-auto">
+            <BreadcrumbsFavorites
+              favoritePage={() => favoritePage(activeSegment.href)}
+              unfavoritePage={() => unfavoritePage(activeSegment.href)}
+              isFavorited={!!isFavorited}
+            />
+          </FlexItem>
+        )}
       </div>
     </PageBreadcrumb>
   );
---
+++ src/components/Breadcrumbs/BreadcrumbsFavorites.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import { Icon, Menu, MenuItem, MenuList, MenuToggle, Popper } from '@patternfly/react-core';
+import StarIcon from '@patternfly/react-icons/dist/js/icons/star-icon';
+import CaretDownIcon from '@patternfly/react-icons/dist/js/icons/caret-down-icon';
+import classNames from 'classnames';
+
+import './BreadcrumbsFavorites.scss';
+import ChromeLink from '../ChromeLink/ChromeLink';
+
+const BreadcrumbsFavorites = ({
+  isFavorited,
+  favoritePage,
+  unfavoritePage,
+}: {
+  isFavorited: boolean;
+  favoritePage: () => void;
+  unfavoritePage: () => void;
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const toggleRef = React.useRef<HTMLButtonElement>(null);
+  const menuRef = React.useRef<HTMLDivElement>(null);
+  const containerRef = React.useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (isOpen && !menuRef.current?.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleMenuKeys = (event: KeyboardEvent) => {
+    if (!isOpen) {
+      return;
+    }
+    if (menuRef.current?.contains(event.target as Node) || toggleRef.current?.contains(event.target as Node)) {
+      if (event.key === 'Escape') {
+        setIsOpen(!isOpen);
+        toggleRef.current?.focus();
+      }
+    }
+  };
+
+  const onToggleClick = (ev: React.MouseEvent) => {
+    ev.stopPropagation(); // Stop handleClickOutside from handling
+    setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleMenuKeys);
+    window.addEventListener('click', handleClickOutside);
+
+    return () => {
+      window.removeEventListener('keydown', handleMenuKeys);
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen, menuRef]);
+  const toggle = (
+    <MenuToggle aria-label="Toggle" ref={toggleRef} variant="plain" onClick={onToggleClick} isExpanded={isOpen}>
+      <Icon
+        className={classNames('chr-c-breadcrumbs__favorite', {
+          isFavorited,
+        })}
+      >
+        <StarIcon />
+      </Icon>
+      <CaretDownIcon className="pf-u-ml-sm" />
+    </MenuToggle>
+  );
+
+  const menu = (
+    <Menu ref={menuRef}>
+      <MenuList key="favorites">
+        <MenuItem
+          onClick={() => {
+            favoritePage();
+            setIsOpen(false);
+          }}
+          isDisabled={isFavorited}
+        >
+          Add to Favorites
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            unfavoritePage();
+            setIsOpen(false);
+          }}
+          isDisabled={!isFavorited}
+        >
+          Remove from Favorites
+        </MenuItem>
+        <MenuItem component={(props) => <ChromeLink {...props} href="/favoritedservices" />}>View all Favorites</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+  return (
+    <div ref={containerRef}>
+      <Popper
+        placement="bottom-end"
+        trigger={toggle}
+        popper={menu}
+        isVisible={isOpen}
+        popperMatchesTriggerWidth={false}
+        appendTo={containerRef.current || undefined}
+      />
+    </div>
+  );
+};
+
+export default BreadcrumbsFavorites;
