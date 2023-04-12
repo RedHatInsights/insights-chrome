e28fb6bb51099cd6f5ffd134167034f93ca93117
ewinchel <ewinchel@redhat.com>
2023-04-12 16:36:59 -0400

    Masthead updates

--- src/components/Header/ServicesLink.tsx
+++ src/components/Header/ServicesLink.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Button, Icon } from '@patternfly/react-core';
-import CloudIcon from '@patternfly/react-icons/dist/js/icons/cloud-icon';
+import { Button } from '@patternfly/react-core';
 
 import ChromeLink from '../ChromeLink';
 
@@ -10,9 +9,6 @@ const ServicesLink = () => {
       className="chr-c-button-masthead pf-u-px-lg-on-md"
       component={(props) => <ChromeLink {...props} href="/allservices" documentTitleUpdate="All Services" />}
     >
-      <Icon isInline className="ins-m-hide-on-sm">
-        <CloudIcon />
-      </Icon>
       Services
     </Button>
   );
