8110d3edb583298222f69d0ab637ead657f089db
Martin Maroši <marvusm.mmi@gmail.com>
2023-04-21 09:24:21 +0200

    Allow deeply nested breadcrumbs.

--- src/hooks/useBreadcrumbsLinks.ts
+++ src/hooks/useBreadcrumbsLinks.ts
@@ -8,7 +8,11 @@ import useBundle from './useBundle';
 import { NavItem, Navigation } from '../@types/types';
 
 function isNavItems(navigation: Navigation | NavItem[]): navigation is Navigation {
-  return Array.isArray((navigation as any).navItems);
+  return Array.isArray((navigation as Navigation).navItems);
+}
+
+function isActiveLeaf(item: NavItem | undefined): item is Required<NavItem, 'href'> {
+  return typeof item?.href === 'string' && item?.active === true;
 }
 
 function isExpandableNav(item: NavItem): item is Required<NavItem, 'routes'> {
@@ -21,6 +25,29 @@ function isGroup(item: NavItem): item is Required<NavItem, 'groupId'> {
 
 function extractNavItemGroups(activeNavigation: Navigation | NavItem[]) {
   return (isNavItems(activeNavigation) ? activeNavigation.navItems.map((item) => (isGroup(item) ? item.navItems : item)) : activeNavigation).flat();
+}
+
+function findActiveLeaf(navItems: (NavItem | undefined)[]): { activeItem: Required<NavItem, 'href'> | undefined; navItems: NavItem[] } {
+  let leaf: Required<NavItem, 'href'> | undefined;
+  // store the parent nodes
+  const leafPath: NavItem[] = [];
+  let index = 0;
+  while (leaf === undefined && index < navItems.length) {
+    const item = navItems[index];
+    index += 1;
+    if (item && isExpandableNav(item)) {
+      const { activeItem, navItems } = findActiveLeaf(item.routes) || {};
+      if (activeItem) {
+        leaf = activeItem;
+        // append parent nodes of an active item
+        leafPath.push(item, ...navItems);
+      }
+    } else if (isActiveLeaf(item)) {
+      leaf = item;
+    }
+  }
+
+  return { activeItem: leaf, navItems: leafPath };
 }
 
 const useBreadcrumbsLinks = () => {
@@ -41,16 +68,15 @@ const useBreadcrumbsLinks = () => {
     const activeNavSegment = navigation[bundleId];
     if (activeNavSegment && isNavItems(activeNavSegment)) {
       const activeNavigation = extractNavItemGroups(activeNavSegment);
-      const activeFragment = activeNavigation.find((item) => item?.active);
-      if (activeFragment && isExpandableNav(activeFragment)) {
-        const leafFragment = activeFragment.routes.find((item) => item.active);
-        const appFragments = leafFragment?.href?.split('/');
-        appFragments?.pop();
-        const groupLink = appFragments ? `${appFragments.join('/')}` : `/${bundleId}`;
-        const groupFragment = { href: groupLink, ...activeFragment };
-        segments.push(groupFragment, ...(leafFragment?.href ? [leafFragment as Required<NavItem, 'href'>] : []));
-      } else if (activeFragment) {
-        segments.push(activeFragment as Required<NavItem, 'href'>);
+      const { activeItem, navItems } = findActiveLeaf(activeNavigation);
+      if (activeItem) {
+        const appFragments = activeItem.href.split('/');
+        appFragments.pop();
+        const groupFragments: Required<NavItem, 'href'>[] = navItems.map((item, index) => ({
+          ...item,
+          href: appFragments.slice(0, appFragments.length - 1 - index).join('/') || `/${bundleId}`,
+        }));
+        segments.push(...groupFragments, activeItem);
       }
     }
     setSegments(segments);
