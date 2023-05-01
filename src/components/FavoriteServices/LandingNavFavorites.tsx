e2bbfbbbd93ca7cfec96779f93af316483cbb85f
Zein Sleiman <zsleiman@redhat.com>
2023-05-01 14:20:48 -0500

    fix pagination

--- src/components/FavoriteServices/LandingNavFavorites.tsx
+++ src/components/FavoriteServices/LandingNavFavorites.tsx
@@ -1,10 +1,8 @@
-import { Icon, Card, CardBody, Tile, Title, Pagination, PaginationProps, PaginationVariant, Gallery, GalleryItem, Text, TextContent } from '@patternfly/react-core';
-import { StarIcon } from '@patternfly/react-icons';
-import React, { useEffect, Fragment, ReactNode, useState } from 'react';
+import { Icon, Card, CardBody, Title, Pagination, Gallery, GalleryItem, Text, TextContent, Button } from '@patternfly/react-core';
+import { BookOpenIcon, StarIcon } from '@patternfly/react-icons';
+import React, { useState } from 'react';
 import useFavoritedServices from '../../hooks/useFavoritedServices';
 import EmptyState from '../FavoriteServices/EmptyState';
-import ServiceTile, { ServiceTileProps } from './ServiceTile';
-import { useNavigate } from "react-router-dom";
 import { bundleMapping } from '../../hooks/useBundle';
 import './LandingNavFavorites.scss';
 import ChromeLink from '../ChromeLink';
@@ -13,57 +11,71 @@ const LandingNavFavorites = () => {
 
   const favoritedServices  = useFavoritedServices();
   const [page, setPage] = useState(1);
-  const [perPage, setPerPage] = React.useState(20);
+  const [perPage, setPerPage] = React.useState(5);
 
   const onSetPage = (_event: React.MouseEvent | React.KeyboardEvent | MouseEvent, newPage: number) => {
     setPage(newPage);
   };
 
-  const favoriteName = (name?: ReactNode): string => {
-    if (name) {
-      return name.toString();
-    } else {
-      return ""
-    }
-  }
+  const onPerPageSelect = (
+    _event: React.MouseEvent | React.KeyboardEvent | MouseEvent,
+    newPerPage: number,
+    newPage: number
+  ) => {
+    setPerPage(newPerPage);
+    setPage(newPage);
+  };
 
   const getBundle = (href: string) => bundleMapping[href.split('/')[1]];
+
+  const buildFavorites = () => {
+    return favoritedServices.slice((page -1)*perPage, page*perPage).map((favorite, index) => (
+      <GalleryItem key={index}>
+        <ChromeLink href={favorite.pathname} className="chr-c-favorite-service__tile">
+          <Card isFullHeight isFlat isSelectableRaised>
+            <CardBody className="pf-u-p-md">
+              {favorite.name}
+              <TextContent>
+                <Text component="small">{getBundle(favorite.pathname)}</Text>
+              </TextContent>
+            </CardBody>
+          </Card>
+        </ChromeLink>
+      </GalleryItem>
+    ))
+  }
 
   return (
     <React.Fragment>
       <Title 
         headingLevel='h2'>
-        <Icon className="chr-c-icon-service-tab pf-u-ml-md" status="warning">
+        <Icon className="chr-c-icon-favorites pf-u-ml-md" status="warning">
           <StarIcon />
         </Icon> 
         My favorite services
+        <ChromeLink href="/allservices" className='landing-all-services-link'>
+          View all services
+        </ChromeLink>
       </Title>
+
       {favoritedServices.length === 0 ? (
         <EmptyState />
       ) : (
-        <Pagination
-          isCompact
-          variant="top">
+        <React.Fragment>
+          <Pagination
+            perPageComponent="button"
+            isCompact
+            variant="top"
+            itemCount={favoritedServices.length}
+            page={page}
+            perPage={perPage}
+            onPerPageSelect={onPerPageSelect}
+            onSetPage={onSetPage}
+            widgetId="favorites-cards-pagination"></Pagination> 
           <Gallery hasGutter className="hr-c-tile-landing">
-            {favoritedServices.map((favorite, index) => (
-              <GalleryItem>
-                {/* <Tile isDisplayLarge={true} title={favoriteName(favorite.name)} isSelected={false} onClick={() => routeChange(favorite.pathname)}>
-                  {getBundle(favorite.pathname)}
-                </Tile> */}
-                <ChromeLink href={favorite.pathname} className="chr-c-favorite-service__tile">
-                  <Card isFullHeight isFlat isSelectableRaised>
-                    <CardBody className="pf-u-p-md">
-                      {favoriteName(favorite.name)}
-                      <TextContent>
-                        <Text component="small">{getBundle(favorite.pathname)}</Text>
-                      </TextContent>
-                    </CardBody>
-                  </Card>
-                </ChromeLink>
-              </GalleryItem>
-            ))}  
+            {buildFavorites()}
           </Gallery>
-        </Pagination>       
+          </React.Fragment>
       )}
     </React.Fragment>
   )
