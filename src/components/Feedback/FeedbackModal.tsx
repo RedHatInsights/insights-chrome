722dbe8c6a13f748e7dd691c1e15d7f7e274da2b
Donald Labaj <dlabaj@redhat.com>
2023-05-17 12:55:11 -0400

    feat(feedback-extension): Replaced feedback modal with feedback-user-extention

--- src/components/Feedback/FeedbackModal.tsx
+++ src/components/Feedback/FeedbackModal.tsx
@@ -1,19 +1,6 @@
-import React, { memo, useState } from 'react';
-import {
-  Button,
-  Card,
-  CardBody,
-  CardTitle,
-  Grid,
-  GridItem,
-  Label,
-  Modal,
-  ModalVariant,
-  Text,
-  TextContent,
-  TextVariants,
-} from '@patternfly/react-core';
-import { ExternalLinkAltIcon, OutlinedCommentsIcon } from '@patternfly/react-icons';
+import React, { memo } from 'react';
+import { Button, Label } from '@patternfly/react-core';
+import { OutlinedCommentsIcon } from '@patternfly/react-icons';
 import { ChromeUser } from '@redhat-cloud-services/types';
 import { useDispatch, useSelector } from 'react-redux';
 import { useIntl } from 'react-intl';
@@ -111,141 +98,6 @@ const FeedbackModal = memo(({ user }: FeedbackModalProps) => {
   const isAvailable = env === 'prod' || env === 'stage';
   const setIsModalOpen = (isOpen: boolean) => dispatch(toggleFeedbackModal(isOpen));
 
-  // const ModalDescription = ({ modalPage }: { modalPage: FeedbackPages }) => {
-  //   switch (modalPage) {
-  //     case 'feedbackHome':
-  //       return (
-  //         <div className="chr-c-feedback-content">
-  //           <TextContent>
-  //             <Text component={TextVariants.h1}>{intl.formatMessage(messages.tellAboutExperience)}</Text>
-  //             <Text>{intl.formatMessage(messages.helpUsImproveHCC)}</Text>
-  //           </TextContent>
-  //           <div className="chr-c-feedback-cards">
-  //             <Card isSelectableRaised isCompact onClick={() => setModalPage('feedbackOne')}>
-  //               <CardTitle className="chr-c-feedback-card-title">{intl.formatMessage(messages.shareFeedback)}</CardTitle>
-  //               <CardBody>{intl.formatMessage(messages.howIsConsoleExperience)}</CardBody>
-  //             </Card>
-  //             <br />
-  //             <Card isSelectableRaised isCompact onClick={() => setModalPage('reportBugOne')}>
-  //               <CardTitle className="chr-c-feedback-card-title">{intl.formatMessage(messages.reportABug)}</CardTitle>
-  //               <CardBody>{intl.formatMessage(messages.describeBugUrgentCases)}</CardBody>
-  //             </Card>
-  //             <br />
-  //             <Card
-  //               isSelectableRaised
-  //               isCompact
-  //             >
-  //               <CardTitle className="chr-c-feedback-card-title">
-  //                 <Text>
-  //                   {intl.formatMessage(messages.openSupportCase)} <ExternalLinkAltIcon />
-  //                 </Text>
-  //               </CardTitle>
-  //               <CardBody>{intl.formatMessage(messages.getSupport)}</CardBody>
-  //             </Card>
-  //             <br />
-  //             <Card isSelectableRaised isCompact onClick={() => setModalPage('informDirection')}>
-  //               <CardTitle className="chr-c-feedback-card-title">
-  //                 <Text>{intl.formatMessage(messages.informRedhatDirection)}</Text>
-  //               </CardTitle>
-  //               <CardBody>{intl.formatMessage(messages.learnAboutResearchOpportunities)}</CardBody>
-  //             </Card>
-  //           </div>
-  //           <Button className="chr-c-feedback-button" ouiaId="cancel-feedback" key="cancel" variant="link" onClick={handleCloseModal}>
-  //             {intl.formatMessage(messages.cancel)}
-  //           </Button>
-  //         </div>
-  //       );
-  //     case 'feedbackOne':
-  //       return (
-  //         <FeedbackForm
-  //           user={user}
-  //           onCloseModal={handleCloseModal}
-  //           onSubmit={() => setModalPage('feedbackSuccess')}
-  //           onClickBack={() => setModalPage('feedbackHome')}
-  //           handleFeedbackError={() => setModalPage('feedbackError')}
-  //           modalTitle={intl.formatMessage(messages.shareYourFeedback)}
-  //           textareaLabel={intl.formatMessage(messages.enterFeedback)}
-  //           feedbackType="Feedback"
-  //           checkboxDescription={intl.formatMessage(messages.learnAboutResearchOpportunities)}
-  //           submitTitle={intl.formatMessage(messages.submitFeedback)}
-  //         />
-  //       );
-  //     case 'reportBugOne':
-  //       return (
-  //         <FeedbackForm
-  //           user={user}
-  //           onCloseModal={handleCloseModal}
-  //           onSubmit={() => setModalPage('bugReportSuccess')}
-  //           onClickBack={() => setModalPage('feedbackHome')}
-  //           handleFeedbackError={() => setModalPage('feedbackError')}
-  //           modalTitle={intl.formatMessage(messages.reportABug)}
-  //           modalDescription={
-  //             <Text>
-  //               {intl.formatMessage(messages.describeReportBug)}{' '}
-  //               <Text component="a" href="https://access.redhat.com/support/cases/#/case/new/open-case?caseCreate=true" target="_blank">
-  //                 {intl.formatMessage(messages.openSupportCase)} <ExternalLinkAltIcon />
-  //               </Text>
-  //             </Text>
-  //           }
-  //           feedbackType="Bug"
-  //           checkboxDescription={`${intl.formatMessage(messages.learnAboutResearchOpportunities)} ${intl.formatMessage(
-  //             messages.weNeverSharePersonalInformation
-  //           )}`}
-  //           submitTitle={intl.formatMessage(messages.submitFeedback)}
-  //         />
-  //       );
-  //     case 'informDirection':
-  //       return (
-  //         <FeedbackForm
-  //           user={user}
-  //           onCloseModal={handleCloseModal}
-  //           onSubmit={() => setModalPage('informDirectionSuccess')}
-  //           onClickBack={() => setModalPage('feedbackHome')}
-  //           handleFeedbackError={() => setModalPage('feedbackError')}
-  //           modalTitle={intl.formatMessage(messages.informRedhatDirection)}
-  //           modalDescription={
-  //             <Text>
-  //               {intl.formatMessage(messages.informDirectionDescription)}
-  //               <Text component="a" href="https://www.redhat.com/en/about/user-research" target="_blank">
-  //                 {intl.formatMessage(messages.userResearchTeam)}
-  //               </Text>
-  //               {intl.formatMessage(messages.directInfluence)}
-  //             </Text>
-  //           }
-  //           feedbackType="[Research Opportunities]"
-  //           textAreaHidden={true}
-  //           checkboxDescription={intl.formatMessage(messages.weNeverSharePersonalInformation)}
-  //           submitTitle={intl.formatMessage(messages.joinMailingList)}
-  //         />
-  //       );
-  //     case 'feedbackSuccess':
-  //       return (
-  //         <FeedbackSuccess
-  //           successTitle={intl.formatMessage(messages.feedbackSent)}
-  //           successDescription={intl.formatMessage(messages.thankYouForFeedback)}
-  //           onCloseModal={handleCloseModal}
-  //         />
-  //       );
-  //     case 'bugReportSuccess':
-  //       return (
-  //         <FeedbackSuccess
-  //           successTitle={intl.formatMessage(messages.bugReported)}
-  //           successDescription={intl.formatMessage(messages.teamWillReviewBug)}
-  //           onCloseModal={handleCloseModal}
-  //         />
-  //       );
-  //     case 'informDirectionSuccess':
-  //       return (
-  //         <FeedbackSuccess
-  //           s
-  //           onCloseModal={handleCloseModal}
-  //         />
-  //       );
-  //     case 'feedbackError':
-  //       return <FeedbackError onCloseModal={handleCloseModal} />;
-  //   }
-  // };
-
   return (
     <React.Fragment>
       <Button
@@ -266,13 +118,14 @@ const FeedbackModal = memo(({ user }: FeedbackModalProps) => {
         <PFFeedbackModal
           email={user.identity.user.email}
           feedbackLocale={feedbackLocale}
-          onShareFeedback={(_email, feedback) => {
-            handleModalSubmission('Feedback', feedback).then((success) => {
-              if (success === true) {
-                return true;
-              }
-              return false;
-            });
+          onShareFeedback={(email, feedback) => {
+            return handleModalSubmission('Feedback', feedback, email ? true : false);
+          }}
+          onReportABug={(email, bug) => {
+            return handleModalSubmission('Feedback', bug, email ? true : false);
+          }}
+          onJoinMailingList={(email) => {
+            return handleModalSubmission('[Research Opportunities]', email);
           }}
           onOpenSupportCase="https://access.redhat.com/support/cases/#/case/new/open-case?caseCreate=true"
           feedbackImg={feedbackIllo}
