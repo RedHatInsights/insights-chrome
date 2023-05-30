0d216440aaab0f1c1ba519db01f568127d502fa7
Donald Labaj <dlabaj@redhat.com>
2023-05-30 09:29:45 -0400

    Fixed jest test

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     url: 'https://test.com',
   },
   transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!@redhat-cloud-services|@openshift|lodash-es|@patternfly/react-core/components|@patternfly/react-core/next|@patternfly/react-core/helpers|@patternfly/react-core/node_modules/@patternfly/react-tokens/dist/esm|@patternfly/react-icons/dist/esm).+(js|jsx)$',
+    '<rootDir>/node_modules/(?!@redhat-cloud-services|@openshift|lodash-es|@patternfly/react-core/components|@patternfly/react-user-feedback|@patternfly/react-core/next|@patternfly/react-core/helpers|@patternfly/react-core/node_modules/@patternfly/react-tokens/dist/esm|@patternfly/react-icons/dist/esm).+(js|jsx)$',
   ],
     transform: {
     '^.+\\.(ts|js)x?$': [
