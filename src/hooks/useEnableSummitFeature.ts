064dca3715c03aab246002cd0a79aefe3f8997fe
Martin Maroši <marvusm.mmi@gmail.com>
2023-05-30 14:01:57 +0200

    Remove summit feature flag conditions. (#2530)

--- src/hooks/useEnableSummitFeature.ts
+++
@@ -1,9 +0,0 @@
-import { useFlag } from '@unleash/proxy-client-react';
-import { isBeta } from '../utils/common';
-
-const useEnableSummitFeature = () => {
-  const flagEnabled = useFlag('platform.chrome.navigation-dropdown');
-  return flagEnabled || isBeta();
-};
-
-export default useEnableSummitFeature;
