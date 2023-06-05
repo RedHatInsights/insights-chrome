75667241fb65aca81634f3fa90125ada464d6d9f
Karel Hala <khala@redhat.com>
2023-06-05 17:37:42 +0200

    Use session to store login creds

--- cypress/e2e/release-gate/landing-page.cy.ts
+++ cypress/e2e/release-gate/landing-page.cy.ts
@@ -1,8 +1,15 @@
 describe('Landing page', () => {
   it('visit landing page', () => {
+    cy.login();
+
     cy.visit('/');
 
-    cy.login();
+    cy.intercept({
+      method: 'GET',
+      url: '**/services/services.json',
+    }).as('services');
+
+    cy.wait('@services').its('response.statusCode').should('equal', 200);
 
     // check if a favorites link exists on the page
     cy.contains('View my favorite services').should('exist');
