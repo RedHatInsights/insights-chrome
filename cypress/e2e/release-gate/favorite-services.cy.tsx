6ef27513573a664e9f70b07615a1aa8c8388848a
rsun19 <robertssun1234@gmail.com>
2023-06-08 10:11:34 -0400

    added assertions and fixed post intercept issue

--- cypress/e2e/release-gate/favorite-services.cy.tsx
+++ cypress/e2e/release-gate/favorite-services.cy.tsx
@@ -44,31 +44,44 @@ describe('Favorite-services', () => {
                 "application-services":true
             }
         }
-        }
-      );
+    });
     // check if a favorites link exists on the page
     cy.get('.pf-c-menu-toggle__text').click();
     cy.contains(dropDownService).click({ force: true });
     cy.get('.pf-c-icon__content').eq(serviceIndexInMenu+3).click({ force: true });
-    cy.intercept('POST', '/api/chrome-service/v1/favorite-pages').as('postRequest');
-    cy.wait('@postRequest', { timeout: 1200 })
-    .then((interceptions) => {
+    cy.intercept('POST', '/api/chrome-service/v1/favorite-pages',
+    {
+        "data": [
+            {
+                "id":4,
+                "createdAt":"2023-01-13T12:50:22.095031Z",
+                "updatedAt":"2023-01-13T12:56:05.377946Z",
+                "deletedAt":null,
+                "pathname":service,
+                "favorite":true,
+                "userIdentityId":1
+            }],
+        "meta": {
+            "count":1,
+            "total":1
+        }
+    }).then((interceptions) => {
         interceptionCounter = true;
     });
-    cy.screenshot();
+    cy.get('span.pf-c-icon.pf-u-ml-sm.chr-c-icon-star.favorite').should('exist');
     cy.get('.pf-c-brand').click();
     cy.reload();
     cy.intercept('GET', '/api/chrome-service/v1/user', 
         {
-            "data":{
-            "id":24353452,
-            "createdAt":"2023-05-15T18:08:27.38611Z",
-            "updatedAt":"2023-06-06T15:04:05.366605Z",
-            "deletedAt":null,
-            "accountId":"56118163",
-            "firstLogin":true,
-            "dayOne":true,
-            "lastLogin":"2023-05-15T18:08:27.376277Z",
+            "data": {
+            "id": 24353452,
+            "createdAt": "2023-05-15T18:08:27.38611Z",
+            "updatedAt": "2023-06-06T15:04:05.366605Z",
+            "deletedAt": null,
+            "accountId": "56118163",
+            "firstLogin": true,
+            "dayOne": true,
+            "lastLogin": "2023-05-15T18:08:27.376277Z",
             "lastVisitedPages": [
                 {
                     "id":2345234534,
@@ -83,34 +96,34 @@ describe('Favorite-services', () => {
             ],
             "favoritePages": [
                 {
-                    "id":1957,
-                    "createdAt":"2023-06-05T18:17:26.849084Z",
-                    "updatedAt":"2023-06-06T19:23:32.813832Z",
-                    "deletedAt":null,
-                    "pathname":service,
-                    "favorite":true,
-                    "userIdentityId":453245
+                    "id": 1957,
+                    "createdAt": "2023-06-05T18:17:26.849084Z",
+                    "updatedAt": "2023-06-06T19:23:32.813832Z",
+                    "deletedAt": null,
+                    "pathname": service,
+                    "favorite": true,
+                    "userIdentityId": 453245
                 }
             ],
             "selfReport": {
-                "createdAt":"0001-01-01T00:00:00Z",
-                "updatedAt":"0001-01-01T00:00:00Z",
-                "deletedAt":null,
-                "productsOfInterest":null,
-                "jobRole":"",
-                "userIdentityID":0
+                "createdAt": "0001-01-01T00:00:00Z",
+                "updatedAt": "0001-01-01T00:00:00Z",
+                "deletedAt": null,
+                "productsOfInterest": null,
+                "jobRole": "",
+                "userIdentityID": 0
             },
             "visitedBundles": {
-                "apps":true,
-                "ansible":true,
-                "landing":true,
-                "staging":true,
-                "insights":true,
-                "internal":true,
-                "openshift":true,
-                "allservices":true,
-                "favoritedservices":true,
-                "application-services":true
+                "apps": true,
+                "ansible": true,
+                "landing": true,
+                "staging": true,
+                "insights": true,
+                "internal": true,
+                "openshift": true,
+                "allservices": true,
+                "favoritedservices": true,
+                "application-services": true
             }
             }
         }
@@ -121,7 +134,6 @@ describe('Favorite-services', () => {
         });
         cy.wait(2000);
         cy.contains('API Management').should('exist');
-        cy.screenshot();
     });
 });
   
