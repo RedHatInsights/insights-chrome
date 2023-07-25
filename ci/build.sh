d88c26428483cf70e8bb45b1b5d4d3fc56fa1396
Matt Knop <mknop@redhat.com>
2023-07-25 14:59:39 -0600

    Changed name of container for lint

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 source ./ci/helpers.sh
 
 PROJECT_NAME="insights-chrome"
-TEST_CONT="${PROJECT_NAME}-lint"
+TEST_CONT="${PROJECT_NAME}-build"
 IMG_TAG=$(git rev-parse --short=8 HEAD)
 
 docker build -t "${TEST_CONT}:${IMG_TAG}" -f Dockerfile.test .
