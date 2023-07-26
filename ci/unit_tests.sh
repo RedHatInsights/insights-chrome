51b2d9e936d4b18e90367304f8db1292c3a93283
Matt Knop <mknop@redhat.com>
2023-07-26 12:55:41 -0600

    Wrapping commands in strings

--- ci/unit_tests.sh
+++ ci/unit_tests.sh
@@ -4,7 +4,7 @@ source ./ci/helpers.sh
 
 PROJECT_NAME="insights-chrome"
 TEST_CONT="${PROJECT_NAME}-unit-tests"
-IMG_TAG=$(git rev-parse --short=8 HEAD)
+IMG_TAG="$(git rev-parse --short=8 HEAD)"
 
 docker build -t "${TEST_CONT}:${IMG_TAG}" -f Dockerfile.test .
 
