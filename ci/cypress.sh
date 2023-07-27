e16f98f0d96de172a093f8f0ce6772b1d8bc41ef
Matt Knop <mknop@redhat.com>
2023-07-27 14:00:04 -0600

    Checking file in dir

--- ci/cypress.sh
+++ ci/cypress.sh
@@ -10,6 +10,8 @@ docker run --name "${TEST_CONT}-${IMG_TAG}" -d -i "${CYPRESS_TEST_IMAGE}" /bin/s
 
 docker cp . "${TEST_CONT}-${IMG_TAG}:/e2e/"
 
+docker exec -i -w "/e2e/" "${TEST_CONT}-${IMG_TAG}" sh -c "ls -l"
+
 docker exec -i -w "/e2e/" "${TEST_CONT}-${IMG_TAG}" sh -c "npm install"
 
 docker exec -i -w "/e2e/" "${TEST_CONT}-${IMG_TAG}" sh -c "npm run test:ct"
