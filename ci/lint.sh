d67c5c364698d5f486a797a199eb05cee9c279ed
Matt Knop <mknop@redhat.com>
2023-07-27 10:15:10 -0600

    Fixed container names for each stage

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT_NAME="insights-chrome"
-TEST_CONT="${PROJECT_NAME}-unit-tests"
+TEST_CONT="${PROJECT_NAME}-lint"
 IMG_TAG=$(git rev-parse --short=8 HEAD)
 
 docker run --name "${TEST_CONT}-${IMG_TAG}" -d -i "${NODE_BASE_IMAGE}" /bin/sh
