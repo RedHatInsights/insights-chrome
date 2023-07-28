957c4618e97fd2add717085cde17a688444a8ef7
Matt Knop <mknop@redhat.com>
2023-07-28 11:07:54 -0600

    Updates to cypress file

--- ci/cypress.sh
+++ ci/cypress.sh
@@ -4,14 +4,16 @@ PROJECT_NAME="insights-chrome"
 TEST_CONT="${PROJECT_NAME}-cypress"
 IMG_TAG=$(git rev-parse --short=8 HEAD)
 
+chown -R node:node package*.json
+
 docker login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
 docker run --name "${TEST_CONT}-${IMG_TAG}" -d -i "${CYPRESS_TEST_IMAGE}" /bin/sh
 
 docker cp . "${TEST_CONT}-${IMG_TAG}:/e2e/"
 
-docker exec -i -w "/e2e/" "${TEST_CONT}-${IMG_TAG}" sh -c "ls -l"
+docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "pwd"
 
-docker exec -i -w "/e2e/" "${TEST_CONT}-${IMG_TAG}" sh -c "npm install"
+docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "npm install"
 
-docker exec -i -w "/e2e/" "${TEST_CONT}-${IMG_TAG}" sh -c "cd /e2e && npm run test:e2e"
+docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "npm run test:e2e"
