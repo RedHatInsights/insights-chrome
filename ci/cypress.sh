dd0049790cc836831013ed8c336a5ae64f29d026
Matt Knop <mknop@redhat.com>
2023-07-28 13:24:49 -0600

    Removing command

--- ci/cypress.sh
+++ ci/cypress.sh
@@ -6,11 +6,9 @@ IMG_TAG=$(git rev-parse --short=8 HEAD)
 
 docker login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
-docker run --name "${TEST_CONT}-${IMG_TAG}" -d -i "${CYPRESS_TEST_IMAGE}" /bin/sh
+docker run --name "${TEST_CONT}-${IMG_TAG}" -d -i "${CYPRESS_TEST_IMAGE}" /bin/bash
 
 docker cp . "${TEST_CONT}-${IMG_TAG}:/e2e/"
-
-docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "pwd"
 
 docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "npm install"
 
