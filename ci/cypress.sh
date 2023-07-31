a075cf496091953b425a1531af9c0152290fdd53
Matt Knop <mknop@redhat.com>
2023-07-31 11:03:25 -0600

    Revert to docker

--- ci/cypress.sh
+++ ci/cypress.sh
@@ -4,16 +4,16 @@ PROJECT_NAME="insights-chrome"
 TEST_CONT="${PROJECT_NAME}-cypress"
 IMG_TAG=$(git rev-parse --short=8 HEAD)
 
-podman login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+docker login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
-podman run --name "${TEST_CONT}-${IMG_TAG}" -d -i "${CYPRESS_TEST_IMAGE}" /bin/bash
+docker run --name "${TEST_CONT}-${IMG_TAG}" -d -i "${CYPRESS_TEST_IMAGE}" /bin/bash
 
-podman cp . "${TEST_CONT}-${IMG_TAG}:/e2e/"
+docker cp . "${TEST_CONT}-${IMG_TAG}:/e2e/"
 
-podman exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "ls -lrd ."
-podman exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "ls -l *"
-podman exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "whoami"
+docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "ls -lrd ."
+docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "ls -l *"
+docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "whoami"
 
-podman exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "npm install"
+docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "npm install"
 
-podman exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "npm run test:ct"
+docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "npm run test:ct"
