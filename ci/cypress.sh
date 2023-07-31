3ce948082cd62404a1fae604a5781d5d3fba27bc
Matt Knop <mknop@redhat.com>
2023-07-31 12:18:30 -0600

    -a flag

--- ci/cypress.sh
+++ ci/cypress.sh
@@ -4,13 +4,11 @@ PROJECT_NAME="insights-chrome"
 TEST_CONT="${PROJECT_NAME}-cypress"
 IMG_TAG=$(git rev-parse --short=8 HEAD)
 
-chown -R tester /e2e
-
 docker login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
 docker run --name "${TEST_CONT}-${IMG_TAG}" -d -i "${CYPRESS_TEST_IMAGE}" /bin/bash
 
-docker cp . "${TEST_CONT}-${IMG_TAG}:/e2e/"
+docker cp -a "${TEST_CONT}-${IMG_TAG}:/e2e/"
 
 docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "ls -lrd ."
 docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "ls -l *"
