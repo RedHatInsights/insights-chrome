adcb97318ac7ab38052d30081177567c262b7157
Matt Knop <mknop@redhat.com>
2023-07-31 11:11:54 -0600

    Changing perms before cp to container

--- ci/cypress.sh
+++ ci/cypress.sh
@@ -4,6 +4,8 @@ PROJECT_NAME="insights-chrome"
 TEST_CONT="${PROJECT_NAME}-cypress"
 IMG_TAG=$(git rev-parse --short=8 HEAD)
 
+chown -R tester:tester .
+
 docker login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
 docker run --name "${TEST_CONT}-${IMG_TAG}" -d -i "${CYPRESS_TEST_IMAGE}" /bin/bash
