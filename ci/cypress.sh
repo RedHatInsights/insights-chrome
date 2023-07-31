bc42af0d593c461b79b16469cbb408928563a6e4
Matt Knop <mknop@redhat.com>
2023-07-31 12:22:02 -0600

    Fixing parameter issue

--- ci/cypress.sh
+++ ci/cypress.sh
@@ -8,7 +8,7 @@ docker login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
 docker run --name "${TEST_CONT}-${IMG_TAG}" -d -i "${CYPRESS_TEST_IMAGE}" /bin/bash
 
-docker cp -a "${TEST_CONT}-${IMG_TAG}:/e2e/"
+docker cp -a . "${TEST_CONT}-${IMG_TAG}:/e2e/"
 
 docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "ls -lrd ."
 docker exec -i "${TEST_CONT}-${IMG_TAG}" sh -c "ls -l *"
