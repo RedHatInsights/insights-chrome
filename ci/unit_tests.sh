984df296c668a982e46e9a44ea844fb6f2416c82
Victor M <vmugicag@redhat.com>
2023-08-01 11:57:28 +0200

    debug

--- ci/unit_tests.sh
+++ ci/unit_tests.sh
@@ -5,8 +5,13 @@ IMG_TAG=$(git rev-parse --short=8 HEAD)
 
 docker run --name "${TEST_CONT}-${IMG_TAG}" -d -i --rm "${NODE_BASE_IMAGE}" /bin/sh
 
-docker cp . "${TEST_CONT}-${IMG_TAG}:/opt/app-root/src/"
-
-docker exec -i -w "/opt/app-root/src/" "${TEST_CONT}-${IMG_TAG}" sh -c "npm install"
-
-docker exec -i -w "/opt/app-root/src/" "${TEST_CONT}-${IMG_TAG}" sh -c "npm run test -- --coverage"
+docker exec -it "${TEST_CONT}-${IMG_TAG}" ls -lrt
+echo "copying..."
+time docker cp . "${TEST_CONT}-${IMG_TAG}:/opt/app-root/src/"
+docker exec -it "${TEST_CONT}-${IMG_TAG}" ls -lrt
+echo "installing ..."
+time docker exec -i -w "/opt/app-root/src/" "${TEST_CONT}-${IMG_TAG}" sh -c "npm install"
+docker exec -it "${TEST_CONT}-${IMG_TAG}" ls -lrt
+echo "running tests..."
+time docker exec -i -w "/opt/app-root/src/" "${TEST_CONT}-${IMG_TAG}" sh -c "npm run test -- --coverage"
+docker exec -it "${TEST_CONT}-${IMG_TAG}" ls -lrt
