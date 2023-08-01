c9f5aa0a47e44c72996ed8b113100bdd1e062ed7
Victor M <vmugicag@redhat.com>
2023-08-01 12:56:53 +0200

    test non interactive

--- ci/unit_tests.sh
+++ ci/unit_tests.sh
@@ -5,17 +5,18 @@ CONTAINER_NAME="${TEST_CONT}-${IMG_TAG}"
 IMG_TAG=$(git rev-parse --short=8 HEAD)
 
 
-docker run --name "${CONTAINER_NAME}" -d -it --rm "${NODE_BASE_IMAGE}" /bin/sh
+docker run --name "${CONTAINER_NAME}" -d -t --rm "${NODE_BASE_IMAGE}"
 
-docker exec -it "$CONTAINER_NAME" ls -lrt
+docker exec "$CONTAINER_NAME" ls -lrt
+docker exec "$CONTAINER_NAME" pwd
 echo "copying..."
 time docker cp . "${CONTAINER_NAME}:/opt/app-root/src/"
-docker exec -it "$CONTAINER_NAME" ls -lrt
+docker exec "$CONTAINER_NAME" ls -lrt
 echo "installing ..."
 time docker exec -i -w "/opt/app-root/src/" "$CONTAINER_NAME" sh -c "npm install"
-docker exec -it "$CONTAINER_NAME" ls -lrt
+docker exec "$CONTAINER_NAME" ls -lrt
 echo "running tests..."
 time docker exec -i -w "/opt/app-root/src/" "$CONTAINER_NAME" sh -c "npm run test -- --coverage"
-docker exec -it "${CONTAINER_NAME}" ls -lrt
+docker exec "$CONTAINER_NAME" ls -lrt
 
-docker stop 
+docker stop "$CONTAINER_NAME"
