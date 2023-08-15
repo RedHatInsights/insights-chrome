c71f7d3373121b5878af474f239d46e16b28a452
Martin Maroši <marvusm.mmi@gmail.com>
2023-08-15 08:56:15 +0200

    Add missing chunk mapper hot reload options.

--- config/chunk-mapper.js
+++ config/chunk-mapper.js
@@ -1,36 +1,64 @@
-/* global require, module */
-const { resolve } = require('path');
-const { writeFileSync, mkdirSync } = require('fs');
-
+const hotChunkRegex = RegExp('\\.hot-update\\.js$');
 class ChunkMapper {
-  config = {};
   constructor(options) {
-    this.options = options || {};
+    const { _unstableHotReload, ...rest } = options;
+    this.config = {};
+    this.options = rest || {};
+    this._unstableHotReload = _unstableHotReload;
   }
 
   apply(compiler) {
     compiler.hooks.emit.tap('ChunkMapper', (compilation) => {
-      const prefix = this.options.prefix || RegExp('^/.*/$').test(compiler.options.output.publicPath) ? compiler.options.output.publicPath : '/';
-      compilation.chunks.forEach(({ id, files, runtime }) => {
+      const prefix = this.options.prefix || (RegExp('^/.*/$').test(compiler.options.output.publicPath) ? compiler.options.output.publicPath : '/');
+      if (this._unstableHotReload) {
         const modules = Array.isArray(this.options.modules) ? this.options.modules : [this.options.modules];
-        if (modules.find((oneEntry) => RegExp(`${oneEntry}$`).test(runtime))) {
-          this.config[runtime] = {
-            ...(this.config[runtime] || {}),
-            ...(id === runtime
-              ? { entry: Array.from(files).map((item) => `${prefix}${item}`) }
-              : {
-                  modules: [
-                    ...((this.config[runtime] && this.config[runtime].modules) || []),
-                    ...Array.from(files).map((item) => `${prefix}${item}`),
-                  ],
-                }),
-          };
-        }
-      });
+        // TODO: Investigate if it is possible we could use the same script for normal builds
+        const hotEntries = Array.from(compilation.chunks)
+          .filter(({ name, runtime }) => !!modules.find((moduleName) => moduleName === name) || name === runtime)
+          .map(({ files }) =>
+            Array.from(files)
+              .filter((file) => !hotChunkRegex.test(file))
+              .map((file) => `${prefix}${file}`)
+          )
+          .flat();
 
-      const outputPath = this.options.output || compiler.options.output.path;
-      mkdirSync(resolve(outputPath), { recursive: true });
-      writeFileSync(`${resolve(outputPath, 'fed-mods.json')}`, JSON.stringify(this.config, null, 4));
+        modules.forEach((name) => {
+          this.config = {
+            [name]: {
+              entry: hotEntries,
+            },
+          };
+        });
+      } else {
+        compilation.chunks.forEach(({ name, files, runtime }) => {
+          const modules = Array.isArray(this.options.modules) ? this.options.modules : [this.options.modules];
+          if (modules.find((oneEntry) => RegExp(`${oneEntry}$`).test(runtime))) {
+            this.config[runtime] = {
+              ...(this.config[runtime] || {}),
+              ...(name === runtime
+                ? {
+                    entry: Array.from(files)
+                      .map((item) => `${prefix}${item}`)
+                      .filter((file, _index, array) => {
+                        if (array.find((item) => !hotChunkRegex.test(item))) {
+                          return !hotChunkRegex.test(file);
+                        }
+
+                        return true;
+                      }),
+                  }
+                : {
+                    modules: [...(this.config[runtime].modules || []), ...Array.from(files).map((item) => `${prefix}${item}`)],
+                  }),
+            };
+          }
+        });
+      }
+
+      compilation.assets['fed-mods.json'] = {
+        source: () => JSON.stringify(this.config, null, 4),
+        size: () => JSON.stringify(this.config, null, 4).length,
+      };
     });
   }
 }
