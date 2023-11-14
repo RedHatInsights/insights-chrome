816542fdd4994240d76629534607bc2452abae32
Martin Marosi <marvusm.mmi@gmail.com>
2023-11-14 16:02:23 +0100

    Collect coverage from cypress component tests.

--- config/webpack.cy.config.js
+++ config/webpack.cy.config.js
@@ -32,6 +32,16 @@ const JSConfig = {
           loader: 'swc-loader',
           options: {
             jsc: {
+              experimental: {
+                plugins: [
+                  [
+                    'swc-plugin-coverage-instrument',
+                    {
+                      compact: false,
+                    },
+                  ],
+                ],
+              },
               parser: {
                 syntax: 'typescript',
                 tsx: true,
