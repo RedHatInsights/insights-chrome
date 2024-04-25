375ba13290e318974f408e2949f417390facccb5
Brandon Tweed <btweed@redhat.com>
2024-04-25 13:28:26 -0500

    Test change to trigger PR check tests

--- pr_check.sh
+++ pr_check.sh
@@ -59,6 +59,7 @@ export IMAGE="quay.io/cloudservices/insights-chrome-frontend"
 export GIT_COMMIT=master
 export DEPLOY_FRONTENDS=true
 source $CICD_ROOT/deploy_ephemeral_env.sh
+echo "Deployment complete"
 
 echo "Taking a short nap to let the deployment stabilize"
 sleep 60
