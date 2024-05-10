8e25d2eacd429be3089fa26448c680d5e94cd8df
Bryan Florkiewicz <bflorkie@redhat.com>
2024-05-10 00:52:18 -0400

    RHCLOUD-32008: create internal jotai state for preview
    Adds an internal state variable via jotai to track if preview is enabled or disabled.
    
    Tested locally by adding a sub in the store with a console log (see screenshots). The state is correctly toggled with the preview toggle and initialized when chrome loads.

--- src/state/chromeStore.ts
+++ src/state/chromeStore.ts
@@ -1,12 +1,15 @@
 import { createStore } from 'jotai';
 import { activeModuleAtom } from './atoms/activeModuleAtom';
 import { contextSwitcherOpenAtom } from './atoms/contextSwitcher';
+import { isPreviewAtom } from './atoms/releaseAtom';
+import { isBeta } from '../utils/common';
 
 const chromeStore = createStore();
 
 // setup initial chrome store state
 chromeStore.set(contextSwitcherOpenAtom, false);
 chromeStore.set(activeModuleAtom, undefined);
+chromeStore.set(isPreviewAtom, isBeta());
 
 // globally handle subscription to activeModuleAtom
 chromeStore.sub(activeModuleAtom, () => {
