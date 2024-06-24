962041322df0f168848444114d20026ac8193357
Janet Cobb <jacobb@redhat.com>
2024-06-24 15:05:59 -0400

    Add comment explaining versions

--- src/outdated-browser-rework.js
+++ src/outdated-browser-rework.js
@@ -1,4 +1,5 @@
 import outdatedBrowser from 'outdated-browser-rework';
 import 'outdated-browser-rework/dist/style.css';
 
+// Use the default versions until we have something meaningful to add?
 outdatedBrowser();
