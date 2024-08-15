d6da3d227b3cf964e54b2c3b71f5f990fcdefdea
Martin Marosi <marvusm.mmi@gmail.com>
2024-08-15 09:52:53 +0200

    Add option to disable PF4 styling.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import './sass/chrome.scss';
 import { chunkLoadErrorRefreshKey } from './utils/common';
+import { CHROME_PF4_DEBUG } from './utils/debugFunctions';
+import removePf4Styles from './utils/removePf4Styles';
+
+if (localStorage.getItem(CHROME_PF4_DEBUG)) {
+  removePf4Styles();
+}
 
 Object.keys(localStorage).map((key) => {
   if (key.includes(chunkLoadErrorRefreshKey)) {
