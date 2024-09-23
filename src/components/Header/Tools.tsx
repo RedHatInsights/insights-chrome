b1bb6dfbfff004be197d9f02daed7315b838a711
ewinchel <ewinchel@redhat.com>
2024-09-23 09:49:11 -0400

    lint fix

--- src/components/Header/Tools.tsx
+++ src/components/Header/Tools.tsx
@@ -83,9 +83,6 @@ const Tools = () => {
 
   const isNotificationsEnabled = useFlag('platform.chrome.notifications-drawer');
 
-  const enableMyUserAccessLanding = useFlag('platform.chrome.my-user-access-landing-page');
-  const myUserAccessPath = enableMyUserAccessLanding ? '/iam/user-access/overview' : '/iam/my-user-access';
-
   /* list out the items for the settings menu */
   const settingsMenuDropdownGroups = [
     {
