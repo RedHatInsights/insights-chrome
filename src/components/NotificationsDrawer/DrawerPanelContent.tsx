05f7de692da46bba631741538401f2ac09409263
Justin Orringer <justinorringer@gmail.com>
2025-02-11 14:44:43 -0500

    Moving stuff out of panelcontent

--- src/components/NotificationsDrawer/DrawerPanelContent.tsx
+++ src/components/NotificationsDrawer/DrawerPanelContent.tsx
@@ -1,13 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 
 import { NotificationDrawer } from '@patternfly/react-core/dist/dynamic/components/NotificationDrawer';
 
 import ChromeAuthContext from '../../auth/ChromeAuthContext';
-import InternalChromeContext from '../../utils/internalChromeContext';
 
 import { getSharedScope } from '@scalprum/core';
-import { useAtomValue, useSetAtom } from 'jotai';
-import { notificationDrawerReadyAtom, notificationDrawerUnreadAtom } from '../../state/atoms/notificationDrawerAtom';
 
 export type DrawerPanelProps = {
   panelRef: React.Ref<unknown>;
@@ -18,37 +15,13 @@ const NOTIF_DRAWER_MODULE = '@notif-module/drawer';
 const DrawerPanelBase: React.FC<DrawerPanelProps> = ({ panelRef, toggleDrawer }) => {
   const auth = useContext(ChromeAuthContext);
   const isOrgAdmin = auth?.user?.identity?.user?.is_org_admin;
-  const { getUserPermissions } = useContext(InternalChromeContext);
   const notificationProps = {
     isOrgAdmin: isOrgAdmin,
-    getUserPermissions: getUserPermissions,
     panelRef: panelRef,
     toggleDrawer: toggleDrawer,
   };
 
-  const [initialized, setInitialized] = useState(false);
-  const { DrawerPanel, useNotificationsDrawer } = getSharedScope()[NOTIF_DRAWER_MODULE]['1.0.0'].get();
-  const isNotificationsDrawerReady = useAtomValue(notificationDrawerReadyAtom);
-  const { state, initialize, hasUnreadNotifications } = useNotificationsDrawer();
-  const setUnreadNotifiations = useSetAtom(notificationDrawerUnreadAtom);
-
-  useEffect(() => {
-    if (!isNotificationsDrawerReady || initialized) return;
-    getUserPermissions('notifications')
-      .then((perms) => {
-        initialize(true, perms);
-        setUnreadNotifiations(hasUnreadNotifications());
-      })
-      .catch((err) => {
-        console.error('Error fetching user notifications permissions while rendering drawer content', err);
-      });
-    setInitialized(true);
-  }, [isNotificationsDrawerReady]);
-
-  useEffect(() => {
-    console.log(state, 'state');
-    setUnreadNotifiations(hasUnreadNotifications());
-  }, [state]);
+  const { DrawerPanel } = getSharedScope()[NOTIF_DRAWER_MODULE]['1.0.0'].get();
 
   return (
     <NotificationDrawer ref={panelRef} {...notificationProps}>
@@ -58,17 +31,7 @@ const DrawerPanelBase: React.FC<DrawerPanelProps> = ({ panelRef, toggleDrawer })
 };
 
 const DrawerPanel = React.forwardRef<unknown, Omit<DrawerPanelProps, 'panelRef'>>((props, innerRef) => {
-  const DrawerPanelProvider = () => {
-    const { DrawerContextProvider } = getSharedScope()[NOTIF_DRAWER_MODULE]['1.0.0'].get();
-
-    return (
-      <DrawerContextProvider>
-        <DrawerPanelBase panelRef={innerRef} {...props} />
-      </DrawerContextProvider>
-    );
-  };
-
-  return <DrawerPanelProvider />;
+  return <DrawerPanelBase panelRef={innerRef} {...props} />;
 });
 DrawerPanel.displayName = 'DrawerPanel';
 
