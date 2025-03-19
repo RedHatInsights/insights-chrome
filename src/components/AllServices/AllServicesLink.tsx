5ac8e855560c83a4c49d90c0fefb89552f548a08
Zein Sleiman <zsleiman@redhat.com>
2025-03-19 09:51:49 -0500

    re enable favoriting for old allservices

--- src/components/AllServices/AllServicesLink.tsx
+++ src/components/AllServices/AllServicesLink.tsx
@@ -8,10 +8,14 @@ import classNames from 'classnames';
 import { matchPath } from 'react-router-dom';
 
 import ExternalLinkAltIcon from '@patternfly/react-icons/dist/dynamic/icons/external-link-alt-icon';
+import StarIcon from '@patternfly/react-icons/dist/dynamic/icons/star-icon';
 
 import ChromeLink from '../ChromeLink';
 import { useAtomValue } from 'jotai';
 import { moduleRoutesAtom } from '../../state/atoms/chromeModuleAtom';
+import useFavoritePagesWrapper from '../../hooks/useFavoritePagesWrapper';
+import { useFlag } from '@unleash/proxy-client-react';
+import { titleToId } from '../../utils/common';
 
 interface AllServicesLinkProps {
   href?: string;
@@ -23,14 +27,26 @@ interface AllServicesLinkProps {
   group?: string;
 }
 
-const AllServicesLink = ({ href, title, sectionTitle, bundleTitle, isExternal = false }: AllServicesLinkProps) => {
+const AllServicesLink = ({ href, title, sectionTitle, bundleTitle, isExternal = false, category, group }: AllServicesLinkProps) => {
+  const enableAllServicesRedesign = useFlag('platform.chrome.allservices.redesign');
+
   const moduleRoutes = useAtomValue(moduleRoutesAtom);
   // Find service appId
   const appId = useMemo(() => {
     return moduleRoutes.find(({ path }) => matchPath(path, href ? href : '') || matchPath(`${path}/*`, href ? href : ''))?.scope;
   }, [moduleRoutes, href]);
+  const { favoritePage, unfavoritePage, favoritePages } = useFavoritePagesWrapper();
 
-  return (
+  const handleFavouriteToggle = (pathname: string, favorite?: boolean) => {
+    if (favorite) {
+      unfavoritePage(pathname);
+    } else {
+      favoritePage(pathname);
+    }
+  };
+
+  const isFavorite = !!favoritePages.find(({ pathname, favorite }) => pathname === href && favorite);
+  return enableAllServicesRedesign ? (
     <Content component={ContentVariants.p} className={classNames('chr-c-favorite-trigger')}>
       <Flex display={{ default: 'inlineFlex' }} gap={{ default: 'gapXs' }}>
         <FlexItem>
@@ -58,6 +74,38 @@ const AllServicesLink = ({ href, title, sectionTitle, bundleTitle, isExternal = 
           <div className="pf-v6-u-font-size-xs pf-v6-u-text-color-subtle">{bundleTitle}</div>
         </FlexItem>
       </Flex>
+    </Content>
+  ) : (
+    <Content
+      component={ContentVariants.p}
+      className={classNames('chr-c-favorite-trigger', {
+        'chr-c-icon-favorited': isFavorite,
+      })}
+    >
+      <ChromeLink
+        appId={appId}
+        isExternal={isExternal}
+        href={href ?? '#'}
+        data-ouia-component-id={`${category}-${group ? `${group}-` : ''}${titleToId(title ?? '')}-Link`}
+      >
+        {title}
+        {isExternal && (
+          <Icon className="pf-v6-u-ml-sm chr-c-icon-external-link" isInline>
+            <ExternalLinkAltIcon />
+          </Icon>
+        )}
+      </ChromeLink>
+      {!isExternal && (
+        <Icon
+          data-ouia-component-id={`${category}-${group ? `${group}-` : ''}${titleToId(title ?? '')}-FavoriteToggle`}
+          onClick={() => handleFavouriteToggle(href ?? '#', isFavorite)}
+          aria-label={`${isFavorite ? 'Unfavorite' : 'Favorite'} ${title}`}
+          className="pf-v6-u-ml-sm chr-c-icon-star"
+          isInline
+        >
+          <StarIcon />
+        </Icon>
+      )}
     </Content>
   );
 };
