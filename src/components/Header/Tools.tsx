b24f339c468a0ca2aa5ee5ca06963be39e12b381
radekkaluzik <51480040+radekkaluzik@users.noreply.github.com>
2025-06-18 14:02:10 +0200

    added Add ouia id to User Access menu (#3120)

--- src/components/Header/Tools.tsx
+++ src/components/Header/Tools.tsx
@@ -119,6 +119,7 @@ const Tools = () => {
       title: 'Identity and Access Management',
       items: [
         {
+          ouiaId: 'UserAccess',
           url: identityAndAccessManagmentPath,
           title: isOrgAdmin ? (workspacesEnabled ? 'Acess management' : 'User Access') : 'My User Access',
           description:
