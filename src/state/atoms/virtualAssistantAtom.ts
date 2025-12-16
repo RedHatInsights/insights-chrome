f1312c7ee45fea1f24afbc76356945883d9da062
Justin Orringer <justinorringer@gmail.com>
2025-12-16 15:52:43 -0500

    Open VA using remote hook instead of chrome hook

--- src/state/atoms/virtualAssistantAtom.ts
+++ src/state/atoms/virtualAssistantAtom.ts
@@ -1,5 +1,3 @@
 import { atom } from 'jotai';
 
 export const virtualAssistantShowAssistantAtom = atom<boolean>(false);
-export const virtualAssistantOpenAtom = atom<boolean>(false);
-export const virtualAssistantStartInputAtom = atom<string | undefined>(undefined);
