2ce95cf03419b6553fd31cbc39b01244ab56e0b9
Charles Mulder <1179063+charlesmulder@users.noreply.github.com>
2026-09-02 10:28:51 +0100

    test(cypress): wrap WorkspaceSelector in a controlled Unleash provider

--- cypress/component/WorkspaceSelector/WorkspaceSelector.cy.tsx
+++ cypress/component/WorkspaceSelector/WorkspaceSelector.cy.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { Provider as JotaiProvider } from 'jotai';
+import { FlagProvider } from '@unleash/proxy-client-react';
 import { IntlProvider } from 'react-intl';
 import WorkspaceSelector from '../../../src/components/WorkspaceSelector/WorkspaceSelector';
 import chromeStore from '../../../src/state/chromeStore';
 
+const mockUnleashConfig = {
+  url: 'http://localhost:4242/api/frontend',
+  clientKey: 'test-key',
+  appName: 'test-app',
+  refreshInterval: 0,
+  disableRefresh: true,
+  bootstrap: [
+    {
+      name: 'platform.chrome.workspace-global_selector',
+      enabled: false,
+      variant: { name: 'disabled', enabled: false },
+      impressionData: false,
+    },
+  ],
+};
+
 const Wrapper = ({ children }: { children: React.ReactNode }) => (
   <IntlProvider locale="en">
-    <JotaiProvider store={chromeStore}>{children}</JotaiProvider>
+    <FlagProvider config={mockUnleashConfig}>
+      <JotaiProvider store={chromeStore}>{children}</JotaiProvider>
+    </FlagProvider>
   </IntlProvider>
 );
 
